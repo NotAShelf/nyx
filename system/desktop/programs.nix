621a05724ae591fdb87821a072f04d83fbe0fc12
NotAShelf <itsashelf@gmail.com>
2022-11-04 02:20:25 +0300

    please work

--- system/desktop/programs.nix
+++ system/desktop/programs.nix
@@ -4,48 +4,11 @@
   lib,
   inputs,
   ...
-}:
-with lib; let
-  # this is required for wayland stuff
-  dbus-hyprland-environment = pkgs.writeTextFile {
-    name = "dbus-hyprland-environment";
-    destination = "/bin/dbus-hyprland-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gesettings/schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnome.desktop.interface
-      gesettings set $gnome_schema gtk-theme 'Adwaita'
-    '';
-  };
-in {
-  # disabledModules = [ "services/hardware/udev.nix" ];
-  imports = [
-    ./bootloader.nix
-    ./fonts.nix
-    ./network.nix
-    ./security.nix
-    ./blocker.nix
-  ];
+}: in {
 
   environment.variables = {
     BROWSER = "firefox";
   };
-
-
 
   programs = {
     ccache.enable = true;
@@ -71,6 +34,4 @@ in {
   };
 
   #upower.enable = true;
-
-  system.stateVersion = "22.05"; # DONT TOUCH THIS
 }
