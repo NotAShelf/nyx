27331bcba7bd7ee8b373e0e3ef1cfdf4ef28e7bf
NotAShelf <itsashelf@gmail.com>
2022-11-03 19:09:44 +0300

    refactor: move services

--- system/services.nix
+++ system/services.nix
@@ -2,7 +2,23 @@
   config,
   pkgs,
   ...
-}: {
+}: 
+
+{
+  systemd.services = {
+    seatd = {
+      enable = true;
+      description = "Seat management daemon";
+      script = "${pkgs.seatd}/bin/seatd -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "1";
+      };
+      wantedBy = ["multi-user.target"];
+    };
+  };
+
   services = {
     syncthing = {
       enable = false;
