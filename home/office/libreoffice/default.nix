1ab834d26cbac63a9ac9bb308194e209769144db
NotAShelf <itsashelf@gmail.com>
2022-11-09 19:59:53 +0300

    fix: how the fuck did this happen

--- home/office/libreoffice/default.nix
+++ home/office/libreoffice/default.nix
@@ -14,56 +14,9 @@ in {
   };
   
   home.packages = with pkgs; [
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-wlr
-    libnotify
-    wf-recorder
-    brightnessctl
-    pamixer
-    python39Packages.requests
-    slurp
-    tesseract5
-    ocr
-    grim
-    screenshot
-    wl-clipboard
-    pngquant
+    libreoffice-qt
+    hunspell
+    hunspellDicts.uk_UA
+    hunspellDicts.th_TH
   ];
-
-  # xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
-  # systemd.user.services.swayidle.Install.WantedBy = lib.mkForce ["hyprland-session.target"];
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.default.override {
-      nvidiaPatches = true;
-      wlroots =
-        inputs.hyprland.packages.${pkgs.system}.wlroots-hyprland.overrideAttrs
-        (old: {
-          patches =
-            (old.patches or [])
-            ++ [
-              (pkgs.fetchpatch {
-                url = "https://aur.archlinux.org/cgit/aur.git/plain/0001-nvidia-format-workaround.patch?h=hyprland-nvidia-screenshare-git";
-                sha256 = "A9f1p5EW++mGCaNq8w7ZJfeWmvTfUm4iO+1KDcnqYX8=";
-              })
-            ];
-        });
-    };
-    systemdIntegration = true;
-    extraConfig = builtins.readFile ./hyprland.conf;
-  };
-
-  services.gammastep = {
-    enable = true;
-    provider = "geoclue2";
-  };
-
-  systemd.user.services = {
-    swaybg = mkService {
-      Unit.Description = "Wallpaper chooser";
-      Service.ExecStart = "${pkgs.swaybg}/bin/swaybg -i ${./wall.png}";
-    };
-  };
 }
