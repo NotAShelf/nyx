326c72bb68e573932688649bf610073da64251a1
NotAShelf <itsashelf@gmail.com>
2022-11-09 21:06:22 +0300

    fix: wrong parent

--- system/desktop/services.nix
+++ system/desktop/services.nix
@@ -8,7 +8,31 @@
   systemd = {
     targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
     services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+    user = {
+      services.nextcloud-autosync = {
+      Unit = {
+        Description = "Auto sync Nextcloud";
+        After = "network-online.target"; 
+      };
+      Service = {
+        Type = "simple";
+        ExecStart= "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --path /music /home/myuser/music https://nextcloud.example.org"; 
+        TimeoutStopSec = "180";
+        KillMode = "process";
+        KillSignal = "SIGINT";
+      };
+      Install.WantedBy = ["multi-user.target"];
+    };
+    timers.nextcloud-autosync = {
+      Unit.Description = "Automatic sync files with Nextcloud when booted up after 5 minutes then rerun every 60 minutes";
+      Timer.OnUnitActiveSec = "60min";
+      Install.WantedBy = ["multi-user.target" "timers.target"];
+    };
+    startServices = true;
+
+    }
   };
+
 
   location.provider = "geoclue2";
 
@@ -47,24 +71,14 @@
       '';
     };
 
-    nextcloud-autosync = {
-      Unit = {
-        Description = "Auto sync Nextcloud";
-        After = "network-online.target"; 
-      };
-      Service = {
-        Type = "simple";
-        ExecStart= "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --path /music /home/myuser/music https://nextcloud.example.org"; 
-        TimeoutStopSec = "180";
-        KillMode = "process";
-        KillSignal = "SIGINT";
-      };
-      Install.WantedBy = ["multi-user.target"];
-    };
-    timers.nextcloud-autosync = {
-      Unit.Description = "Automatic sync files with Nextcloud when booted up after 5 minutes then rerun every 60 minutes";
-      Timer.OnUnitActiveSec = "60min";
-      Install.WantedBy = ["multi-user.target" "timers.target"];
+    syncthing = {
+      enable = false;
+      openDefaultPorts = true;
+      user = "notashelf";
+      group = "wheel";
+      dataDir = "$HOME/syncthing";
+      configDir = "$HOME/.config/syncthing/";
+      systemService = true;
     };
 
     resolved.enable = true;
@@ -97,5 +111,4 @@
       };
     };
   };
-  startServices = true;
 }
