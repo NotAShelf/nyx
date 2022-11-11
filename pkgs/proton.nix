7ade481fcec2d34d9fa7f0b0be6826d52de00642
NotAShelf <itsashelf@gmail.com>
2022-11-12 02:18:30 +0300

    style: formatting

--- pkgs/proton.nix
+++ pkgs/proton.nix
@@ -3,7 +3,7 @@
   pkgs,
   ...
 }: let
-  version = "7-41"; 
+  version = "7-41";
   source = fetchTarball {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/GE-Proton${version}.tar.gz";
     sha256 = "18hfag1nzj6ldy0ign2yjfzfms0w23vmcykgl8h1dfk0xjaql8gk";
