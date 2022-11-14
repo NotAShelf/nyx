37348cce9e9bfd4820c293ffc90127b501782762
NotAShelf <itsashelf@gmail.com>
2022-11-15 02:27:44 +0300

    dev: update nix settings

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -89,6 +89,8 @@
     extraOptions = ''
       accept-flake-config = true
       warn-dirty = false
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
     '';
   };
 }
