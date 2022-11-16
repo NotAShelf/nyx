ca5b54452f17de11135bf5032a10dc95c244696f
NotAShelf <itsashelf@gmail.com>
2022-11-16 16:51:46 +0300

    refactor: network wait timer style change

--- modules/desktop/services.nix
+++ modules/desktop/services.nix
@@ -6,8 +6,7 @@
   # Don't wait for network startup
   # https://old.reddit.com/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a
   systemd = {
-    targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
-    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+    services.NetworkManager-wait-online.enable = false;
   };
 
   location.provider = "geoclue2";
