d3ca40d5baa4a0fa56d2256ae98ba11a38ecc9d8
NotAShelf <itsashelf@gmail.com>
2022-11-20 10:51:25 +0300

    fix: update doom-emacs sha256 param

--- modules/home/terminal/emacs/default.nix
+++ modules/home/terminal/emacs/default.nix
@@ -2,7 +2,7 @@
   doom-emacs =
     pkgs.callPackage (builtins.fetchTarball {
       url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
-      sha256 = "0mz4032pd3ky51h9q3fxm581naal241wjh9gnvkh72s1jgymka33";
+      sha256 = "16l0kpahklch9qq21k00ipq85q5nxw3na0kflcybfdsxr9i3z95a";
     }) {
       doomPrivateDir = ./doom.d;
     };
