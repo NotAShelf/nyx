04fe08b15bdbf315c7940a7673182108c89b5afe
NotAShelf <itsashelf@gmail.com>
2022-11-20 12:38:18 +0300

    dev: patch me baby one more time

--- pkgs/nicksfetch.nix
+++ pkgs/nicksfetch.nix
@@ -21,12 +21,7 @@ stdenvNoCC.mkDerivation rec {
   };
 
   patches = [
-    (fetchpatch {
-      url = "https://github.com/dylanaraps/neofetch/commit/413c32e55dc16f0360f8e84af2b59fe45505f81b.patch";
-      sha256 = "1fapdg9z79f0j3vw7fgi72b54aw4brn42bjsj48brbvg3ixsciph";
-      name = "avoid_overwriting_gio_extra_modules_env_var.patch";
-    })
-    ./patches/neofetch-01.patch
+    ./patches/nicksfetch.patch
   ];
 
   outputs = ["out" "man"];
