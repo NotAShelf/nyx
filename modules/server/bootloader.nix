b2ca09f76879e00cc2061e9653082ed698b70f64
NotAShelf <itsashelf@gmail.com>
2022-11-21 13:40:36 +0300

    dev: undefine server bootloader

--- modules/server/bootloader.nix
+++ modules/server/bootloader.nix
@@ -7,17 +7,17 @@
 }: {
   boot = {
     loader = {
-      systemd-boot.enable = false;
-      grub = {
-        enable = true;
-        useOSProber = true;
-        efiSupport = true;
-        enableCryptodisk = false;
-        device = "nodev";
-        theme = null;
-        backgroundColor = null;
-        splashImage = null;
-      };
+      #systemd-boot.enable = false;
+      #grub = {
+      #  enable = true;
+      #  useOSProber = true;
+      #  efiSupport = true;
+      #  enableCryptodisk = false;
+      #  device = "nodev";
+      #  theme = null;
+      #  backgroundColor = null;
+      #  splashImage = null;
+      #};
     };
 
     cleanTmpDir = true;
