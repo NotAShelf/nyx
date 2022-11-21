46dd5266ef95000789b96791d69cc32db1036b61
NotAShelf <itsashelf@gmail.com>
2022-11-21 12:19:44 +0300

    feat: add server specific users

--- modules/server/users.nix
+++ modules/server/users.nix
@@ -11,4 +11,24 @@
     home = "/var/lib/gitea";
     group = "gitea";
   };
+
+  users.users.donut = {
+    group = "users";
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    initialPassword = "changeme";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICYvciy1uq5tU179OgChwrgbrGia4tUQr1onik5GuHc6 amr@nicksos"
+    ];
+  };
+
+  users.users.raf = {
+    group = "users";
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    initialPassword = "changeme";
+    openssh.authorizedKeys.keys = [
+      ""
+    ];
+  };
 }
