c5c9280a3b146268dc3ac75db0ef0a49c582b792
sioodmy <sioodmy@tuta.io>
2022-11-26 13:31:17 +0100

    dev: fix emacs commit mismatch

--- modules/home/terminal/emacs/default.nix
+++ modules/home/terminal/emacs/default.nix
@@ -2,7 +2,7 @@
   doom-emacs =
     pkgs.callPackage (builtins.fetchTarball {
       url = "https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz";
-      sha256 = "16l0kpahklch9qq21k00ipq85q5nxw3na0kflcybfdsxr9i3z95a";
+      sha256 = "1ha3lwpxd72hqz8dll0vx1qva0h9q6yqi0h66szrv69sg2kdi0px";
     }) {
       doomPrivateDir = ./doom.d;
     };
