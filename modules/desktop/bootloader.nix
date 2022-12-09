04c86fc6b2ef4362a6968da80eb24d1e02a9eaa8
NotAShelf <itsashelf@gmail.com>
2022-12-09 14:15:08 +0300

    dev: disable plymouth themeing again

--- modules/desktop/bootloader.nix
+++ modules/desktop/bootloader.nix
@@ -4,20 +4,18 @@
   pkgs,
   ...
 }: {
-  # TODO: one day make this use custom themes from
-  # https://github.com/spikespaz/dotfiles/tree/master/dotpkgs/plymouth-themes
-  #boot.plymouth.enable = true;
+  boot.plymouth.enable = true;
 
   # configure plymouth theme
   # <https://github.com/adi1090x/plymouth-themes>
-  boot.plymouth = let
-    pack = 3;
-    theme = "hud_3";
-  in {
-    enable = true;
-    themePackages = [
-      (pkgs.nixos-plymouth.override {inherit pack theme;})
-    ];
-    inherit theme;
-  };
+  #boot.plymouth = let
+  #  pack = 3;
+  #  theme = "hud_3";
+  #in {
+  #  enable = true;
+  #  themePackages = [
+  #    (pkgs.nixos-plymouth.override {inherit pack theme;})
+  #  ];
+  #  inherit theme;
+  #};
 }
