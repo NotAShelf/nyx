6c4c673adf13c8dadd17ab1c8987921e1b124398
NotAShelf <itsashelf@gmail.com>
2022-12-15 11:21:02 +0300

    feat: add more options to spotifyd

--- modules/server/services.nix
+++ modules/server/services.nix
@@ -17,5 +17,22 @@
     };
 
     spotifyd.enable = true;
+    services.spotifyd = {
+      enable = true;
+      package = pkgs.spotifyd.override {
+        withKeyring = true;
+        withMpris = true;
+        withPulseAudio = true;
+      };
+      settings = {
+        global = {
+          backend = "pulseaudio";
+          bitrate = 320;
+          use_mpris = true;
+          username = "spikespaz@outlook.com";
+          password_cmd = "cat '${config.homeage.mount}/jacob.spotifyd.age'";
+        };
+      };
+    };
   };
 }
