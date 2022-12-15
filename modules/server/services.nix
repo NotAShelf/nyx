72a1460956b01a4a7b7a7758f1b72eafdb873592
NotAShelf <itsashelf@gmail.com>
2022-12-15 16:33:17 +0300

    feat: remove spotifyd

--- modules/server/services.nix
+++ modules/server/services.nix
@@ -4,9 +4,6 @@
   lib,
   ...
 }: {
-  # let spotifyd be discovered
-  networking.firewall.allowedTCPPorts = [57621];
-
   services = {
     # Enable cron service
     cron = {
@@ -14,24 +11,6 @@
       systemCronJobs = [
         "*/50 * * * *      root    date >> /tmp/cron.log"
       ];
-    };
-
-    spotifyd = {
-      enable = false;
-      package = pkgs.spotifyd.override {
-        withKeyring = true;
-        withMpris = true;
-        withPulseAudio = true;
-      };
-      settings = {
-        global = {
-          backend = "pulseaudio";
-          bitrate = 320;
-          use_mpris = true;
-          username = "email for spotify"; # TODO: bind new email to spotify
-          password_cmd = "cat <password file>"; # TODO: secret encryption w/ ragenix
-        };
-      };
     };
   };
 }
