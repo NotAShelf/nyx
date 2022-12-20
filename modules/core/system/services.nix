47192a05fea87d9ccdd839d359b26ca4c767babd
NotAShelf <itsashelf@gmail.com>
2022-12-20 09:56:17 +0300

    doc: add comments to more system services

--- modules/core/system/services.nix
+++ modules/core/system/services.nix
@@ -1,11 +1,16 @@
 {pkgs, ...}: {
   services = {
+    # monitor and control temparature
     thermald.enable = true;
+    # handle ACPI events
     acpid.enable = true;
+    # discard blocks that are not in use by the filesystem, good for SSDs
     fstrim.enable = true;
     # firmware updater for machine hardware
     fwupd.enable = true;
+    # "nix-shell replacement for project development"
     lorri.enable = true;
+    # limit systemd journal size
     journald.extraConfig = ''
       SystemMaxUse=50M
       RuntimeMaxUse=10M
