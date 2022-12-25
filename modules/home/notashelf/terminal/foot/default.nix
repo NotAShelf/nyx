6e7f2baa54f8f117f025450971daa40a29fd1084
NotAShelf <itsashelf@gmail.com>
2022-12-26 01:37:16 +0300

    feat(nix-colors): switch to nix-colors for foot

--- modules/home/notashelf/terminal/foot/default.nix
+++ modules/home/notashelf/terminal/foot/default.nix
@@ -2,7 +2,9 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  inherit (config.colorscheme) colors;
+in {
   home.packages = with pkgs; [
     libsixel # for displaying images
   ];
@@ -21,23 +23,23 @@
         foreground = "cdd6f4"; # Text
         background = "1e1e2e"; # Base
 
-        regular0 = "45475a"; # Surface 1
-        regular1 = "f38ba8"; # red
-        regular2 = "a6e3a1"; # green
-        regular3 = "f9e2af"; # yellow
-        regular4 = "89b4fa"; # blue
-        regular5 = "f5c2e7"; # pink
-        regular6 = "94e2d5"; # teal
-        regular7 = "bac2de";
+        regular0 = "${colors.base03}"; # Surface 1
+        regular1 = "${colors.base08}"; # red
+        regular2 = "${colors.base0B}"; # green
+        regular3 = "${colors.base0A}"; # yellow
+        regular4 = "${colors.base0D}"; # blue
+        regular5 = "${colors.base0F}"; # pink
+        regular6 = "${colors.base0C}"; # teal
+        regular7 = "${colors.base06}"; # Subtext 0
         # Subtext 1
-        bright0 = "585b70 "; # Surface 2
-        bright1 = "f38ba8 "; # red
-        bright2 = "a6e3a1 "; # green
-        bright3 = "f9e2af "; # yellow
-        bright4 = "89b4fa "; # blue
-        bright5 = "f5c2e7 "; # pink
-        bright6 = "94e2d5 "; # teal
-        bright7 = "a6adc8 "; # Subtext 0
+        bright0 = "${colors.base04}"; # Surface 2
+        bright1 = "${colors.base08}"; # red
+        bright2 = "${colors.base0B}"; # green
+        bright3 = "${colors.base0A}"; # yellow
+        bright4 = "${colors.base0D}"; # blue
+        bright5 = "${colors.base0F}"; # pink
+        bright6 = "${colors.base0C}"; # teal
+        bright7 = "${colors.base07}"; # Subtext 0
       };
       mouse = {
         hide-when-typing = "yes";
