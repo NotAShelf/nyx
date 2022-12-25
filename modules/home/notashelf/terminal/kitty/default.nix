b6893831d004d0313da891b2dd8264b4eab2069a
NotAShelf <itsashelf@gmail.com>
2022-12-26 01:37:26 +0300

    feat(nix-colors): switch to nix-colors for kitty

--- modules/home/notashelf/terminal/kitty/default.nix
+++ modules/home/notashelf/terminal/kitty/default.nix
@@ -1,18 +1,22 @@
 {
   config,
-  lib,
   pkgs,
   ...
-}: {
+}: let
+  inherit (config.colorscheme) colors;
+in {
   programs.kitty = {
     enable = true;
     settings = {
-      background_opacity = "0.87";
+      # General
+      background_opacity = "0.85";
       font_family = "monospace";
-      font_size = 14;
+      font_size = 16;
+      disable_ligatures = "never";
+      cursor_shape = "underline";
       cursor_blink_interval = "0.5";
       cursor_stop_blinking_after = "15.0";
-      scrollback_lines = 2000;
+      scrollback_lines = 10000;
       click_interval = "0.5";
       select_by_word_characters = ":@-./_~?&=%+#";
       remember_window_size = false;
@@ -22,60 +26,88 @@
       repaint_delay = 15;
       input_delay = 3;
       visual_bell_duration = "0.0";
+      url_style = "double";
       open_url_with = "default";
       confirm_os_window_close = 0;
-
       enable_audio_bell = false;
-
       window_padding_width = 15;
       window_margin_width = 10;
-      disable_ligatures = "never";
-      foreground = "#C6D0F5";
-      background = "#303446";
 
-      selection_foreground = "#303446";
-      selection_background = "#F2D5CF";
-
-      cursor = "#F2D5CF";
-      cursor_text_color = "#303446";
-      url_color = "#F2D5CF";
-
-      mark1_foreground = "#303446";
-      mark1_background = "#BABBF1";
-      mark2_foreground = "#303446";
-      mark2_background = "#CA9EE6";
-      mark3_foreground = "#303446";
-      mark3_background = "#85C1DC";
-
-      active_border_color = "#BABBF1";
-      inactive_border_color = "#737994";
-
-      color0 = "#51576D";
-      color1 = "#E78284";
-      color2 = "#A6D189";
-      color3 = "#E5C890";
-      color4 = "#8CAAEE";
-      color5 = "#F4B8E4";
-      color6 = "#81C8BE";
-      color7 = "#B5BFE2";
-      color8 = "#626880";
-      color9 = "#E78284";
-      color10 = "#A6D189";
-      color11 = "#E5C890";
-      color12 = "#8CAAEE";
-      color13 = "#F4B8E4";
-      color14 = "#81C8BE";
-      color15 = "#A5ADCE";
+      # Colorschemes
+      foreground = "#${colors.base05}";
+      background = "#${colors.base00}";
+      selection_background = "#${colors.base05}";
+      selection_foreground = "#${colors.base00}";
+      url_color = "#${colors.base04}";
+      cursor = "#${colors.base05}";
+      active_border_color = "#${colors.base03}";
+      inactive_border_color = "#${colors.base01}";
+      active_tab_background = "#${colors.base00}";
+      active_tab_foreground = "#${colors.base05}";
+      inactive_tab_background = "#${colors.base01}";
+      inactive_tab_foreground = "#${colors.base04}";
+      tab_bar_background = "#${colors.base01}";
+      color0 = "#${colors.base00}";
+      color1 = "#${colors.base08}";
+      color2 = "#${colors.base0B}";
+      color3 = "#${colors.base0A}";
+      color4 = "#${colors.base0D}";
+      color5 = "#${colors.base0E}";
+      color6 = "#${colors.base0C}";
+      color7 = "#${colors.base05}";
+      color8 = "#${colors.base03}";
+      color9 = "#${colors.base08}";
+      color10 = "#${colors.base0B}";
+      color11 = "#${colors.base0A}";
+      color12 = "#${colors.base0D}";
+      color13 = "#${colors.base0E}";
+      color14 = "#${colors.base0C}";
+      color15 = "#${colors.base07}";
+      color16 = "#${colors.base09}";
+      color17 = "#${colors.base0F}";
+      color18 = "#${colors.base01}";
+      color19 = "#${colors.base02}";
+      color20 = "#${colors.base04}";
+      color21 = "#${colors.base06}";
     };
 
     keybindings = {
       "ctrl+c" = "copy_or_interrupt";
-      "ctrl+shift+up" = "increase_font_size";
-      "ctrl+shift+down" = "decrease_font_size";
-      "ctrl+shift+backspace" = "restore_font_size";
       "ctrl+alt+c" = "copy_to_clipboard";
       "ctrl+alt+v" = "paste_from_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
+
+      "ctrl+shift+up" = "increase_font_size";
+      "ctrl+shift+down" = "decrease_font_size";
+      "ctrl+shift+backspace" = "restore_font_size";
+
+      "ctrl+shift+enter" = "new_window";
+      "ctrl+shift+n" = "new_os_window";
+      "ctrl+shift+w" = "close_window";
+      "ctrl+shift+]" = "next_window";
+      "ctrl+shift+[" = "previous_window";
+      "ctrl+shift+f" = "move_window_forward";
+      "ctrl+shift+b" = "move_window_backward";
+      "ctrl+shift+`" = "move_window_to_top";
+      "ctrl+shift+1" = "first_window";
+      "ctrl+shift+2" = "second_window";
+      "ctrl+shift+3" = "third_window";
+      "ctrl+shift+4" = "fourth_window";
+      "ctrl+shift+5" = "fifth_window";
+      "ctrl+shift+6" = "sixth_window";
+      "ctrl+shift+7" = "seventh_window";
+      "ctrl+shift+8" = "eighth_window";
+      "ctrl+shift+9" = "ninth_window";
+      "ctrl+shift+0" = "tenth_window";
+
+      "ctrl+shift+right" = "next_tab";
+      "ctrl+shift+left" = "previous_tab";
+      "ctrl+shift+t" = "new_tab";
+      "ctrl+shift+q" = "close_tab";
+      "ctrl+shift+l" = "next_layout";
+      "ctrl+shift+." = "move_tab_forward";
+      "ctrl+shift+," = "move_tab_backward";
+      "ctrl+shift+alt+t" = "set_tab_title";
     };
   };
 }
