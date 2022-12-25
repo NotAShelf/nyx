43795abbf14f031fad5016e42dfe9c1a3175413e
NotAShelf <itsashelf@gmail.com>
2022-12-26 00:35:32 +0300

    feat: use nix-colors for declarative theming

---
+++ modules/home/notashelf/colors/palettes/catppuccin-mocha.nix
@@ -0,0 +1,25 @@
+{
+  colorscheme = {
+    slug = "catppuccin-mocha";
+    name = "Catppuccin Mocha";
+    kind = "dark";
+    colors = {
+      base00 = "#1e1e2e"; # Base
+      base01 = "#181825"; # Mantle
+      base02 = "#313244"; # Surface0
+      base03 = "#45475a"; # Surface1
+      base04 = "#585b70"; # Surface2
+      base05 = "#cdd6f4"; # text
+      base06 = "#f5e0dc"; # rosewater
+      base07 = "#b4befe"; # lavender
+      base08 = "#f38ba8"; # red
+      base09 = "#fab387"; # peach
+      base0A = "#a6e3a1"; # yellow
+      base0B = "#94e2d5"; # green
+      base0C = "#94e2d5"; # teal
+      base0D = "#89b4fa"; # blue
+      base0E = "#cba6f7"; # mauve
+      base0F = "#f2cdcd"; # flamingo
+    };
+  };
+}
