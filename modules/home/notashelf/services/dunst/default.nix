40660aac8ef5b16baa4a9dc0a4f0c067faed472f
NotAShelf <itsashelf@gmail.com>
2022-12-27 23:23:18 +0300

    feat: update dunst icons

--- modules/home/notashelf/services/dunst/default.nix
+++ modules/home/notashelf/services/dunst/default.nix
@@ -1,39 +1,79 @@
 {
-  config,
-  self,
   pkgs,
-  lib,
+  config,
   ...
 }: let
-  volume = let
-    pamixer = lib.getExe pkgs.pamixer;
-    notify-send = pkgs.libnotify + "/bin/notify-send";
+  notify-send = pkgs.libnotify + "/bin/notify-send";
+  pamixer = pkgs.pamixer + "/bin/pamixer";
+
+  volume = pkgs.writeShellScriptBin "volume" ''
+    #!/bin/sh
+
+    ${pamixer} "$@"
+    volume="$(${pamixer} --get-volume-human)"
+
+    if [ "$volume" = "muted" ]; then
+        ${notify-send} -r 69 \
+            -a "Volume" \
+            "Muted" \
+            -i ${./assets/volume-mute.svg} \
+            -t 888 \
+            -u low
+    else
+        ${notify-send} -r 69 \
+            -a "Volume" "Currently at $volume" \
+            -h int:value:"$volume" \
+            -i ${./assets/volume.svg} \
+            -t 888 \
+            -u low
+    fi
+  '';
+
+  microphone = pkgs.writeShellScriptBin "microphone" ''
+    #!/bin/sh
+
+    ${pamixer} --default-source "$@"
+    mic="$(${pamixer} --default-source --get-volume-human)"
+
+    if [ "$mic" = "muted" ]; then
+        ${notify-send} -r 69 \
+            -a "Microphone" \
+            "Muted" \
+            -i ${./assets/mic-mute.svg} \
+            -t 888 \
+            -u low
+    else
+      ${notify-send} -r 69 \
+            -a "Microphone" "Currently at $mic" \
+            -h int:value:"$mic" \
+            -i ${./assets/mic.svg} \
+            -t 888 \
+            -u low
+    fi
+  '';
+
+  brightness = let
+    brightnessctl = pkgs.brightnessctl + "/bin/brightnessctl";
   in
-    pkgs.writeShellScriptBin "volume" ''
+    pkgs.writeShellScriptBin "brightness" ''
       #!/bin/sh
 
-      ${pamixer} "$@"
+      ${brightnessctl} "$@"
+      brightness=$(echo $(($(${brightnessctl} g) * 100 / $(${brightnessctl} m))))
 
-      volume="$(${pamixer} --get-volume-human)"
-
-      if [ "$volume" = "muted" ]; then
-          ${notify-send} -r 69 \
-              -a "Volume" \
-              "Muted" \
-              -i ${./icons/mute.svg} \
-              -t 888 \
-              -u low
-      else
-          ${notify-send} -r 69 \
-              -a "Volume" "Currently at $volume" \
-              -h int:value:"$volume" \
-              -i ${./icons/volume.svg} \
-              -t 888 \
-              -u low
-      fi
+      ${notify-send} -r 69 \
+          -a "Brightness" "Currently at $brightness%" \
+          -h int:value:"$brightness" \
+          -i ${./assets/brightness.svg} \
+          -t 888 \
+          -u low
     '';
 in {
-  home.packages = [volume];
+  home.packages = [
+    volume
+    microphone
+    brightness
+  ];
   services.dunst = {
     enable = true;
     package = pkgs.dunst.overrideAttrs (oldAttrs: {
@@ -45,54 +85,83 @@ in {
       };
     });
     iconTheme = {
-      package = self.packages.${pkgs.system}.catppuccin-folders;
-      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
     };
     settings = let
       inherit (config.colorscheme) colors;
     in {
       global = {
-        frame_color = "#${colors.base0F}";
-        separator_color = "#${colors.base0D}";
-        width = 220;
-        height = 220;
-        offset = "0x15";
-        font = "Iosevka 16";
-        corner_radius = 10;
+        follow = "mouse";
+        width = 300;
         origin = "top-center";
-        notification_limit = 3;
+        alignment = "left";
+        vertical_alignment = "center";
+        ellipsize = "middle";
+        offset = "15x15";
+        padding = 15;
+        horizontal_padding = 15;
+        text_icon_padding = 15;
+        icon_position = "left";
+        min_icon_size = 48;
+        max_icon_size = 64;
+        progress_bar = true;
+        progress_bar_height = 8;
+        progress_bar_frame_width = 1;
+        progress_bar_min_width = 150;
+        progress_bar_max_width = 300;
+        separator_height = 2;
+        frame_width = 2;
+        frame_color = "#${colors.base01}";
+        separator_color = "frame";
+        corner_radius = 8;
+        transparency = 0;
+        gap_size = 8;
+        line_height = 0;
+        notification_limit = 0;
         idle_threshold = 120;
-        ignore_newline = "no";
-        mouse_left_click = "close_current";
-        mouse_right_click = "close_all";
-        sticky_history = "yes";
         history_length = 20;
         show_age_threshold = 60;
-        ellipsize = "middle";
-        padding = 10;
+        markup = "full";
+        font = "Liga SFMono Nerd Font 10";
+        format = "<span size='x-large' font_desc='Cantarell,Liga SFMono Nerd Font 9' weight='bold' foreground='#${colors.base05}'>%a</span>\\n%s\\n%b";
+        word_wrap = "yes";
+        sort = "yes";
+        shrink = "no";
+        indicate_hidden = "yes";
+        sticky_history = "yes";
+        ignore_newline = "no";
+        show_indicators = "no";
+        stack_duplicates = true;
         always_run_script = true;
-        frame_width = 3;
-        transparency = 10;
-        progress_bar = true;
-        progress_bar_frame_width = 0;
-        highlight = "#${colors.base06}";
+        hide_duplicate_count = false;
+        ignore_dbusclose = false;
+        force_xwayland = false;
+        force_xinerama = false;
+        mouse_left_click = "do_action";
+        mouse_middle_click = "close_all";
+        mouse_right_click = "close_current";
       };
-      fullscreen_delay_everything.fullscreen = "delay";
+
+      fullscreen_delay_everything = {fullscreen = "delay";};
+
       urgency_low = {
-        background = "#${colors.base02}";
+        timeout = 3;
+        background = "#${colors.base00}";
         foreground = "#${colors.base05}";
-        timeout = 5;
+        highlight = "#${colors.base0C}";
       };
       urgency_normal = {
-        background = "##${colors.base02}";
-        foreground = "#${colors.base05}";
         timeout = 6;
+        background = "#${colors.base00}";
+        foreground = "#${colors.base05}";
+        highlight = "#${colors.base0C}";
       };
       urgency_critical = {
-        background = "##${colors.base02}";
-        foreground = "#${colors.base05}";
-        frame_color = "#${colors.base09}";
         timeout = 0;
+        background = "#${colors.base00}";
+        foreground = "#${colors.base05}";
+        highlight = "#${colors.base0C}";
       };
     };
   };
