783e13f9b8211c9c3f798fed444707bf6d65c935
NotAShelf <itsashelf@gmail.com>
2022-12-28 00:53:46 +0300

    feat: groundwork for decay base16

---
+++ modules/home/notashelf/themes/palettes/decay.nix
@@ -0,0 +1,26 @@
+{
+  colorscheme = {
+    slug = "decay-dark";
+    name = "Decay Dark";
+    kind = "dark";
+    colors = {
+      # TODO: make this a real colorscheme because the maintainers won't
+      base00 = ""; # Base
+      base01 = ""; # Mantle
+      base02 = ""; # Surface0
+      base03 = ""; # Surface1
+      base04 = ""; # Surface2
+      base05 = ""; # text
+      base06 = ""; # rosewater
+      base07 = ""; # lavender
+      base08 = ""; # red
+      base09 = ""; # peach
+      base0A = ""; # yellow
+      base0B = ""; # green
+      base0C = ""; # teal
+      base0D = ""; # blue
+      base0E = ""; # mauve
+      base0F = ""; # flamingo
+    };
+  };
+}
