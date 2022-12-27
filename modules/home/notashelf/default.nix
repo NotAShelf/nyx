a9e81b85bb014f01a7df249fb16e131cb4f29c87
NotAShelf <itsashelf@gmail.com>
2022-12-27 20:33:31 +0300

    doc: add comments to configurations

--- modules/home/notashelf/default.nix
+++ modules/home/notashelf/default.nix
@@ -5,15 +5,18 @@
   ...
 }: {
   imports = [
+    # import system packages
     ./packages.nix
 
+    # apps and services I use
     ./graphical
     ./terminal
+    ./services
     ./gaming
 
+    # declarative system and program theme
     ./themes
   ];
-
   config = {
     home = {
       stateVersion = "23.05";
