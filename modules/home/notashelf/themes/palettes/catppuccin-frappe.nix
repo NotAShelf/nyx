fcc7f5715747125c03e51e50f13c4b9ae172032c
NotAShelf <itsashelf@gmail.com>
2022-12-27 20:44:07 +0300

    feat: add license for initializing projects

---
+++ modules/home/notashelf/themes/palettes/catppuccin-frappe.nix
@@ -0,0 +1,25 @@
+{
+  colorscheme = {
+    slug = "catppuccin-frappe";
+    name = "Catppuccin Frappe";
+    kind = "dark";
+    colors = {
+      base00 = "#303446"; # Base
+      base01 = "#292c3c"; # Mantle
+      base02 = "#414559"; # Surface0
+      base03 = "#51576d"; # Surface1
+      base04 = "#626880"; # Surface2
+      base05 = "#c6d0f5"; # Text
+      base06 = "#f2d5cf"; # Rosewater
+      base07 = "#babbf1"; # Lavender
+      base08 = "#e78284"; # Red
+      base09 = "#ef9f76"; # Peach
+      base0A = "#e5c890"; # Yellow
+      base0B = "#a6d189"; # Green
+      base0C = "#81c8be"; # Teal
+      base0D = "#8caaee"; # Blue
+      base0E = "#ca9ee6"; # Mauve
+      base0F = "#eebebe"; # Flamingo
+    };
+  };
+}
