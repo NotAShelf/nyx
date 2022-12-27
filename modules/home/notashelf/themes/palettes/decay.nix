4c6e86e87f5f089ca4baf1995e533e7e265a5609
NotAShelf <itsashelf@gmail.com>
2022-12-28 01:04:01 +0300

    feat: cleanup

--- modules/home/notashelf/themes/palettes/decay.nix
+++ modules/home/notashelf/themes/palettes/decay.nix
@@ -11,16 +11,16 @@
       base03 = "#1c1c26"; # Surface1
       base04 = "#2b2b35"; # Surface2
       base05 = "#c3cddb"; # text
-      base06 = "#c3cddb"; # rosewater
-      base07 = "#c8d2e0"; # lavender
-      base08 = "#c8d2e0"; # red
-      base09 = "#fa9a9a"; # peach
+      base06 = "#c3cddb"; # "
+      base07 = "#c8d2e0"; # white
+      base08 = "#c8d2e0"; # "
+      base09 = "#fa9a9a"; # red
       base0A = "#f8e7b7"; # yellow
       base0B = "#b0e2ae"; # green
-      base0C = "#8cbef7"; # teal
-      base0D = "#93cff7"; # blue
-      base0E = "#bfa6fa"; # mauve
-      base0F = "#fa9a9a"; # flamingo
+      base0C = "#93cff7"; # teal
+      base0D = "#8cbef7"; # blue
+      base0E = "#bfa6fa"; # magenta
+      base0F = "#fa9a9a"; # red again
     };
   };
 }
