912d48fc769aef38b40c72f5df4e7224ae9128de
NotAShelf <itsashelf@gmail.com>
2022-12-29 17:32:48 +0300

    feat: relocate touchpad to laptop module

--- modules/desktop/xserver.nix
+++ modules/desktop/xserver.nix
@@ -7,21 +7,5 @@
     enable = true;
     displayManager.gdm.enable = false;
     displayManager.lightdm.enable = false;
-
-    libinput = {
-      enable = true;
-
-      # disable mouse acceleration
-      mouse.accelProfile = "flat";
-      mouse.accelSpeed = "0";
-      mouse.middleEmulation = false;
-
-      # touchpad settings
-      touchpad.naturalScrolling = true;
-      touchpad.tapping = true;
-      touchpad.clickMethod = "clickfinger";
-      touchpad.horizontalScrolling = false;
-      touchpad.disableWhileTyping = true;
-    };
   };
 }
