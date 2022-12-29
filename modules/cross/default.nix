96d93beb471d86ad67a33c7f3ae7883519134f67
NotAShelf <itsashelf@gmail.com>
2022-12-29 17:32:30 +0300

    feat: modularize binfmt

---
+++ modules/cross/default.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  boot.binfmt = {
+    emulatedSystems = ["aarch64-linux"];
+    registrations.aarch64-linux = {
+      interpreter = lib.mkForce "${pkgs.qemu}/bin/qemu-aarch64";
+    };
+  };
+  nix.settings.extra-sandbox-paths = ["/run/binfmt" "${pkgs.qemu}"];
+}
