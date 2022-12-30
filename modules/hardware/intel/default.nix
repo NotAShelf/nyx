55d1ea52be251c056731a484a999b6acaee581ea
NotAShelf <itsashelf@gmail.com>
2022-12-31 00:19:13 +0300

    feat: make intel go FAST

--- modules/hardware/intel/default.nix
+++ modules/hardware/intel/default.nix
@@ -6,7 +6,10 @@
 }: {
   boot.initrd.kernelModules = ["i915"];
 
+  boot.kernelParams = ["i915.fastboot=1" "i915.enable_fbc=1" "enable_gvt=1"];
+
   nixpkgs.config.packageOverrides = pkgs: {
+    # let play youtube videos without h.264, please and thank you
     vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
   };
 
@@ -17,8 +20,8 @@
   hardware = {
     opengl = {
       extraPackages = with pkgs; [
-        intel-media-driver
-        vaapiIntel
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       ];
       extraPackages32 = with pkgs.pkgsi686Linux; [
         intel-media-driver
