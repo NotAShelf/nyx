dc324a8f5c338dd73cc86fa34654fe3454e5f47e
NotAShelf <itsashelf@gmail.com>
2023-01-10 14:11:44 +0300

    feat: steam compatibility tools override

--- modules/desktop/gaming/default.nix
+++ modules/desktop/gaming/default.nix
@@ -1,38 +1,11 @@
 {
+  config,
   pkgs,
-  inputs,
   lib,
   ...
-}: let
-  programs = lib.makeBinPath (with pkgs; [
-    inputs.hyprland.packages.${pkgs.system}.default
-    gojq
-    systemd
-  ]);
-
-  startscript = pkgs.writeShellScript "gamemode-start" ''
-    export PATH=$PATH:${programs}
-    export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:no_vfr 1'
-  '';
-
-  endscript = pkgs.writeShellScript "gamemode-end" ''
-    export PATH=$PATH:${programs}
-    export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:no_vfr 0'
-  '';
-in {
-  programs.gamemode = {
-    enable = true;
-    settings = {
-      general = {
-        softrealtime = "auto";
-        renice = 15;
-      };
-      custom = {
-        start = "${startscript}";
-        end = "${endscript}";
-      };
-    };
-  };
+}: {
+  imports = [
+    ./steam.nix
+    ./gamemode.nix
+  ];
 }
