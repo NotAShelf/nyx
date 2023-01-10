5837dd034477fcce8c36900530d1dac4903074ef
NotAShelf <itsashelf@gmail.com>
2023-01-10 14:41:02 +0300

    fix: reocurring definition

--- home/notashelf/terminal/shell/bin/show-zombie-parents.nix
+++ home/notashelf/terminal/shell/bin/show-zombie-parents.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}:
 # https://www.linkedin.com/pulse/how-identify-kill-zombiedefunct-processes-linux-without-george-gabra/
-pkgs.writeShellScriptBin "show-zombie-parents" ''
+''
   ps -A -ostat,ppid | grep -e '[zZ]'| awk '{ print $2 }' | uniq | xargs ps -p
 ''
