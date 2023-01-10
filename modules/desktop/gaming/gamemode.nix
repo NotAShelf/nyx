5edbd7f79388b469f728ed0c64038bd9345ef074
NotAShelf <itsashelf@gmail.com>
2023-01-10 19:01:06 +0300

    feat: add notifications to gamemode scripts

--- modules/desktop/gaming/gamemode.nix
+++ modules/desktop/gaming/gamemode.nix
@@ -14,16 +14,19 @@
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
     hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:no_vfr 1'
+    ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations activated'
   '';
 
   endscript = pkgs.writeShellScript "gamemode-end" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
     hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:no_vfr 0'
+    ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations deactivated'
   '';
 in {
   programs.gamemode = {
     enable = true;
+    enableRenice = true;
     settings = {
       general = {
         softrealtime = "auto";
