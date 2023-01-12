248b623bf6b92bf78077623fac2cad8ef5653d54
NotAShelf <itsashelf@gmail.com>
2023-01-12 22:41:31 +0300

    feat: break overlays into multiple files

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -9,37 +9,4 @@ final: prev: {
   cloneit = prev.callPackage ../../pkgs/cloneit.nix {};
   proton-ge = prev.callPackage ../../pkgs/proton-ge.nix {};
   anime4k = prev.callPackage ../../pkgs/anime4k.nix {};
-
-  ungoogled-chromium = prev.ungoogled-chromium.override {
-    commandLineArgs = toString [
-      # Ungoogled features
-      "--disable-search-engine-collection"
-      "--extension-mime-request-handling=always-prompt-for-install"
-      "--fingerprinting-canvas-image-data-noise"
-      "--fingerprinting-canvas-measuretext-noise"
-      "--fingerprinting-client-rects-noise"
-      "--popups-to-tabs"
-      "--show-avatar-button=incognito-and-guest"
-
-      # Experimental features
-      "--enable-features=${
-        final.lib.concatStringsSep "," [
-          "BackForwardCache:enable_same_site/true"
-          "CopyLinkToText"
-          "OverlayScrollbar"
-          "TabHoverCardImages"
-          "VaapiVideoDecoder"
-        ]
-      }"
-
-      # Aesthetics
-      "--force-dark-mode"
-
-      # Performance
-      "--enable-gpu-rasterization"
-      "--enable-oop-rasterization"
-      "--enable-zero-copy"
-      "--ignore-gpu-blocklist"
-    ];
-  };
 }
