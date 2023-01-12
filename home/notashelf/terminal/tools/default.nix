950a011ec3cb3b4991db9921b77746ea159a96d5
NotAShelf <itsashelf@gmail.com>
2023-01-12 17:18:52 +0300

    feat: break terminal tools into multiple files

--- home/notashelf/terminal/tools/default.nix
+++ home/notashelf/terminal/tools/default.nix
@@ -3,110 +3,11 @@
   lib,
   config,
   ...
-}: let
-  browser = ["firefox.desktop"];
-
-  associations = {
-    "text/html" = browser;
-    "x-scheme-handler/http" = browser;
-    "x-scheme-handler/https" = browser;
-    "x-scheme-handler/ftp" = browser;
-    "x-scheme-handler/about" = browser;
-    "x-scheme-handler/unknown" = browser;
-    "application/x-extension-htm" = browser;
-    "application/x-extension-html" = browser;
-    "application/x-extension-shtml" = browser;
-    "application/xhtml+xml" = browser;
-    "application/x-extension-xhtml" = browser;
-    "application/x-extension-xht" = browser;
-
-    "audio/*" = ["mpv.desktop"];
-    "video/*" = ["mpv.dekstop"];
-    "image/*" = ["imv.desktop"];
-    "application/json" = browser;
-    "application/pdf" = ["org.pwmt.zathura.desktop.desktop"];
-    "x-scheme-handler/tg" = ["telegramdesktop.desktop"];
-    "x-scheme-handler/spotify" = ["spotify.desktop"];
-    "x-scheme-handler/discord" = ["WebCord.desktop"];
-  };
-in {
-  imports = [./nix-index.nix];
-
-  services = {
-    udiskie.enable = true;
-    gpg-agent = {
-      enable = true;
-      pinentryFlavor = "gnome3";
-      enableSshSupport = true;
-      enableZshIntegration = true;
-    };
-  };
-
-  programs = {
-    man.enable = true;
-    gpg = {
-      enable = true;
-      homedir = "${config.xdg.dataHome}/gnupg";
-    };
-
-    # nix-index
-    /*
-    nix-index = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-    };
-    */
-
-    tealdeer = {
-      enable = true;
-      settings = {
-        display = {
-          compact = false;
-          use_pager = true;
-        };
-        updates = {
-          auto_update = true;
-        };
-      };
-    };
-    bat = {
-      enable = true;
-      themes = {
-        Catppuccin-frappe = builtins.readFile (pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "bat";
-            rev = "00bd462e8fab5f74490335dcf881ebe7784d23fa";
-            sha256 = "yzn+1IXxQaKcCK7fBdjtVohns0kbN+gcqbWVE4Bx7G8=";
-          }
-          + "/Catppuccin-frappe.tmTheme");
-      };
-      config.theme = "Catppuccin-frappe";
-    };
-  };
-  xdg = {
-    userDirs = {
-      enable = true;
-      createDirectories = true;
-      documents = "$HOME/Documents";
-      download = "$HOME/Downloads";
-      videos = "$HOME/Media/Videos";
-      music = "$HOME/Media/Music";
-      pictures = "$HOME/Media/Pictures";
-      desktop = "$HOME/Desktop";
-      publicShare = "$HOME/Public/Share";
-      templates = "$HOME/Public/Templates";
-
-      extraConfig = {
-        XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
-        XDG_DEV_DIR = "$HOME/Dev";
-      };
-    };
-
-    mimeApps = {
-      enable = true;
-      associations.added = associations;
-      defaultApplications = associations;
-    };
-  };
+}: {
+  imports = [
+    ./nix-index.nix
+    ./programs.nix
+    ./services.nix
+    ./xdg.nix
+  ];
 }
---
+++ home/notashelf/terminal/tools/services.nix
@@ -0,0 +1,11 @@
+{...}: {
+  services = {
+    udiskie.enable = true;
+    gpg-agent = {
+      enable = true;
+      pinentryFlavor = "gnome3";
+      enableSshSupport = true;
+      enableZshIntegration = true;
+    };
+  };
+}
