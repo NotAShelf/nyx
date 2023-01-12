fdc7e034d759a58090749ea4059bcb22b37d0a09
NotAShelf <itsashelf@gmail.com>
2023-01-12 14:03:00 +0300

    feat: btrfs settings for icarus

--- hosts/icarus/system.nix
+++ hosts/icarus/system.nix
@@ -1,3 +1,24 @@
-{...}: {
-  # ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    acpi
+  ];
+
+  fileSystems = {
+    "/".options = ["compress=zstd" "noatime"];
+    "/home".options = ["compress=zstd"];
+    "/nix".options = ["compress=zstd" "noatime"];
+  };
+
+  hardware = {
+    # allow usage of potentially proprietary firmware b lobs
+    enableRedistributableFirmware = true;
+    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    # my GPU is not properly supported by open source drivers
+  };
 }
