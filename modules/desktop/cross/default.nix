d4365e44265d34c7f775bbedb9e6d8b7f2e4af26
NotAShelf <itsashelf@gmail.com>
2023-01-14 18:11:40 +0300

    feat: additional emulation

--- modules/desktop/cross/default.nix
+++ modules/desktop/cross/default.nix
@@ -4,7 +4,10 @@
   ...
 }: {
   boot.binfmt = {
-    emulatedSystems = ["aarch64-linux"];
+    emulatedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+    ];
     registrations.aarch64-linux = {
       interpreter = lib.mkForce "${pkgs.qemu}/bin/qemu-aarch64";
     };
