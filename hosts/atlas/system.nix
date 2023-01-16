8acd0fba6398823a71413b5e9f3eb72a00418f81
NotAShelf <itsashelf@gmail.com>
2023-01-16 23:56:01 +0300

    feat: guess you can sponsor me now?

--- hosts/atlas/system.nix
+++ hosts/atlas/system.nix
@@ -3,52 +3,82 @@
   pkgs,
   lib,
   ...
-}: {
-  fileSystems = {
-    "/" = {
-      device = lib.mkForce "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = ["noatime"];
-    };
-  };
-
-  environment.systemPackages = with pkgs; [git neovim];
-
-  # Enable GPU acceleration
-  hardware.raspberry-pi."4".fkms-3d.enable = true;
-
-  boot = {
-    # Use mainline kernel, vendor kernel has some issues compiling due to
-    # missing modules that shouldn't even be in the closure.
-    # https://github.com/NixOS/nixpkgs/issues/111683
-
-    # we define this at config level so that we can use it in the
-    # pi module
-    #kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = lib.mkForce ["bridge" "macvlan" "tap" "tun" "loop" "atkbd" "ctr"];
-    supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "ext4" "vfat"];
-  };
-
-  services = {
-    xserver = {
-      enable = false;
-      displayManager.lightdm.enable = false;
-      desktopManager.xfce.enable = false;
-    };
-
-    create_ap = {
-      enable = true;
-      settings = {
-        INTERNET_IFACE = "eth0";
-        WIFI_IFACE = "wlan0";
-        SSID = "Pizone";
-        PASSPHRASE = "12345678";
+}:
+with lib; let
+  cfg = config.modules.device;
+in {
+  config = {
+    modules = {
+      device = {
+        type = "server";
+        cpu = "pi";
+        gpu = "pi";
+        monitors = ["HDMI-A-1"];
+        hasBluetooth = true;
+        hasSound = true;
+        hasTPM = true;
+      };
+      system = {
+        fs = ["ext4" "vfat"];
+        video.enable = true;
+        sound.enable = true;
+        username = "notashelf";
+      };
+      usrEnv = {
+        isWayland = false;
+        desktop = [];
+        useHomeManager = false; # FALSE FOR NOW FIXME
       };
     };
-  };
 
-  security.tpm2 = {
-    enable = false;
-    abrmd.enable = false;
+    environment.systemPackages = with pkgs; [git neovim];
+    hardware = {
+      # Enable GPU acceleration
+      raspberry-pi."4".fkms-3d.enable = true;
+
+      opengl = {
+        # this only takes effect in 64 bit systems
+        driSupport32Bit = mkForce false;
+      };
+    };
+
+    boot = {
+      kernelModules = lib.mkForce ["bridge" "macvlan" "tap" "tun" "loop" "atkbd" "ctr"];
+      supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "ext4" "vfat"];
+    };
+
+    services = {
+      xserver = {
+        enable = false;
+        displayManager.lightdm.enable = false;
+        desktopManager.xfce.enable = false;
+      };
+
+      create_ap = {
+        enable = true;
+        settings = {
+          INTERNET_IFACE = "eth0";
+          WIFI_IFACE = "wlan0";
+          SSID = "Pizone";
+          PASSPHRASE = "12345678";
+        };
+      };
+    };
+
+    security.tpm2 = {
+      enable = false;
+    };
+
+    fileSystems = {
+      "/" = {
+        device = lib.mkForce "/dev/disk/by-label/NIXOS_SD";
+        fsType = "ext4";
+        options = ["noatime"];
+      };
+    };
+
+    hardware = {
+      enableRedistributableFirmware = true;
+    };
   };
 }
--- hosts/icarus/system.nix
+++ hosts/icarus/system.nix
@@ -1,24 +1,65 @@
 {
   config,
-  lib,
   pkgs,
+  lib,
   ...
-}: {
-  environment.systemPackages = with pkgs; [
-    acpi
-  ];
+}:
+with lib; let
+  cfg = config.modules.device;
+in {
+  config = {
+    modules = {
+      device = {
+        type = "laptop";
+        cpu = "intel";
+        gpu = "intel";
+        monitors = ["eDP-1"];
+        hasBluetooth = true;
+        hasSound = true;
+        hasTPM = true;
+      };
+      system = {
+        fs = ["btrfs" "vfat" "ntfs"];
+        video.enable = true;
+        sound.enable = true;
+        username = "notashelf";
+      };
+      usrEnv = {
+        isWayland = true;
+        desktop = ["hyprland"];
+        useHomeManager = true;
+      };
+    };
 
-  fileSystems = {
-    "/".options = ["compress=zstd" "noatime"];
-    "/home".options = ["compress=zstd"];
-    "/nix".options = ["compress=zstd" "noatime"];
-  };
+    security.tpm2 = {
+      # enable Trusted Platform Module 2 support
+      enable = cfg.hasTPM;
+      # enable Trusted Platform 2 userspace resource manager daemon
+      abrmd.enable = mkDefault true;
+      # set TCTI environment variables to the specified values if enabled
+      # - TPM2TOOLS_TCTI
+      # - TPM2_PKCS11_TCTI
+      tctiEnvironment.enable = mkDefault true;
+      # enable TPM2 PKCS#11 tool and shared library in system path
+      pkcs11.enable = mkDefault true;
+    };
 
-  hardware = {
-    # allow usage of potentially proprietary firmware b lobs
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    fileSystems = {
+      "/".options = ["compress=zstd" "noatime"];
+      "/home".options = ["compress=zstd"];
+      "/nix".options = ["compress=zstd" "noatime"];
+    };
 
-    # my GPU is not properly supported by open source drivers
+    hardware = {
+      enableRedistributableFirmware = true;
+    };
+
+    boot = {
+      kernelParams = [
+        "i915.enable_fbc=1"
+        "i915.enable_psr=2"
+        "nohibernate"
+      ];
+    };
   };
 }
