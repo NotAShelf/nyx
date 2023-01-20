1b2542b186c1cb500c1dcc6a36feb539a1aa8e03
NotAShelf <itsashelf@gmail.com>
2023-01-21 01:07:55 +0300

    feat: swayidle gets its own service

---
+++ home/notashelf/services/swayidle/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  suspendScript = pkgs.writeShellScript "suspend-script" ''
+    ${pkgs.pipewire}/bin/pw-cli i all | ${pkgs.ripgrep}/bin/rg running
+    # only suspend if audio isn't running
+    if [ $? == 1 ]; then
+      ${pkgs.systemd}/bin/systemctl suspend
+    fi
+  '';
+in {
+  config = {
+    # screen idle
+    services.swayidle = {
+      enable = true;
+      events = [
+        {
+          event = "before-sleep";
+          command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+        }
+        {
+          event = "lock";
+          command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+        }
+      ];
+      timeouts = [
+        {
+          timeout = 310;
+          command = "${pkgs.systemd}/bin/loginctl lock-session";
+        }
+        {
+          timeout = 310;
+          command = suspendScript.outPath;
+        }
+      ];
+    };
+  };
+}
