d5d3a1dac221e26b667b96691fa3c55af2d87b63
NotAShelf <itsashelf@gmail.com>
2023-01-23 20:56:23 +0300

    feat: obs plugins

--- home/notashelf/gaming/obs/default.nix
+++ home/notashelf/gaming/obs/default.nix
@@ -1,10 +1,23 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+with lib; let
+  env = osConfig.modules.usrEnv;
+in {
   programs = {
     obs-studio = {
       enable = true;
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-      ];
+      plugins = with pkgs.obs-studio-plugins;
+        [
+          obs-gstreamer
+          obs-pipewire-audio-capture
+          obs-vkcapture
+        ]
+        ++ optional (env.isWayland)
+        pkgs.obs-studio-plugins.wlrobs;
     };
   };
 }
