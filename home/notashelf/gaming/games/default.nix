0d26966a6d8339f7dee0996c259482de986a756b
NotAShelf <itsashelf@gmail.com>
2023-01-28 09:19:59 +0300

    feat: modularize home-manager gaming configuration

--- home/notashelf/gaming/games/default.nix
+++ home/notashelf/gaming/games/default.nix
@@ -1,31 +1,17 @@
 {
+  lib,
   pkgs,
-  inputs,
-  self,
+  osConfig,
   ...
-}: {
-  home.packages = with inputs.nix-gaming.packages.${pkgs.system};
-    [
-      (inputs.nix-gaming.lib.legendaryBuilder {
-        games = {
-          rocket-league = {
-            desktopName = "Rocket League";
-            tricks = ["dxvk" "win10"];
-            icon = builtins.fetchurl rec {
-              url = "https://img.favpng.com/21/12/21/rocket-league-video-game-psyonix-logo-decal-png-favpng-yYh6A3FRCJNh7JYgYZchHxAia.jpg";
-              name = "rocket-league-${sha256}.png";
-              sha256 = "0ymisjgcmw8mkarnkm95dsndii2aw9yn5i3vfximi2dchy4ng8ab";
-            };
-            discordIntegration = false;
-          };
-        };
+}:
+with lib; let
+  programs = osConfig.modules.programs;
+  device = osConfig.modules.device;
 
-        opts.wine = wine-tkg;
-        inherit (pkgs) system;
-      })
-      .rocket-league
-    ]
-    ++ (with pkgs; [
+  acceptedTypes = ["laptop" "desktop" "lite"];
+in {
+  config = mkIf ((builtins.elem device.type acceptedTypes) && (programs.gaming.enable)) {
+    home.packages = with pkgs; [
       #gamescope # FIXME: override package wlroots
       legendary-gl
       prismlauncher
@@ -34,5 +20,6 @@
       mangohud
       taisei
       unciv
-    ]);
+    ];
+  };
 }
