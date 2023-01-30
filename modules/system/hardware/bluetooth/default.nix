75143008ff424015b5b56855d78711e50cfa7d1f
NotAShelf <itsashelf@gmail.com>
2023-01-30 11:06:08 +0300

    feat: more bluetooth options to the bluetooth module

--- modules/system/hardware/bluetooth/default.nix
+++ modules/system/hardware/bluetooth/default.nix
@@ -12,6 +12,14 @@ in {
       enable = true;
       package = pkgs.bluez5-experimental;
       #hsphfpd.enable = true;
+      powerOnBoot = true;
+      disabledPlugins = ["sap"];
+      settings = {
+        General = {
+          JustWorksRepairing = "always";
+          MultiProfile = "multiple";
+        };
+      };
     };
 
     # https://nixos.wiki/wiki/Bluetooth
