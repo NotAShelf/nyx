512d71c7d10bc02ec4eeb1d2d86c97a6e0b3e74b
NotAShelf <itsashelf@gmail.com>
2023-03-04 18:49:29 +0300

    feat: disable nvimtree on_startup opts

--- home/notashelf/terminal/editors/cnvim/nvim/lua/tree.lua
+++ home/notashelf/terminal/editors/cnvim/nvim/lua/tree.lua
@@ -1,8 +1,6 @@
 require("nvim-tree").setup({
 	disable_netrw = true,
 	hijack_netrw = true,
-	-- open_on_setup = false,
-	ignore_ft_on_setup = { "alpha" },
 	hijack_cursor = true,
 	hijack_unnamed_buffer_when_opening = false,
 	update_cwd = true,
