b08378041f13881c544f515710b8bf79fcdc8b43
NotAShelf <itsashelf@gmail.com>
2023-03-09 22:35:43 +0300

    feat: prism should use catppuccin mocha

--- home/notashelf/gaming/games/default.nix
+++ home/notashelf/gaming/games/default.nix
@@ -9,24 +9,32 @@ with lib; let
   device = osConfig.modules.device;
 
   acceptedTypes = ["laptop" "desktop" "lite"];
+
+  catppuccin = pkgs.fetchzip {
+    url = "https://raw.githubusercontent.com/catppuccin/prismlauncher/main/themes/Mocha/Catppuccin-Mocha.zip";
+    sha256 = "8uRqCoe9iSIwNnK13d6S4XSX945g88mVyoY+LZSPBtQ=";
+  };
 in {
   config = mkIf ((builtins.elem device.type acceptedTypes) && (programs.gaming.enable)) {
-    home.packages = with pkgs; [
-      #gamescope # FIXME: override package wlroots
-      legendary-gl
-      prismlauncher
-      mono
-      winetricks
-      mangohud
-      taisei
-      unciv
-      # get dotnet runtime 6
-      dotnet-runtime_6
+    home = {
+      file.".local/share/PrismLauncher/themes/mocha".source = catppuccin;
+      file.".local/share/PrismLauncher/themes/mocha".recursive = true;
 
-      # jre 17
-      temurin-jre-bin-17
-    ];
+      packages = with pkgs; [
+        #gamescope # FIXME: override package wlroots
+        legendary-gl
+        prismlauncher
+        mono
+        winetricks
+        mangohud
+        taisei
+        unciv
+        # get dotnet runtime 6
+        dotnet-runtime_6
+
+        # jre 17
+        temurin-jre-bin-17
+      ];
+    };
   };
-
-  # TODO: configure mangohud
 }
