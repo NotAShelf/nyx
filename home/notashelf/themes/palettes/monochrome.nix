5cc7d7bfee49525a5f13e5b772c8c410f45901d6
NotAShelf <itsashelf@gmail.com>
2023-03-09 09:29:42 +0300

    dev: monochrome palette?

---
+++ home/notashelf/themes/palettes/monochrome.nix
@@ -0,0 +1,25 @@
+{
+  colorscheme = {
+    slug = "monochrome";
+    name = "monochrome";
+    kind = "dark";
+    colors = {
+      base00 = "#121517"; # Base
+      base01 = "#2e3338"; # Mantle
+      base02 = "#4a5159"; # Surface0
+      base03 = "#6c757d"; # Surface1
+      base04 = "#6c757d"; # Surface2
+      base05 = "#e9ecef"; # text
+      base06 = "#e9ecef"; # "
+      base07 = "#f8f9fa"; # white
+      base08 = "#f8f9fa"; # white
+      base09 = "#f8f9fa"; # white
+      base0A = "#f8f9fa"; # white
+      base0B = "#f8f9fa"; # white
+      base0C = "#f8f9fa"; # white
+      base0D = "#f8f9fa"; # white
+      base0E = "#f8f9fa"; # white
+      base0F = "#f8f9fa"; # white
+    };
+  };
+}
