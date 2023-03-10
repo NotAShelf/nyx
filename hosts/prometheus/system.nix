3df26e7f3de22a630ff4f8d981e9f23e52351784
NotAShelf <itsashelf@gmail.com>
2023-03-10 21:22:29 +0300

    dev: give up on nvidia

--- hosts/prometheus/system.nix
+++ hosts/prometheus/system.nix
@@ -13,7 +13,7 @@ in {
       device = {
         type = "laptop";
         cpu = "intel";
-        gpu = "hybrid-nv"; # nvidia drivers :b:roke
+        gpu = "intel"; # nvidia drivers :b:roke
         monitors = ["eDP-1" "HDMI-A-1"];
         hasBluetooth = true;
         hasSound = true;
