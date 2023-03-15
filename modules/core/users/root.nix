f212a840f73631a703e8f7d2c166465fec3777fe
NotAShelf <itsashelf@gmail.com>
2023-03-15 20:42:22 +0300

    feat: configure root user

--- modules/core/users/root.nix
+++ modules/core/users/root.nix
@@ -1,6 +1,7 @@
 _: {
-  users.mutableUsers = false;
   users.users.root = {
+    # I am *not* locking myself out of the system again
+    # thank you very much.
     initialPassword = "changeme";
 
     # passwordFile needs to be in a volume marked with `neededForBoot = true`
