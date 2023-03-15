d21f40acace3f564746f4ff99b6b446092e4721a
NotAShelf <itsashelf@gmail.com>
2023-03-15 20:44:00 +0300

    feat: make users immutable

--- modules/core/users/default.nix
+++ modules/core/users/default.nix
@@ -2,5 +2,11 @@
   # we want to handle user configurations on a per-file basis - users that are not in users/<username>.nix don't get to be a real user
   imports = [
     ./notashelf.nix
+    ./root.nix
   ];
+
+  # users are not mutable outside our configurations
+  # if you are on nixos, you are probably smart enough to not try and edit users
+  # manually outside your configuration.nix or whatever
+  users.mutableUsers = false;
 }
