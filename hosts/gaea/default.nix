550cc56e6bc2f10c4cf14785dadbafbabfd5a83e
NotAShelf <itsashelf@gmail.com>
2023-03-15 20:42:07 +0300

    feat: slim down rescue iso

--- hosts/gaea/default.nix
+++ hosts/gaea/default.nix
@@ -3,7 +3,8 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+with lib; {
   # FIXME: for some reason, we cannot boot off ventoy
   # and have to burn the iso to an entire USB with dd
   # dd if=result/iso/*.iso of=/dev/sdX status=progress #
@@ -40,6 +41,7 @@
   console = let
     variant = "u24n";
   in {
+    # hidpi terminal font
     font = "${pkgs.terminus_font}/share/consolefonts/ter-${variant}.psf.gz";
     keyMap = "trq";
   };
@@ -58,4 +60,28 @@
 
   # faster compression in exchange for larger iso size
   isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+
+  services.getty.helpLine =
+    ''
+      The "nixos" and "root" accounts have empty passwords.
+      An ssh daemon is running. You then must set a password
+      for either "root" or "nixos" with `passwd` or add an ssh key
+      to /home/nixos/.ssh/authorized_keys be able to login.
+      If you need a wireless connection, you may use networkmanager
+      by invoking `nmcli` or `nmtui`, the ncurses interface.
+    ''
+    + optionalString config.services.xserver.enable ''
+      Type `sudo systemctl start display-manager' to
+      start the graphical user interface.
+    '';
+
+  # borrow some environment options from the minimal profile to save space
+  environment.noXlibs = mkDefault true;
+  documentation = {
+    enable = mkDefault false;
+
+    doc.enable = mkDefault false;
+
+    info.enable = mkDefault false;
+  };
 }
