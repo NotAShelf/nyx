ad7ef4f4b30572786b927331c045aaf6781cff0c
NotAShelf <itsashelf@gmail.com>
2023-03-15 21:05:00 +0300

    feat: add key device for unattended boot

--- hosts/epimetheus/encryption.nix
+++ hosts/epimetheus/encryption.nix
@@ -3,5 +3,12 @@ _: {
   boot.initrd.availableKernelModules = [
     "aesni_intel"
     "cryptd"
+    "usb_storage"
   ];
+
+  boot.initrd.luks.devices."enc" = {
+    allowDiscards = true;
+    keyFileSize = 4096;
+    keyFile = "/dev/disk/by-id/usb-Generic_Flash_Disk_B314B63E-0:0";
+  };
 }
