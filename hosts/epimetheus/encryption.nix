9c327093777359598d427ce5548a98502602dbcb
NotAShelf <itsashelf@gmail.com>
2023-03-20 01:13:50 +0300

    feat: decrypt disk before LVM

--- hosts/epimetheus/encryption.nix
+++ hosts/epimetheus/encryption.nix
@@ -12,6 +12,7 @@ _: {
     keyFileSize = 4096;
     # the device with the maching id will be searched for the key file
     keyFile = "/dev/disk/by-id/usb-Generic_Flash_Disk_B314B63E-0:0";
+    preLVM = true;
     # if keyfile is not there, fall back to cryptsetup password
     # fallbackToPassword = true; # IMPLIED BY config.boot.initrd.systemd.enable
   };
