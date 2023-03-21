ceec7975e7e49b559db97d4edeedd70b6007dbe9
NotAShelf <itsashelf@gmail.com>
2023-03-21 21:56:16 +0300

    feat(runners): run non-nix software easily

---
+++ modules/desktop/system/services/runners.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  # run appimages with appimage-run
+  boot.binfmt.registrations = lib.genAttrs ["appimage" "AppImage"] (ext: {
+    recognitionType = "extension";
+    magicOrExtension = ext;
+    interpreter = "/run/current-system/sw/bin/appimage-run";
+  });
+
+  # run unpatched linux binaries with nix-ld
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc
+      openssl
+      curl
+      glib
+      util-linux
+      glibc
+      icu
+      libunwind
+      libuuid
+      zlib
+      libsecret
+      # graphical
+      freetype
+      libglvnd
+      libnotify
+      SDL2
+      vulkan-loader
+      gdk-pixbuf
+      xorg.libX11
+    ];
+  };
+}
