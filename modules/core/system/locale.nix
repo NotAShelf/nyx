45d84391f56164cffcfbde9292a4145569b55443
NotAShelf <itsashelf@gmail.com>
2023-03-21 21:57:35 +0300

    feat: configure more locale settings

--- modules/core/system/locale.nix
+++ modules/core/system/locale.nix
@@ -8,11 +8,27 @@
     hardwareClockInLocalTime = true;
   };
 
-  i18n = {
-    defaultLocale = lib.mkDefault "en_US.UTF-8";
+  i18n = let
+    defaultLocale = "en_US.UTF-8";
+    tr = "tr_TR.UTF-8";
+  in {
+    inherit defaultLocale;
 
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "en_US.UTF-8";
+      LANG = defaultLocale;
+      LC_COLLATE = defaultLocale;
+      LC_CTYPE = defaultLocale;
+      LC_MESSAGES = defaultLocale;
+
+      LC_ADDRESS = tr;
+      LC_IDENTIFICATION = tr;
+      LC_MEASUREMENT = tr;
+      LC_MONETARY = tr;
+      LC_NAME = tr;
+      LC_NUMERIC = tr;
+      LC_PAPER = tr;
+      LC_TELEPHONE = tr;
+      LC_TIME = tr;
     };
 
     supportedLocales = lib.mkDefault [
