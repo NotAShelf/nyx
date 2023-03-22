7a8bde0c149c2e830fa3fd8439dd0dc9106fc134
NotAShelf <itsashelf@gmail.com>
2023-03-22 20:55:42 +0300

    dev(grub): reset theme package to `null`

--- modules/bootloader/common.nix
+++ modules/bootloader/common.nix
@@ -54,60 +54,88 @@ in {
 
       # https://www.kernel.org/doc/html/v4.14/admin-guide/kernel-parameters.html
       kernelParams = [
-        #  enables calls to ACPI methods through /proc/acpi/call
+        # enables calls to ACPI methods through /proc/acpi/call
         "acpi_call"
+
         # https://en.wikipedia.org/wiki/Kernel_page-table_isolation
         "pti=on"
+
         # make stack-based attacks on the kernel harder
         "randomize_kstack_offset=on"
+
         # this has been defaulted to none back in 2016 - break really old binaries for security
         "vsyscall=none"
+
         # https://tails.boum.org/contribute/design/kernel_hardening/
         "slab_nomerge"
+
         # needs to be on for powertop
         "debugfs=on"
+
         # only allow signed modules
         "module.sig_enforce=1"
+
         # blocks access to all kernel memory, even preventing administrators from being able to inspect and probe the kernel
         "lockdown=confidentiality"
+
         # enable buddy allocator free poisoning
         "page_poison=1"
+
         # performance improvement for direct-mapped memory-side-cache utilization, reduces the predictability of page allocations
         "page_alloc.shuffle=1"
+
         # for debugging kernel-level slab issues
-        "slub_debug=FZP" # WARNING: this will leak unhashed memory addresses to dmesg
+        "slub_debug=FZP"
+        /*
+        WARNING: this will leak unhashed memory addresses to dmesg
+        */
+
         # always-enable sysrq keys. Useful for debugging
         "sysrq_always_enabled=0"
+
         # save power on idle by limiting c-states
         # https://gist.github.com/wmealing/2dd2b543c4d3cff6cab7
         "processor.max_cstate=5"
+
         # disable the intel_idle driver and use acpi_idle instead
         "idle=nomwait"
+
         # ignore access time (atime) updates on files, except when they coincide with updates to the ctime or mtime
         "rootflags=noatime"
+
         # enable IOMMU for devices used in passthrough and provide better host performance
         "iommu=pt"
+
         # disable usb autosuspend
         "usbcore.autosuspend=-1"
         # linux security modules
         "lsm=landlock,lockdown,yama,apparmor,bpf"
-        # KERN_DEBUG for debugging
+
+        # 7 = KERN_DEBUG for debugging
         "loglevel=7"
+
         # isables resume and restores original swap space
         "noresume"
+
         # allows systemd to set and save the backlight state
         "acpi_backlight=native" # none | vendor | video | native
+
         # prevent the kernel from blanking plymouth out of the fb
         "fbcon=nodefer"
+
         # disable boot logo if any
         "logo.nologo"
+
         # tell the kernel to not be verbose
         "quiet"
+
         # disable systemd status messages
         # rd prefix means systemd-udev will be used instead of initrd
         "rd.systemd.show_status=auto"
+
         # lower the udev log level to show only errors or worse
         "rd.udev.log_level=3"
+
         # disable the cursor in vt to get a black screen during intermissions
         # TODO turn back on in tty
         "vt.global_cursor_default=0"
@@ -142,7 +170,6 @@ in {
           "xhci_pci"
           "ahci"
           "btrfs"
-          "kvm-intel"
           "sd_mod"
           "dm_mod"
           "usb_storage"
