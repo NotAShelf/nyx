cf11b7b37bc8e0216a454f7454332d1ea6426a74
NotAShelf <itsashelf@gmail.com>
2023-03-24 02:24:19 +0300

    dev: possibly fix direnv cache location?

--- home/notashelf/terminal/shell/nix-shell.nix
+++ home/notashelf/terminal/shell/nix-shell.nix
@@ -18,19 +18,10 @@
 
   programs.direnv = {
     enable = true;
-    nix-direnv.enable = true;
-    enableZshIntegration = true;
+    nix-direnv = {
+      enable = true;
+    };
 
-    stdlib = ''
-      # https://github.com/direnv/direnv/wiki/Customizing-cache-location
-      : ''${XDG_CACHE_HOME:=$HOME/.cache}
-      declare -A direnv_layout_dirs
-      direnv_layout_dir() {
-          echo "''${direnv_layout_dirs[$PWD]:=$(
-              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-              echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
-          )}"
-      }
-    '';
+    enableZshIntegration = true;
   };
 }
