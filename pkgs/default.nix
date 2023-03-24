bd189669625e4e7a79ffe4a8a67cff4415da6347
NotAShelf <itsashelf@gmail.com>
2023-03-24 13:18:10 +0300

    feat: remove swww from system overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,10 +9,10 @@
   rofi-emoji-wayland = pkgs.callPackage ./rofi-emoji-wayland.nix {};
 
   # My personal derivations for packages that are not on nixpkgs
-  nicksfetch = pkgs.callPackage ./nicksfetch.nix {};
   fastfetch = pkgs.callPackage ./overlays/fastfetch {};
+
+  nicksfetch = pkgs.callPackage ./nicksfetch.nix {};
   cloneit = pkgs.callPackage ./cloneit.nix {};
-  swww = pkgs.callPackage ./swww.nix {};
   discordo = pkgs.callPackage ./discordo.nix {};
   proton-ge = pkgs.callPackage ./proton-ge.nix {};
 }
