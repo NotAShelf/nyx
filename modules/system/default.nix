26bdc62f9f05829672271af16eb96d246cc0c865
NotAShelf <itsashelf@gmail.com>
2023-03-28 14:09:19 +0300

    feat(system): cleanup

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -3,11 +3,12 @@ _: {
     # module is the skeleton that adds all nixos options for the rest of the imports
     ./module
 
-    ./display # display protocol
+    ./display # display protocol (wayland/xorg)
     ./fs # filesystem support options
     ./hardware # hardware - i.e bluetooth, sound, tpm etc.
     ./media # enable multimedia
-    ./type # device type and associated module definitions
     ./networking # tcp optimizations
+
+    ./nixos # modules used to extend nixos
   ];
 }
