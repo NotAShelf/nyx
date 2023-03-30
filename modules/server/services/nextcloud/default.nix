bf41c30ebefce1dc74e2f2c3a78072a45d91bb75
NotAShelf <itsashelf@gmail.com>
2023-03-31 01:20:00 +0300

    feat(server): nextcloud service

---
+++ modules/server/services/nextcloud/default.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  domain = "cloud.notashelf.dev";
+
+  device = config.modules.device;
+  #cfg = config.modules.programs.override;
+  acceptedTypes = ["server" "hybrid"];
+in {
+  config = {
+    services.nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud26;
+      hostName = domain;
+      nginx.recommendedHttpHeaders = true;
+      enableBrokenCiphersForSSE = false;
+      https = true;
+      autoUpdateApps.enable = true;
+      config = {
+        dbtype = "pgsql";
+        dbhost = "/run/postgresql";
+        dbpassFile = config.age.secrets.nextcloud-db-pass.path;
+        adminpassFile = config.age.secrets.nextcloud-admin-root-pass.path;
+        adminuser = "admin-root";
+        defaultPhoneRegion = "IL";
+      };
+    };
+
+    services.postgresql = {
+      enable = true;
+      ensureDatabases = [config.services.nextcloud.config.dbname];
+      ensureUsers = [
+        {
+          name = config.services.nextcloud.config.dbuser;
+          ensurePermissions."DATABASE ${config.services.nextcloud.config.dbname}" = "ALL PRIVILEGES";
+        }
+      ];
+    };
+
+    # Ensure nextcloud does not start before its database
+    systemd.services."nextcloud-setup" = {
+      requires = ["postgresql.service"];
+      after = ["postgresql.service"];
+    };
+
+    services.nginx = {
+      enable = true;
+      virtualHosts.${domain} = {
+        forceSSL = true;
+        enableACME = true;
+      };
+    };
+
+    /*
+    age.secrets = {
+      nextcloud-db-pass = {
+        file = "${inputs.self}/secrets/nextcloud-db-pass.age";
+        group = "nextcloud";
+        owner = "nextcloud";
+      };
+      nextcloud-admin-root-pass = {
+        file = "${inputs.self}/secrets/nextcloud-admin-root-pass.age";
+        group = "nextcloud";
+        owner = "nextcloud";
+      };
+    };
+    */
+  };
+}
