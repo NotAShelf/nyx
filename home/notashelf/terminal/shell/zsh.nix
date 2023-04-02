7b74729a1ea3e1c3921e0451f8158cc909efa380
NotAShelf <itsashelf@gmail.com>
2023-04-02 12:27:22 +0300

    feat(zsh): more vim-mode binds

--- home/notashelf/terminal/shell/zsh.nix
+++ home/notashelf/terminal/shell/zsh.nix
@@ -161,6 +161,9 @@ in {
       bindkey -M menuselect 'l' vi-forward-char
       bindkey -M menuselect 'j' vi-down-line-or-history
 
+      bindkey "^A" vi-beginning-of-line
+      bindkey "^E" vi-end-of-line
+
 
 
       # If this is an xterm set the title to user@host:dir
