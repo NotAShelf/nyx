f6eee702b95051318855994ad98392a8512b418e
NotAShelf <itsashelf@gmail.com>
2023-04-02 12:14:44 +0300

    feat: mpd discord rpc & yams

--- home/notashelf/services/media/mpd/default.nix
+++ home/notashelf/services/media/mpd/default.nix
@@ -1,8 +1,25 @@
 {
   config,
   pkgs,
+  osConfig,
   ...
-}: {
+}: let
+  username = osConfig.modules.system.username;
+in {
+  # yams service
+  systemd.user.services.yams = {
+    Unit = {
+      Description = "Last.FM scrobbler for MPD";
+      After = ["mpd.service"];
+    };
+    Service = {
+      ExecStart = "${pkgs.yams}/bin/yams -N";
+      Environment = "NON_INTERACTIVE=1";
+      Restart = "always";
+    };
+    Install.WantedBy = ["default.target"];
+  };
+
   services = {
     playerctld.enable = true;
     mpris-proxy.enable = true;
@@ -21,76 +38,97 @@
         auto_update "yes"
       '';
     };
-  };
 
-  home.packages = with pkgs; [playerctl mpc_cli];
+    mpd-discord-rpc = {
+      enable = true;
+      settings = {
+        format = {
+          details = "$title";
+          state = "$artist";
+          large_text = "$album";
+          small_image = "";
+        };
+      };
+    };
 
-  services.mpdris2 = {
-    enable = true;
-    notifications = true;
-    mpd = {
-      host = "127.0.0.1";
+    mpdris2 = {
+      enable = true;
+      notifications = true;
+      mpd = {
+        host = "127.0.0.1";
+      };
     };
   };
+
+  home.packages = with pkgs; [
+    playerctl # CLI interface for playerctld
+    mpc_cli # control mpd through the CLI
+    cava # music visualizer
+    spek # spectogram analyzer
+  ];
 
   programs.ncmpcpp = {
     enable = true;
     package = pkgs.ncmpcpp;
-    mpdMusicDir = "/home/notashelf/Media/Music";
+    mpdMusicDir = "/home/${username}/Media/Music";
     settings = {
-      ncmpcpp_directory = "/home/notashelf/.config/ncmpcpp";
-      mpd_crossfade_time = 2;
-      lyrics_directory = "/home/notashelf/.cache/lyrics";
-      progressbar_look = "▃▃▃";
-      progressbar_elapsed_color = 5;
-      progressbar_color = "black";
-      media_library_primary_tag = "album_artist";
-      follow_now_playing_lyrics = "yes";
-      connected_message_on_startup = "no";
-      ignore_leading_the = "yes";
-      screen_switcher_mode = "playlist, media_library";
-      song_columns_list_format = "(50)[]{t|fr:Title} (0)[blue]{a}";
-      song_list_format = "$8%a - %t$R  %l";
-      song_library_format = "{{%a - %t} (%b)}|{%f}";
-      song_status_format = "$7%t";
-      song_window_title_format = "Now Playing ..";
-      now_playing_prefix = "$b$6 ";
-      now_playing_suffix = "  $/b$8";
-      current_item_prefix = "$b$6$/b$6 ";
-      current_item_suffix = "  $8";
-      statusbar_color = "white";
-      color1 = "white";
-      color2 = "blue";
-      header_visibility = "no";
-      statusbar_visibility = "no";
-      titles_visibility = "no";
-      enable_window_title = "yes";
-      cyclic_scrolling = "yes";
-      mouse_support = "yes";
-      mouse_list_scroll_whole_page = "yes";
-      lines_scrolled = "1";
-      message_delay_time = "1";
-      playlist_shorten_total_times = "yes";
-      playlist_display_mode = "columns";
-      browser_display_mode = "columns";
-      search_engine_display_mode = "columns";
-      playlist_editor_display_mode = "columns";
+      # Miscelaneous
+      ncmpcpp_directory = "${config.home.homeDirectory}/.config/ncmpcpp";
+      ignore_leading_the = true;
+      external_editor = "nvim";
+      message_delay_time = 1;
+      playlist_disable_highlight_delay = 2;
       autocenter_mode = "yes";
       centered_cursor = "yes";
-      user_interface = "classic";
-      locked_screen_width_part = "50";
-      ask_for_locked_screen_width_part = "yes";
-      display_bitrate = "no";
-      external_editor = "nvim";
-      main_window_color = "default";
-      startup_screen = "playlist";
+      allow_for_physical_item_deletion = "no";
+      lines_scrolled = "0";
+      follow_now_playing_lyrics = "yes";
+      lyrics_fetchers = "musixmatch";
+
+      # visualizer
       visualizer_data_source = "/tmp/mpd.fifo";
-      visualizer_output_name = "Visualizer";
-      visualizer_in_stereo = "no";
+      visualizer_output_name = "mpd_visualizer";
       visualizer_type = "ellipse";
-      visualizer_fps = "60";
-      visualizer_look = "●▮";
-      visualizer_color = "33,39,63,75,81,99,117,153,189";
+      visualizer_look = "●● ";
+      visualizer_color = "blue, green";
+
+      # appearance
+      colors_enabled = "yes";
+      playlist_display_mode = "classic";
+      user_interface = "classic";
+      volume_color = "white";
+
+      # window
+      song_window_title_format = "Music";
+      statusbar_visibility = "no";
+      header_visibility = "no";
+      titles_visibility = "no";
+      # progress bar
+      progressbar_look = "‎‎‎";
+      progressbar_color = "black";
+      progressbar_elapsed_color = "blue";
+
+      # song list
+      song_status_format = "$7%t";
+      song_list_format = "$(008)%t$R  $(247)%a$R$5  %l$8";
+      song_columns_list_format = "(53)[blue]{tr} (45)[blue]{a}";
+
+      current_item_prefix = "$b$2| ";
+      current_item_suffix = "$/b$5";
+
+      now_playing_prefix = "$b$5| ";
+      now_playing_suffix = "$/b$5";
+
+      song_library_format = "{{%a - %t} (%b)}|{%f}";
+
+      # colors
+      main_window_color = "blue";
+
+      current_item_inactive_column_prefix = "$b$5";
+      current_item_inactive_column_suffix = "$/b$5";
+
+      color1 = "white";
+      color2 = "blue";
     };
     bindings = [
       {
