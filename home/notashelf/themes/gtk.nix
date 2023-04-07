0a2ad093f2a3b7294a28cdb43a6d9cdf963726ff
NotAShelf <itsashelf@gmail.com>
2023-04-07 14:55:55 +0300

    dev: try to add Nextcloud to kvantum managed apps

--- home/notashelf/themes/gtk.nix
+++ home/notashelf/themes/gtk.nix
@@ -63,6 +63,6 @@
     theme=catppuccin
 
     [Applications]
-    catppuccin=qt5ct, org.kde.dolphin, org.kde.kalendar, org.qbittorrent.qBittorrent, hyprland-share-picker, dolphin-emu
+    catppuccin=qt5ct, org.kde.dolphin, org.kde.kalendar, org.qbittorrent.qBittorrent, hyprland-share-picker, dolphin-emu, Nextcloud
   '';
 }
