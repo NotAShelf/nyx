0895406b96a31825719d37012f5ab403cb639ece
NotAShelf <itsashelf@gmail.com>
2023-04-10 23:38:54 +0300

    feat: add tailscale

--- hosts/enyo/system.nix
+++ hosts/enyo/system.nix
@@ -31,6 +31,7 @@ with lib; {
 
         networking = {
           optimizeTcp = true;
+          useTailscale = true;
         };
       };
       usrEnv = {
---
+++ modules/core/system/tailscale.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  sys = config.modules.system.networking;
+in {
+  config = mkIf (sys.useTailscale) {
+    # make the tailscale command usable to users
+    environment.systemPackages = [pkgs.tailscale];
+
+    # enable tailscale, inter-machine VPN service
+    services.tailscale.enable = true;
+  };
+}
