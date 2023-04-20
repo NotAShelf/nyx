27eb354f3fc3459b58f3d8d679ab2f42bf11c456
NotAShelf <itsashelf@gmail.com>
2023-04-20 12:52:12 +0300

    feat: move programs.zsh to core

--- modules/core/system/programs.nix
+++ modules/core/system/programs.nix
@@ -12,5 +12,8 @@
     # less pager
     # TODO: package moar for nix
     less.enable = true;
+
+    # this needs to be enabled globally to be able to use zsh as a login shell
+    zsh.enable = true;
   };
 }
