e1ecf8a2989be0bb2f18c27f3d1a914a9a2c1cdb
NotAShelf <itsashelf@gmail.com>
2023-04-21 01:17:56 +0300

    fix: helios secrets

--- hosts/helios/ragenix.nix
+++ hosts/helios/ragenix.nix
@@ -10,4 +10,3 @@
     group = "users";
   };
 }
-
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,8 +3,9 @@ let
   notashelf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABG2T60uEoq4qTZtAZfSBPtlqWs2b4V4O+EptQ6S/ru notashelf@prometheus";
 
   # hosts
-  helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGsKD35Pdl6MauuhbvfH3p7XM00W5GG5VQXyL+j2dtUn notashelf@helios";
+  helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOznQQTpblKWT0p5RyVPyq3BZFRimYC+5FiBJHuLeRV";
 in {
   "spotify.age".publicKeys = [notashelf];
   "wireguard.age".publicKeys = [helios notashelf];
 }
+
