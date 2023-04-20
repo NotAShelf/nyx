f6c7740a869eb59761587fcaac0e9298dec51e3d
NotAShelf <itsashelf@gmail.com>
2023-04-20 12:49:09 +0300

    feat: encrypt wireguard secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,11 @@
 let
-  notashelf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABG2T60uEoq4qTZtAZfSBPtlqWs2b4V4O+EptQ6S/ru notashelf@prometheus";
-  hosts = ["enyo" "epimetheus" "icarus"];
+  # users
+  notashelf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABG2T60uEoq4qTZtAZfSBPtlqWs2b4V4O+EptQ6S/ru notashelf@prometheus
+";
+
+  # hosts
+  helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB6M56Tp63WfuvqlL3ek3K5T0+jVC7c+nV5NfqrJM5EM root@nixos";
 in {
-  "spotify.age".publicKeys = [notashelf hosts];
-  "wireguard.age".publicKeys = [notashelf hosts];
+  "spotify.age".publicKeys = [notashelf];
+  "wireguard.age".publicKeys = [helios notashelf];
 }
