3f925c20ffbaf4cbb59472d91463f19738d7891a
NotAShelf <itsashelf@gmail.com>
2023-04-21 00:50:16 +0300

    feat: refactor wireguard

--- hosts/helios/ragenix.nix
+++ hosts/helios/ragenix.nix
@@ -10,3 +10,4 @@
     group = "users";
   };
 }
+
--- modules/server/services/wireguard/server.nix
+++ modules/server/services/wireguard/server.nix
@@ -1,58 +1,32 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }: {
-  # enable NAT
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
-  networking.nat.internalInterfaces = ["wg0"];
-  networking.firewall = {
-    allowedUDPPorts = [51820];
-  };
-
-  networking.wireguard = {
-    enable = true;
-    interfaces = {
-      # "wg0" is the network interface name. You can name the interface arbitrarily.
-      wg0 = {
-        # Determines the IP address and subnet of the server's end of the tunnel interface.
-        ips = ["10.100.0.1/24"];
-
-        # The port that WireGuard listens to. Must be accessible by the client.
-        listenPort = 51820;
-
-        # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-        # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-        postSetup = ''
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-        '';
-
-        # This undoes the above command
-        postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-        '';
-
-        # Path to the private key file.
-        privateKeyFile = config.age.secrets.wireguard.path;
-
-        peers = [
-          # List of allowed peers.
-          {
-            # Feel free to give a meaning full name
-            # Public key of the peer (not a file path).
-            publicKey = "{client public key}";
-            # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-            allowedIPs = ["10.100.0.2/32"];
-          }
-          {
-            # John Doe
-            publicKey = "{john doe's public key}";
-            allowedIPs = ["10.100.0.3/32"];
-          }
-        ];
-      };
+  networking = {
+    nat = {
+      enable = true;
+      externalInterface = "enp6s0";
+      internalInterfaces = ["wg0"];
+    };
+    wireguard.interfaces.wg0 = {
+      ips = ["10.100.0.1/24" "fdc9:281f:04d7:9ee9::1/64"];
+      listenPort = 51820;
+      privateKeyFile = config.age.secrets.wireguard.path;
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+      peers = [
+        {
+          # desktop
+          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAYCaA6JEnTt2BI6MJn8t2Qc3E45ARZua1VWhQpSPQi root@enyo
+";
+          allowedIPs = ["10.100.0.2/32"];
+        }
+      ];
     };
   };
 }
