e311586d2a848c4a14cdc65a185cd19a671cd7c7
NotAShelf <itsashelf@gmail.com>
2023-04-20 16:01:44 +0300

    feat(helios): new hardware for helios

--- hosts/helios/hardware-configuration.nix
+++ hosts/helios/hardware-configuration.nix
@@ -1,10 +1,40 @@
-{modulesPath, ...}: {
-  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-  boot.loader.grub.device = "/dev/sda";
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
-  boot.initrd.kernelModules = ["nvme"];
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = ["ata_piix" "virtio_pci" "virtio_scsi" "xhci_pci" "sd_mod" "sr_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
   fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
+    device = "/dev/disk/by-uuid/388b6f75-793e-4385-89cb-8d97820d7378";
+    fsType = "btrfs";
   };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/7419-AB2C";
+    fsType = "vfat";
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/22dcda3c-10c7-4dc5-8535-f118a18c1f79";}
+  ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
