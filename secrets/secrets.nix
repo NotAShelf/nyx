07463c0d8b930f18df0584bc034267fd50bad616
NotAShelf <itsashelf@gmail.com>
2023-04-21 05:07:51 +0300

    feat: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,13 @@
 let
   # users
-  notashelf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABG2T60uEoq4qTZtAZfSBPtlqWs2b4V4O+EptQ6S/ru notashelf@prometheus";
+  notashelf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAYCaA6JEnTt2BI6MJn8t2Qc3E45ARZua1VWhQpSPQi";
 
   # hosts
   helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILxe8T2Bo3dbQbszpKXDo1P6r4VIdjSdvlzPw16VRKe1";
 
   shared = [notashelf];
 in {
-  "spotify.age".publicKeys = [notashelf helios];
+  "spotify.age".publicKeys = [notashelf];
   "wireguard.age".publicKeys = [helios] ++ shared;
+  "wg-client.age".publicKeys = [notashelf];
 }
