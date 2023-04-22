75489b355c5c527a249e9e930e06f0d0ee0e0d3b
NotAShelf <itsashelf@gmail.com>
2023-04-22 12:02:14 +0300

    dev: test custom ifTheyExist function

--- lib/default.nix
+++ lib/default.nix
@@ -8,10 +8,16 @@ with lib;
     final: prev: {
       # filter files that have the .nix suffix
       filterNixFiles = k: v: v == "regular" && hasSuffix ".nix" k;
+
       # import files that are selected by filterNixFiles
       importNixFiles = path:
         (lists.forEach (mapAttrsToList (name: _: path + ("/" + name))
             (filterAttrs filterNixFiles (builtins.readDir path))))
         import;
+
+      # a function that checks for the existence of a group
+      # TODO: this function needs config to be inherited in flake.nix, but config is not a thing in the flake
+      # maybe import this lib elsewhere?
+      # ifTheyExist = config: groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
     }
   )
