db22c9a850486ba44cb36a0ae207edae3c758408
NotAShelf <itsashelf@gmail.com>
2023-04-26 18:41:39 +0300

    style: formatting

--- hosts/enyo/ragenix.nix
+++ hosts/enyo/ragenix.nix
@@ -3,8 +3,7 @@
   self,
   ...
 }: {
-  
-  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  age.identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
   age.secrets.spotify = {
     file = "${self}/secrets/spotify.age";
