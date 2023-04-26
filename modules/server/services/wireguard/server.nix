96c007bf981f56d657cae75aa4fb4a06cdd0e05c
NotAShelf <itsashelf@gmail.com>
2023-04-26 20:47:56 +0300

    style: formatting

--- modules/server/services/wireguard/server.nix
+++ modules/server/services/wireguard/server.nix
@@ -6,37 +6,38 @@
   networking.firewall = {
     allowedUDPPorts = [51820];
   };
+
   # Wireguard Server Peer Setup
   networking.wireguard = {
     enable = true;
     interfaces = {
-    wg1 = {
-      # General settings
-      privateKeyFile = config.age.secrets.wg-server.path;
-      listenPort = 51820;
+      wg1 = {
+        # General settings
+        privateKeyFile = config.age.secrets.wg-server.path;
+        listenPort = 51820;
 
-      # IPs
-      ips = [
-        "10.255.255.10/24" # v4 general
-        "10.255.255.254/24" # v4 adtl
-        "fe80::10/64" # v6 link local
-        "2a01:4f9:c010:2cf9:f::10/80" #v6 general
-        "2a01:4f9:c010:2cf9:f::ffff/80" #v6 adtl
-      ];
+        # IPs
+        ips = [
+          "10.255.255.10/24" # v4 general
+          "10.255.255.254/24" # v4 adtl
+          "fe80::10/64" # v6 link local
+          "2a01:4f9:c010:2cf9:f::10/80" #v6 general
+          "2a01:4f9:c010:2cf9:f::ffff/80" #v6 adtl
+        ];
 
-      # Peers
-      peers = [
-        # enyo
-        {
-          allowedIPs = [
-            "10.255.255.11/32"
-            "fe80::11/128"
-            "2a01:4f9:c010:2cf9:f::11/128"
-          ];
-          publicKey = "u5Riuu4NEWEH06qATdnrPO+LacZTspoghqMnoWQ+uEQ=";
-        }
-      ];
-    };
+        # Peers
+        peers = [
+          # enyo
+          {
+            allowedIPs = [
+              "10.255.255.11/32"
+              "fe80::11/128"
+              "2a01:4f9:c010:2cf9:f::11/128"
+            ];
+            publicKey = "u5Riuu4NEWEH06qATdnrPO+LacZTspoghqMnoWQ+uEQ=";
+          }
+        ];
+      };
     };
   };
 }
