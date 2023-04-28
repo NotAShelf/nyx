282aaee249895d3cc9f7cca4423751897b6eca33
NotAShelf <itsashelf@gmail.com>
2023-04-28 23:23:01 +0300

    bootloader: display plymouth animations on suspend hooks

--- modules/desktop/bootloader/plymouth.nix
+++ modules/desktop/bootloader/plymouth.nix
@@ -4,7 +4,16 @@
   pkgs,
   ...
 }: {
+  # enable plymouth with default theme
   boot.plymouth.enable = true;
+
+  # make plymouth work with sleep
+  powerManagement.powerDownCommands = ''
+    ${pkgs.plymouth} --show-splash
+  '';
+  powerManagement.resumeCommands = ''
+    ${pkgs.plymouth} --quit
+  '';
 
   # configure plymouth theme
   # <https://github.com/adi1090x/plymouth-themes>
