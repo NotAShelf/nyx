c564f89d46629992022cac8fd4177b3f23d21785
NotAShelf <itsashelf@gmail.com>
2023-05-02 14:41:01 +0300

    filesystems: support for additional filesystems

--- modules/system/fs/default.nix
+++ modules/system/fs/default.nix
@@ -14,7 +14,7 @@ in {
 
       # this fixes initrd.systemd.enable for whatever reason
       boot = {
-        supportedFilesystems = mkForce ["btrfs"];
+        supportedFilesystems = ["btrfs"];
         initrd = {
           supportedFilesystems = ["btrfs"];
         };
@@ -23,21 +23,30 @@ in {
 
     (mkIf (builtins.elem "ext4" sys.fs) {
       boot = {
-        supportedFilesystems = mkForce ["ext4"];
+        supportedFilesystems = ["ext4"];
         initrd = {
           supportedFilesystems = ["ext4"];
         };
       };
     })
 
+    (mkIf (builtins.elem "exfat" sys.fs) {
+      boot = {
+        supportedFilesystems = ["exfat"];
+        initrd = {
+          supportedFilesystems = ["exfat"];
+        };
+      };
+    })
+
     (mkIf (builtins.elem "zfs" sys.fs) {
-      # TODO
+      # TODO: zfs needs a lot of options to be enabled, perhaps this could be its own separate module once I start using zfs for basically anything
     })
 
     # accept both ntfs and ntfs3 as valid values
     (mkIf ((builtins.elem "ntfs" sys.fs) || (builtins.elem "ntfs3" sys.fs)) {
       boot = {
-        supportedFilesystems = mkForce ["ntfs"];
+        supportedFilesystems = ["ntfs"];
       };
     })
   ];
