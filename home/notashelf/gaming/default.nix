7d2256142b1e3a4864c0f55622a5b3612ccbbc0c
NotAShelf <itsashelf@gmail.com>
2023-05-04 23:31:05 +0300

    home: separate minecraft from gaming/games

--- home/notashelf/gaming/default.nix
+++ home/notashelf/gaming/default.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     ./games
+    ./mangohud
     ./steam
     ./obs
   ];
--- home/notashelf/gaming/games/minecraft/default.nix
+++ home/notashelf/gaming/games/minecraft/default.nix
@@ -16,6 +16,8 @@ with lib; let
   };
 in {
   config = mkIf ((builtins.elem device.type acceptedTypes) && (programs.gaming.enable)) {
+    programs.mangohud.settings = {};
+
     home = {
       # copy the catppuccin theme to the themes directory of PrismLauncher
       file.".local/share/PrismLauncher/themes/mocha" = {
@@ -24,17 +26,11 @@ in {
       };
 
       packages = with pkgs; [
-        gamescope
-        legendary-gl
-        mono
-        winetricks
-        mangohud
-        lutris
-        dolphin-emu # cool emulator
-        yuzu # switch emulator
-        taisei # open-source touhou fan game
-        # get dotnet runtime 6 - needed by terraria
-        dotnet-runtime_6
+        # the successor to polyMC, which is now mostly abandoned
+        #prismlauncher # FIXME: some nixpkgs issue, again
+
+        # jre 17 - needed by newer minecraft versions
+        temurin-jre-bin-17
       ];
     };
   };
