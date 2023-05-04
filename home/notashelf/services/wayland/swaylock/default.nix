9879b449087f84cf87282c8eaba1a46fa1ab0faa
NotAShelf <itsashelf@gmail.com>
2023-05-04 23:33:15 +0300

    wayland: clean-up swayidle artifacts in swaylock module

--- home/notashelf/services/wayland/swaylock/default.nix
+++ home/notashelf/services/wayland/swaylock/default.nix
@@ -57,32 +57,5 @@ in {
         ignore-empty-password = true;
       };
     };
-
-    services.swayidle = {
-      enable = true;
-      events = [
-        {
-          event = "before-sleep";
-          command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-        }
-        {
-          event = "lock";
-          command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-        }
-      ];
-      timeouts = [
-        {
-          timeout = 300;
-          command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
-          resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
-        }
-        {
-          timeout = 310;
-          command = "${pkgs.systemd}/bin/loginctl lock-session";
-        }
-      ];
-    };
-
-    systemd.user.services.swayidle.Install.WantedBy = lib.mkForce ["hyprland-session.target"];
   };
 }
