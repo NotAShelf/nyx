71eb614ff17f355048d2b03abca375e036f9bb7e
NotAShelf <itsashelf@gmail.com>
2023-05-05 00:11:30 +0300

    home: clean-up shell configurations

--- home/notashelf/terminal/shell/tools/services.nix
+++ home/notashelf/terminal/shell/tools/services.nix
@@ -1,10 +1,16 @@
 {...}: {
   services = {
     udiskie.enable = true;
+
     gpg-agent = {
       enable = true;
-      pinentryFlavor = "gnome3";
+      pinentryFlavor = "gnome3"; # requires services.dbus.packages = [ pkgs.gcr ]
       enableSshSupport = true;
+      defaultCacheTtl = 1209600;
+      defaultCacheTtlSsh = 1209600;
+      maxCacheTtl = 1209600;
+      maxCacheTtlSsh = 1209600;
+      extraConfig = "allow-preset-passphrase";
       enableZshIntegration = true;
     };
   };
