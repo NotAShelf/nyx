1ad291f54c610e7974bb806ec267a91961fab363
NotAShelf <itsashelf@gmail.com>
2023-05-05 00:00:04 +0300

    ssh: commonIdFile

--- home/notashelf/terminal/shell/ssh.nix
+++ home/notashelf/terminal/shell/ssh.nix
@@ -5,7 +5,9 @@ _: {
       enable = true;
       hashKnownHosts = true;
       compression = true;
-      matchBlocks = {
+      matchBlocks = let
+        commonIdFile = "~/.ssh/id_ed25519";
+      in {
         "aur" = {
           hostname = "aur.archlinux.org";
           identityFile = "~/.ssh/aur";
