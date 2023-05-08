8856af3f6ab360312299d1c4f6a5d355b536e0f1
NotAShelf <itsashelf@gmail.com>
2023-05-08 18:34:02 +0300

    flake: provide pinned nixpkgs for broken nerdfonts

--- flake.nix
+++ flake.nix
@@ -12,7 +12,10 @@
       # ... add more systems as they are used
     ];
 
+    # map each system from supportedSystems
     forSystemEach = nixpkgs.lib.genAttrs supportedSystems;
+
+    # gen pkgs for each mapped supportedSystem
     forPkgsEach = f: forSystemEach (system: f nixpkgs.legacyPackages.${system});
 
     # extended nixpkgs lib, contains my custom functions
@@ -59,27 +62,7 @@
 
     # build against nixos unstable, more variants can be added if deemed necessary
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # impermanence
-    impermanence.url = "github:nix-community/impermanence";
-
-    # secure-boot on nixos
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # my personal neovim-flake
-    neovim-flake = {
-      url = "github:notashelf/neovim-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    helix = {
-      url = "github:SoraTenshi/helix/new-daily-driver";
-      inputs.rust-overlay.follows = "rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs-pinned.url = "github:NixOS/nixpkgs/b610c60e23e0583cdc1997c54badfd32592d3d3e";
 
     # Automated, pre-built packages for Wayland
     nixpkgs-wayland = {
@@ -91,7 +74,10 @@
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
     # Easy color integration
-    nix-colors.url = "github:misterio77/nix-colors";
+    nix-colors = {
+      url = "github:misterio77/nix-colors";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Nix gaming packages
     nix-gaming.url = "github:fufexan/nix-gaming";
@@ -102,6 +88,30 @@
     # sops-nix for atomic secrets management
     sops-nix = {
       url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # my personal neovim-flake
+    neovim-flake = {
+      url = "github:notashelf/neovim-flake";
+      inputs.nixpkgs.follows = "nixpkgs-pinned";
+    };
+
+    # use my own arrpc-flake to provide arRPC package
+    arrpc = {
+      url = "github:notashelf/arrpc-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    helix = {
+      url = "github:SoraTenshi/helix/new-daily-driver";
+      inputs.rust-overlay.follows = "rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -131,14 +141,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Home Manager
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    # impermanence
+    impermanence.url = "github:nix-community/impermanence";
+
+    # secure-boot on nixos
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # use my own arrpc-flake to provide arRPC package
-    arrpc.url = "github:notashelf/arrpc-flake";
 
     # Rust overlay
     rust-overlay = {
