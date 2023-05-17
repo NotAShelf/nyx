3aedace0bd35874adbeb3bfe2160a576b4cddae5
NotAShelf <itsashelf@gmail.com>
2023-05-17 09:25:09 +0300

    hosts/epimetheus: useDHPC by default

--- hosts/epimetheus/hardware-configuration.nix
+++ hosts/epimetheus/hardware-configuration.nix
@@ -64,9 +64,9 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networkingnetworking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
