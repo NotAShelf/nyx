c900f53a19151c5e79a3b615333dbbffb1a06a1d
NotAShelf <itsashelf@gmail.com>
2023-05-20 19:56:00 +0300

    home/services/shared: use systemd user services for nextcloud

--- home/notashelf/services/shared/cloud/default.nix
+++ home/notashelf/services/shared/cloud/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   osConfig,
+  pkgs,
   ...
 }:
 with lib; let
@@ -11,9 +12,22 @@ with lib; let
   acceptedTypes = ["desktop" "laptop" "lite" "hybrid"];
 in {
   config = mkIf ((builtins.elem device.type acceptedTypes) && (video.enable && env.isWayland)) {
+    /*
     services = {
       nextcloud-client.enable = true;
       nextcloud-client.startInBackground = true;
+    };
+    */
+
+    home.packages = [pkgs.nextcloud-client];
+
+    systemd.user.services.nextcloud = lib.mkGraphicalService {
+      Unit.Description = "Nextcloud client service";
+      Service = {
+        ExecStart = "${pkgs.nextcloud-client}/bin/nextcloud --background";
+        Restart = "always";
+        Slice = "background.slice";
+      };
     };
   };
 }
