441a8c09593988b36d9dd0a6dbab3de812e4991c
NotAShelf <itsashelf@gmail.com>
2023-05-21 07:57:33 +0300

    hosts/helios: move to new hardware

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   notashelf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABG2T60uEoq4qTZtAZfSBPtlqWs2b4V4O+EptQ6S/ru";
 
-  helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILxe8T2Bo3dbQbszpKXDo1P6r4VIdjSdvlzPw16VRKe1";
+  helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8XojSEerAwKwXUPIZASZ5sXPPT7v/26ONQcH9zIFK+";
   enyo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAYCaA6JEnTt2BI6MJn8t2Qc3E45ARZua1VWhQpSPQi";
 in {
   "spotify.age".publicKeys = [enyo notashelf];
