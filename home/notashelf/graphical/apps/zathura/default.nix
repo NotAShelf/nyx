d7a22b58a89787536d2eb4d6b95320b1cdd6a7d0
NotAShelf <itsashelf@gmail.com>
2023-05-27 22:42:04 +0300

    apps/zathura: get zathura colors from catppuccin repository

--- home/notashelf/graphical/apps/zathura/default.nix
+++ home/notashelf/graphical/apps/zathura/default.nix
@@ -12,53 +12,17 @@ with lib; let
   inherit (config.colorscheme) colors;
 in {
   config = mkIf (builtins.elem device.type acceptedTypes) {
+    xdg.configFile."zathura/catppuccin-mocha".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-mocha";
+      hash = "sha256-/HXecio3My2eXTpY7JoYiN9mnXsps4PAThDPs4OCsAk=";
+    };
+
     programs.zathura = {
       enable = true;
+      extraConfig = "include catppuccin-mocha";
+
       options = {
         font = "Iosevka 15";
-
-        default-fg = "${colors.base05}";
-        default-bg = "${colors.base00}";
-
-        completion-bg = "${colors.base02}";
-        completion-fg = "${colors.base05}";
-        completion-highlight-bg = "#575268";
-        completion-highlight-fg = "${colors.base05}";
-        completion-group-bg = "${colors.base02}";
-        completion-group-fg = "${colors.base0D}";
-
-        statusbar-fg = "${colors.base05}";
-        statusbar-bg = "${colors.base02}";
-        statusbar-h-padding = 10;
-        statusbar-v-padding = 10;
-
-        notification-bg = "${colors.base02}";
-        notification-fg = "${colors.base05}";
-        notification-error-bg = "${colors.base02}";
-        notification-error-fg = "${colors.base08}";
-        notification-warning-bg = "${colors.base02}";
-        notification-warning-fg = "#FAE3B0";
-        selection-notification = true;
-
-        inputbar-fg = "${colors.base05}";
-        inputbar-bg = "${colors.base02}";
-
-        recolor = true;
-        recolor-lightcolor = "${colors.base00}";
-        recolor-darkcolor = "${colors.base05}";
-
-        index-fg = "${colors.base05}";
-        index-bg = "${colors.base00}";
-        index-active-fg = "${colors.base05}";
-        index-active-bg = "${colors.base02}";
-
-        render-loading-bg = "${colors.base00}";
-        render-loading-fg = "${colors.base05}";
-
-        highlight-color = "#575268";
-        highlight-active-color = "#F4B8E4";
-        highlight-fg = "#F4B8E4";
-
         selection-clipboard = "clipboard";
         adjust-open = "best-fit";
         pages-per-row = "1";
