bf89204e31f4aa1c9e600df130cd8bbfeeba08b2
NotAShelf <itsashelf@gmail.com>
2023-06-02 13:19:23 +0300

    services/mkm: get docker image file from flake input

--- modules/extra/server/services/mkm/default.nix
+++ modules/extra/server/services/mkm/default.nix
@@ -13,54 +13,16 @@ in {
     containers = {
       "mkm-web" = mkIf (config.networking.hostName == "helios") {
         autoStart = true;
-        ports = ["3000:3001"];
+        ports = [
+          "3000:3001"
+          "3306:3306"
+        ];
         entrypoint = "next dev";
         workdir = "/tmp/mkm-web";
         extraOptions = ["--network=host"];
 
-        image = "mkm-web:latest";
-        imageFile = with pkgs;
-          dockerTools.buildImage {
-            config = {
-              #User = "nonroot";
-              Entrypoint = ["pnpm run dev"];
-            };
-
-            name = "mkm-web";
-            tag = "latest";
-
-            fromImage = "node:18-alpine";
-
-            copyToRoot = pkgs.buildEnv {
-              name = "image-root";
-              paths = [
-                inputs.mkm.packages.${pkgs.system}.default
-                pkgs.nodePackages.pnpm
-              ];
-              pathsToLink = [
-                "/mkm-web"
-                "/bin"
-              ];
-            };
-
-            /*
-            runAsRoot = ''
-              #!${pkgs.runtimeShell}
-              ${pkgs.dockerTools.shadowSetup}
-              mkdir /tmp
-              chmod 777 -R /tmp
-              mkdir -p /usr/bin
-              ln -s ${pkgs.coreutils}/bin/env /usr/bin/env
-              groupadd -r nonroot
-              useradd -r -g nonroot nonroot
-              mkdir -p /home/nonroot
-              chown nonroot:nonroot /home/nonroot
-            '';
-            */
-
-            #diskSize = 1024;
-            #buildVMMemorySize = 256;
-          };
+        image = "mkm-web";
+        imageFile = inputs.mkm.packages.${pkgs.system}.dockerImage;
       };
     };
   };
