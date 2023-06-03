ed16421658087ce6420fa92b78454ee0914d4fb6
NotAShelf <itsashelf@gmail.com>
2023-06-04 02:20:14 +0300

    modules/programs: seperate programs and services

--- modules/common/system/module/programs.nix
+++ modules/common/system/module/programs.nix
@@ -1,25 +1,13 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{lib, ...}:
 with lib; {
   # this module provides overrides for certain defaults and lets you set
   # default programs for referencing in other config files.
-  options.modules.programs = {
-    # "override" is a simple option that sets the programs' state to the oppossite of their default
-    override = {
-      # override basic desktop applications
-      program = {
-        # an example override for the libreoffice program
-        # if set to true, libreoffice module will not be enabled as it is by default
-        libreoffice = mkEnableOption "Override Libreoffice suite";
-      };
-
-      # override server-side services
-      service = {
+  options.modules = {
+    services = {
+      override = {
         nextcloud = mkEnableOption "Override Nextcloud service";
+        mailserver = mkEnableOption "Override nixos-mailserver service";
+        mkm = mkEnableOption "Override mkm-ticketing service";
         vaultwarden = mkEnableOption "Override VaultWarden service";
         gitea = mkEnableOption "Override Gitea service";
         grafana = mkEnableOption "Override Grafana and Prometheus services";
@@ -27,69 +15,79 @@ with lib; {
         jellyfin = mkEnableOption "Override Jellyfin media service";
         matrix = mkEnableOption "Override Matrix-synapse service";
         wireguard = mkEnableOption "Override Wireguard service";
+        database = {
+          mysql = mkEnableOption "Override MySQL database service";
+          mongodb = mkEnableOption "Override MongoDB service";
+        };
       };
     };
 
-    # TODO: turn those into overrides
-    # load GUI and CLI programs by default, but check if those overrides are enabled
-    # so that they can be disabled at will
-    cli = {
-      enable = mkEnableOption "Enable CLI programs";
-    };
-    gui = {
-      enable = mkEnableOption "Enable GUI programs";
-    };
-
-    gaming = {
-      enable = mkEnableOption "Enable packages required for the device to be gaming-ready";
-
-      emulation.enable = mkEnableOption "Enable programs required to emulate other platforms, such as Nintendo Switch";
-
-      chess.enable = mkEnableOption "Chess programs and engines";
-
-      gamescope = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Gamescope";
-      };
-    };
-
-    git = {
-      signingKey = mkOption {
-        type = types.str;
-        default = "";
-        description = "The default gpg key used for signing commits";
-      };
-    };
-
-    # default program options
-    default = {
-      # what program should be used as the default terminal
-      # do note this is NOT the command, but just the name. i.e setting footclient will
-      # not work because the program name will be references as "foot" in the rest of the config
-      terminal = mkOption {
-        type = types.enum ["foot" "kitty" "wezterm"];
-        default = "kitty";
+    programs = {
+      # "override" is a simple option that sets the programs' state to the oppossite of their default
+      override = {
+        # override basic desktop applications
+        # an example override for the libreoffice program
+        # if set to true, libreoffice module will not be enabled as it is by default
+        libreoffice = mkEnableOption "Override Libreoffice suite";
       };
 
-      fileManager = mkOption {
-        type = types.enum ["thunar" "dolphin" "nemo"];
-        default = "dolphin";
+      # TODO: turn those into overrides
+      # load GUI and CLI programs by default, but check if those overrides are enabled
+      # so that they can be disabled at will
+      cli = {
+        enable = mkEnableOption "Enable CLI programs";
+      };
+      gui = {
+        enable = mkEnableOption "Enable GUI programs";
       };
 
-      browser = mkOption {
-        type = types.enum ["firefox" "librewolf" "chromium"];
-        default = "firefox";
+      gaming = {
+        enable = mkEnableOption "Enable packages required for the device to be gaming-ready";
+
+        emulation.enable = mkEnableOption "Enable programs required to emulate other platforms";
+
+        chess.enable = mkEnableOption "Chess programs and engines";
+
+        gamescope.enable = mkEnableOption "Gamescope compositing manager" // {default = true;};
       };
 
-      editor = mkOption {
-        type = types.enum ["neovim" "helix" "emacs"];
-        default = "neovim";
+      git = {
+        signingKey = mkOption {
+          type = types.str;
+          default = "";
+          description = "The default gpg key used for signing commits";
+        };
       };
 
-      launcher = mkOption {
-        type = types.enum ["rofi" "wofi" "anyrun"];
-        default = "rofi";
+      # default program options
+      default = {
+        # what program should be used as the default terminal
+        # do note this is NOT the command, but just the name. i.e setting footclient will
+        # not work because the program name will be references as "foot" in the rest of the config
+        terminal = mkOption {
+          type = types.enum ["foot" "kitty" "wezterm"];
+          default = "kitty";
+        };
+
+        fileManager = mkOption {
+          type = types.enum ["thunar" "dolphin" "nemo"];
+          default = "dolphin";
+        };
+
+        browser = mkOption {
+          type = types.enum ["firefox" "librewolf" "chromium"];
+          default = "firefox";
+        };
+
+        editor = mkOption {
+          type = types.enum ["neovim" "helix" "emacs"];
+          default = "neovim";
+        };
+
+        launcher = mkOption {
+          type = types.enum ["rofi" "wofi" "anyrun"];
+          default = "rofi";
+        };
       };
     };
   };
