cb7bb60d53ba572cfd71a01a561ba90467feb3b6
NotAShelf <raf@notashelf.dev>
2023-06-21 12:02:51 +0300

    core/services: automatically clean up audit.log

--- modules/common/core/system/services.nix
+++ modules/common/core/system/services.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  lib,
+  config,
+  ...
+}: {
   services = {
     # monitor and control temparature
     thermald.enable = true;
@@ -10,8 +14,40 @@
     fwupd.enable = true;
     # limit systemd journal size
     journald.extraConfig = ''
-      SystemMaxUse=50M
-      RuntimeMaxUse=10M
+      SystemMaxUse=100M
+      RuntimeMaxUse=50M
     '';
   };
+
+  systemd =
+    {}
+    // lib.optionalAttrs (config.security.auditd.enable) {
+      # a systemd timer to clean /var/log/audit.log daily
+      # this can probably be weekly, but daily means we get to clean it every 2-3 days instead of once a week
+      timers."clean-audit-log" = {
+        description = "Periodically clean audit log";
+        wantedBy = ["timers.target"];
+        timerConfig = {
+          OnCalendar = "daily";
+          Persistent = true;
+        };
+      };
+
+      # clean audit log if it's more than 524,288,000 bytes, which is roughly 500 megabytes
+      # it can grow MASSIVE in size if left unchecked
+      services."clean-audit-log" = {
+        script = ''
+          set -eu
+          if [[ $(stat -c "%s" /var/log/audit/audit.log) -gt 524288000 ]]; then
+            echo "Clearing Audit Log";
+            rm -rvf /var/log/audit/audit.log;
+            echo "Done!"
+          fi
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+      };
+    };
 }
