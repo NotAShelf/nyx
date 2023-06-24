207fa926551b3c659246f0d5ed90a0d9ee1249fa
NotAShelf <raf@notashelf.dev>
2023-06-24 22:13:57 +0300

    services/nix-index: write to bin instead of shell script

--- home/notashelf/terminal/tools/services/nix-index.nix
+++ home/notashelf/terminal/tools/services/nix-index.nix
@@ -3,18 +3,18 @@
   lib,
   ...
 }: let
-  nix-index-update = pkgs.writeShellScript "nix-index-update-db" ''
+  nix-index-update = ''
     export filename="index-x86_64-$(uname | tr A-Z a-z)"
     mkdir -p ~/.cache/nix-index
     cd ~/.cache/nix-index
     # -N will only download a new version if there is an update.
-    wget -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+    wget -Nv https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
     ln -f $filename files
   '';
 in {
   home.packages = [
     pkgs.nix-index
-    nix-index-update # add it to packages so that we can invoke it manually
+    (pkgs.writeScriptBin "nix-index-update-db" nix-index-update) # add update script to packages so that we can invoke it manually
   ];
 
   # set up nix-index
@@ -33,7 +33,7 @@ in {
     };
     Service = {
       Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath [pkgs.wget pkgs.coreutils]}";
-      ExecStart = "${nix-index-update}";
+      ExecStart = "${pkgs.writeShellScript "nix-index-update-db" nix-index-update}";
     };
     Install.WantedBy = ["multi-user.target"];
   };
