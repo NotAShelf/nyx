0c9f4e9e5023fb415adca40fe7c1ef1e7f054b83
NotAShelf <raf@notashelf.dev>
2023-06-28 22:31:48 +0300

    launchers/anyrun: use the home-manager module

--- home/notashelf/graphical/launchers/anyrun/default.nix
+++ home/notashelf/graphical/launchers/anyrun/default.nix
@@ -10,60 +10,52 @@
   env = osConfig.modules.usrEnv;
   acceptedTypes = ["laptop" "desktop" "hybrid" "lite"];
 in {
+  imports = [inputs.anyrun.homeManagerModules.default];
   config = lib.mkIf (builtins.elem device.type acceptedTypes && env.isWayland) {
-    home.packages = [anyrun];
+    # home.packages = [anyrun];
 
-    xdg.configFile = {
-      "anyrun/config.ron".text = ''
-        Config(
-          // `width` and `vertical_offset` use an enum for the value it can be either:
-          // Absolute(n): The absolute value in pixels
-          // Fraction(n): A fraction of the width or height of the full screen (depends on exclusive zones and the settings related to them) window respectively
+    programs.anyrun = {
+      enable = true;
+      config = {
+        plugins = with inputs.anyrun.packages.${pkgs.system}; [
+          applications
+          rink
+          translate
+          randr
+          shell
+          symbols
+          translate
+        ];
 
-          // How wide the input box and results are.
-          width: Fraction(0.3),
+        # Where Anyrun is located on the screen: Top, Center
+        position = "top";
 
-          // Where Anyrun is located on the screen: Top, Center
-          position: Top,
+        # How much the runner is shifted vertically
+        verticalOffset.absolute = 15;
 
-          // How much the runner is shifted vertically
-          vertical_offset: Absolute(15),
+        # Hide match and plugin info icons
+        hideIcons = false;
 
-          // Hide match and plugin info icons
-          hide_icons: false,
+        # ignore exclusive zones, i.e. Waybar
+        ignoreExclusiveZones = false;
 
-          // ignore exclusive zones, f.e. Waybar
-          ignore_exclusive_zones: false,
+        # Layer shell layer: Background, Bottom, Top, Overlay
+        layer = "overlay";
 
-          // Layer shell layer: Background, Bottom, Top, Overlay
-          layer: Overlay,
+        # Hide the plugin info panel
+        hidePluginInfo = false;
 
-          // Hide the plugin info panel
-          hide_plugin_info: false,
+        # Close window when a click outside the main box is received
+        closeOnClick = false;
 
-          // Close window when a click outside the main box is received
-          close_on_click: true,
+        # Show search results immediately when Anyrun starts
+        showResultsImmediately = false;
 
-          // Show search results immediately when Anyrun starts
-          show_results_immediately: false,
+        # Limit amount of entries shown in total
+        maxEntries = null;
+      };
 
-          // Limit amount of entries shown in total
-          max_entries: None,
-
-          // List of plugins to be loaded by default, can be specified with a relative path to be loaded from the
-          // `<anyrun config dir>/plugins` directory or with an absolute path to just load the file the path points to.
-          plugins: [
-            "${anyrun}/lib/libapplications.so",
-            "${anyrun}/lib/librandr.so",
-            "${anyrun}/lib/librink.so",
-            "${anyrun}/lib/libshell.so",
-            "${anyrun}/lib/libsymbols.so",
-            "${anyrun}/lib/libtranslate.so",
-          ],
-        )
-      '';
-
-      "anyrun/style.css".text = ''
+      extraCss = ''
         * {
           transition: 200ms ease;
           font-family: Lexend;
