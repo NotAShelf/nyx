5853e2d57dd55e23711f124fb94904de747b6343
NotAShelf <raf@notashelf.dev>
2023-06-28 23:52:20 +0300

    hosts/enyo: disable dhcp for primary interface

--- hosts/enyo/hardware-configuration.nix
+++ hosts/enyo/hardware-configuration.nix
@@ -60,7 +60,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault false;
-  networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
+  #networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
   #networking.interfaces.wlp1s0f0u8.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
