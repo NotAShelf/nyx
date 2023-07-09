2379ae8c0418f2d6b5e03f09979a8d148e4f9c3e
NotAShelf <raf@notashelf.dev>
2023-07-10 00:15:47 +0300

    modules/boot: enable plymouth conditionally

--- modules/common/system/boot/plymouth/default.nix
+++ modules/common/system/boot/plymouth/default.nix
@@ -6,27 +6,30 @@
   ...
 }: let
   inherit (pkgs) plymouth;
+  inherit (lib) mkIf;
 
   cfg = config.modules.system.boot.plymouth;
 in {
-  # configure plymouth theme
-  # <https://github.com/adi1090x/plymouth-themes>
-  boot.plymouth = let
-    pack = cfg.pack;
-    theme = cfg.theme;
-  in {
-    enable = true;
-    themePackages = [(self'.packages.plymouth-themes.override {inherit pack theme;})];
-    # inherit theme;
-  };
+  config = mkIf cfg.enable {
+    # configure plymouth theme
+    # <https://github.com/adi1090x/plymouth-themes>
+    boot.plymouth = let
+      pack = cfg.pack;
+      theme = cfg.theme;
+    in {
+      enable = true;
+      themePackages = [(self'.packages.plymouth-themes.override {inherit pack theme;})];
+      # inherit theme;
+    };
 
-  # make plymouth work with sleep
-  powerManagement = {
-    powerDownCommands = ''
-      ${plymouth} --show-splash
-    '';
-    resumeCommands = ''
-      ${plymouth} --quit
-    '';
+    # make plymouth work with sleep
+    powerManagement = {
+      powerDownCommands = ''
+        ${plymouth} --show-splash
+      '';
+      resumeCommands = ''
+        ${plymouth} --quit
+      '';
+    };
   };
 }
