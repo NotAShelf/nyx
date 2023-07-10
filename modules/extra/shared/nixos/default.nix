2273828cc7bd361f6604c7a83e671cd204d0f4ce
NotAShelf <raf@notashelf.dev>
2023-07-10 11:46:56 +0300

    modules/shared: init xplr module

--- modules/extra/shared/nixos/default.nix
+++ modules/extra/shared/nixos/default.nix
@@ -2,5 +2,6 @@ _: {
   imports = [
     ./comma
     ./steam
+    ./xplr
   ];
 }
---
+++ modules/extra/shared/nixos/xplr/default.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with builtins; let
+  inherit (lib) types mkIf mkOption mkEnableOption mdDoc literalExpression concatStringsSep;
+
+  cfg = config.programs.xplr;
+  initialConfig = ''
+    version = '${cfg.package.version}'
+  '';
+  # we provide a default version line within the configuration file, which is obtained from the package's attributes
+  # merge the initial configFile, a mapped list of plugins and then the user defined configuration to obtain the final configuration
+  configFile = initialConfig + ("package.path=\n" + (concatStringsSep " ..\n" (map (p: ''"${p}/init.lua;"'') cfg.plugins)) + " ..\npackage.path\n") + cfg.extraConfig;
+in {
+  options.programs.xplr = {
+    enable = mkEnableOption "xplr, terminal UI based file explorer" // {default = true;};
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.xplr;
+      defaultText = literalExpression "pkgs.xplr";
+      description = mdDoc "xplr package to use";
+    };
+
+    plugins = mkOption {
+      type = with types; nullOr (listOf (either package str));
+      default = [];
+      defaultText = literalExpression "[]";
+      description = mdDoc ''
+        Plugins to be added to your configuration file. Must be a package, an absolute plugin path, or string
+        to be recognized by xplr
+      '';
+    };
+
+    # TODO: rename, this is the main configuration
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = lib.mdDoc ''
+        Extra xplr configuration.
+      '';
+
+      example = literalExpression ''
+        require("wl-clipboard").setup {
+          copy_command = "wl-copy -t text/uri-list",
+          paste_command = "wl-paste",
+          keep_selection = true,
+        }
+      '';
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    environment = {
+      # TODO: wrap the package to set config location
+      systemPackages = [cfg.package];
+
+      etc = {
+        "xplr/init.lua".source = pkgs.writeText "init.lua" configFile;
+      };
+    };
+  };
+}
