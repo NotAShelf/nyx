9451bded2964040bdf7be980c1c0523dc0bc8bea
NotAShelf <raf@notashelf.dev>
2023-07-14 17:33:30 +0300

    lib: adaquately separate lib functions

--- lib/default.nix
+++ lib/default.nix
@@ -4,31 +4,9 @@
   inputs,
   ...
 }: let
-  inherit (lib) lists mapAttrsToList filterAttrs hasSuffix;
-
   builders = import ./builders.nix {inherit lib inputs nixpkgs;};
   services = import ./services.nix {inherit lib;};
   validators = import ./validators.nix {inherit lib;};
-
-  # filter files that have the .nix suffix
-  filterNixFiles = k: v: v == "regular" && hasSuffix ".nix" k;
-
-  # import files that are selected by filterNixFiles
-  importNixFiles = path:
-    (lists.forEach (mapAttrsToList (name: _: path + ("/" + name))
-        (filterAttrs filterNixFiles (builtins.readDir path))))
-    import;
-
-  # return an int (1/0) based on boolean value
-  boolToNum = bool:
-    if bool
-    then 1
-    else 0;
-
-  # a basic function to fetch a specified user's public keys from github .keys url
-  fetchKeys = username: (builtins.fetchurl "https://github.com/${username}.keys");
+  helpers = import ./helpers.nix {inherit lib;};
 in
-  nixpkgs.lib.extend (
-    self: super:
-      {inherit filterNixFiles importNixFiles boolToNum fetchKeys;} // builders // services // validators
-  )
+  nixpkgs.lib.extend (_: _: builders // services // validators // helpers)
---
+++ lib/helpers.nix
@@ -0,0 +1,25 @@
+{lib, ...}: let
+  inherit (lib) lists mapAttrsToList filterAttrs hasSuffix;
+
+  primaryMonitor = config: builtins.elemAt 0 config.modules.device.monitors;
+
+  # filter files that have the .nix suffix
+  filterNixFiles = k: v: v == "regular" && hasSuffix ".nix" k;
+
+  # import files that are selected by filterNixFiles
+  importNixFiles = path:
+    (lists.forEach (mapAttrsToList (name: _: path + ("/" + name))
+        (filterAttrs filterNixFiles (builtins.readDir path))))
+    import;
+
+  # return an int (1/0) based on boolean value
+  boolToNum = bool:
+    if bool
+    then 1
+    else 0;
+
+  # a basic function to fetch a specified user's public keys from github .keys url
+  fetchKeys = username: (builtins.fetchurl "https://github.com/${username}.keys");
+in {
+  inherit primaryMonitor filterNixFiles importNixFiles boolToNum fetchKeys;
+}
