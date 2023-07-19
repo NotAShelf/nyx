bfe014281a799cc67021ee3bad9516ef0f383fd7
NotAShelf <raf@notashelf.dev>
2023-07-19 21:54:17 +0300

    parts/pkgs: cleanup

--- parts/pkgs/ani-cli.nix
+++ parts/pkgs/ani-cli.nix
@@ -12,13 +12,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "ani-cli";
-  version = "4.4";
+  version = "4.5";
 
   src = fetchFromGitHub {
     owner = "pystardust";
     repo = "${pname}";
-    rev = "${version}";
-    sha256 = "eY5FXwNRSt4ZFnvMyPLEFnTazwsXOkuQ6zivCHD70YY=";
+    rev = "v${version}";
+    hash = "sha256-HDpspU9OZxDET7/1rnKdGgaVEBt0gpzGtd3DuNIj7FY=";
   };
 
   nativeBuildInputs = [makeWrapper];
