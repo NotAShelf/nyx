5129ed0db0846fc9d0c73b2370eaf91e40ea83fa
NotAShelf <raf@notashelf.dev>
2023-08-01 15:50:25 +0300

    editors/neovim: disable beacon for irrelevant filetypes

--- homes/notashelf/terminal/editors/neovim/default.nix
+++ homes/notashelf/terminal/editors/neovim/default.nix
@@ -45,6 +45,16 @@ in {
 
           beacon-nvim = {
             package = beacon;
+            setup = ''
+              vim.cmd([[
+                let g:beacon_timeout = 450
+                let g:beacon_size = 70
+                let g:beacon_minimal_jump = 10
+                let g:beacon_ignore_filetypes = ["toggleterm", "NvimTree", "qf", "help", "TelescopePrompt", "harpoon", "grapple", "fzf", "portal", "nvim-navbuddy"]
+                " highlight Beacon guibg=#3a567d
+                " base = "#3a567d", " bright = "#617897", " dim = "#2e4564"
+              ]])
+            '';
           };
         };
 
