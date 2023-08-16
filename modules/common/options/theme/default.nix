cae74cf6fc1155383bd43fae5506cb165379ccd2
NotAShelf <raf@notashelf.dev>
2023-08-16 20:05:56 +0300

    options/theme: more options for qt themes

--- modules/common/options/theme/default.nix
+++ modules/common/options/theme/default.nix
@@ -55,10 +55,14 @@ in {
 
         # qt specific options
         qt = {
-          style = {
+          theme = {
             package = mkOption {
               type = types.package;
-              default = pkgs.catppucin-kde;
+              default = pkgs.catppuccin-kde.override {
+                flavour = ["mocha"];
+                accents = ["blue"];
+                winDecStyles = ["modern"];
+              };
               description = "The theme package to be used for QT programs";
             };
             name = mkOption {
@@ -66,6 +70,11 @@ in {
               default = "Catppuccin-Mocha-Dark";
               description = "The name for the QT theme package";
             };
+          };
+
+          kdeglobals.source = mkOption {
+            type = types.path;
+            default = "${cfg.qt.theme.package}/";
           };
         };
 
