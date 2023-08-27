999186f22094d4f45d72522e847e1da39049faa0
NotAShelf <raf@notashelf.dev>
2023-08-27 20:57:53 +0300

    syntax

--- homes/notashelf/terminal/tools/programs/bat/default.nix
+++ homes/notashelf/terminal/tools/programs/bat/default.nix
@@ -1,11 +1,13 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  catppuccin = builtins.readFile (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
+    hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
+  });
+in {
   programs.bat = {
     enable = true;
     themes = {
-      Catppuccin-mocha = builtins.readFile (pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
-        hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
-      });
+      Catppuccin-mocha = catppuccin;
     };
     config = {
       theme = "Catppuccin-mocha";
