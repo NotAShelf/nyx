1dbc7a637f515aa8d5842aabb3582c2a6d221090
NotAShelf <raf@notashelf.dev>
2023-09-06 17:58:30 +0300

    flake: use nix-index-database flake

--- homes/notashelf/terminal/tools/services/nix-index.nix
+++ homes/notashelf/terminal/tools/services/nix-index.nix
@@ -1,40 +1,12 @@
-{
-  pkgs,
-  lib,
-  ...
-}: let
-  nix-index-update = ''
-    export filename="index-x86_64-$(uname | tr A-Z a-z)"
-    mkdir -p ~/.cache/nix-index
-    cd ~/.cache/nix-index
-    # -N will only download a new version if there is an update.
-    wget -Nv https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
-    ln -f $filename files
-  '';
-in {
-  home.packages = [
-    pkgs.nix-index
-    (pkgs.writeScriptBin "nix-index-update-db" nix-index-update) # add update script to packages so that we can invoke it manually
-  ];
+{inputs, ...}: {
+  imports = [inputs.nix-index-dh.hmModules.nix-index];
 
-  # set up nix-index
-  systemd.user.timers.nix-index-db-update = {
-    Timer = {
-      OnCalendar = "weekly";
-      Persistent = true;
-      RandomizedDelaySec = 0;
-    };
-  };
+  config = {
+    programs.nix-index = {
+      enable = true;
 
-  systemd.user.services.nix-index-db-update = {
-    Unit = {
-      Description = "nix-index database update";
-      PartOf = ["multi-user.target"];
+      # link nix-inde database to ~/.cache/nix-index
+      symlinkToCacheHome = true;
     };
-    Service = {
-      Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath [pkgs.wget pkgs.coreutils]}";
-      ExecStart = "${pkgs.writeShellScript "nix-index-update-db" nix-index-update}";
-    };
-    Install.WantedBy = ["multi-user.target"];
   };
 }
