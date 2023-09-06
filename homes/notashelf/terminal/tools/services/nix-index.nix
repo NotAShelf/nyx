80fca27808724686860753604cfd06b657d454a1
NotAShelf <raf@notashelf.dev>
2023-09-06 19:38:58 +0300

    tools/nix-index: typo in module name

--- homes/notashelf/terminal/tools/services/nix-index.nix
+++ homes/notashelf/terminal/tools/services/nix-index.nix
@@ -1,12 +1,15 @@
 {inputs, ...}: {
-  imports = [inputs.nix-index-dh.hmModules.nix-index];
+  imports = [inputs.nix-index-db.hmModules.nix-index];
 
   config = {
-    programs.nix-index = {
-      enable = true;
+    programs = {
+      nix-index = {
+        enable = true;
 
-      # link nix-inde database to ~/.cache/nix-index
-      symlinkToCacheHome = true;
+        # link nix-inde database to ~/.cache/nix-index
+        symlinkToCacheHome = true;
+      };
+      nix-index-database.comma.enable = true;
     };
   };
 }
