441d7359f4e25f8fb3da28f0848d91496550426f
NotAShelf <raf@notashelf.dev>
2023-09-22 22:32:18 +0300

    modules/common: direnv cleanup

--- modules/common/core/system/os/programs/default.nix
+++ modules/common/core/system/os/programs/default.nix
@@ -30,10 +30,6 @@
       # shut up. SHUT UP
       silent = true;
 
-      # set `keep-derivations` and `keep-outputs` nix settings
-      # I already set those myself, but we still make sure
-      persistDerivations = true;
-
       # faster, persistent implementation of use_nix and use_flake
       nix-direnv.enable = true;
     };
