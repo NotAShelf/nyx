027283e36274d9cf7ec6a22d677c3158cf0efa3d
NotAShelf <raf@notashelf.dev>
2023-10-01 20:48:54 +0300

    cpu/amd: load amd-pstate module unconditionally

--- modules/common/core/system/hardware/cpu/amd/default.nix
+++ modules/common/core/system/hardware/cpu/amd/default.nix
@@ -8,6 +8,9 @@ with lib; let
 in {
   config = mkIf (device.cpu == "amd" || device.cpu == "vm-amd") {
     hardware.cpu.amd.updateMicrocode = true;
-    boot.kernelModules = ["kvm-amd"];
+    boot.kernelModules = [
+      "kvm-amd"
+      "amd-pstate" # load pstate module in case the device has a newer gpu
+    ];
   };
 }
