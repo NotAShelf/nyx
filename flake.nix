3a500731b21fb4cd95e5e04ef383aac0e6f2a35a
NotAShelf <raf@notashelf.dev>
2023-10-11 17:52:49 +0300

    treewide: enable formatting checks & treefmt prettier module

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
         ./flake/args.nix # args that are passsed to the flake, moved away from the main file
         ./flake/pre-commit.nix # pre-commit hooks
-        ./flake/checks.nix # checks for the flake
+        #./flake/checks.nix # checks for the flake
       ];
 
       flake = let
@@ -55,7 +55,6 @@
 
       perSystem = {
         inputs',
-        self',
         config,
         pkgs,
         ...
--- flake/treefmt/default.nix
+++ flake/treefmt/default.nix
@@ -1,15 +1,24 @@
 _: {
   systems = ["x86_64-linux" "aarch64-linux"];
 
-  perSystem = _: {
+  perSystem = {pkgs, ...}: {
     # configure treefmt
     treefmt = {
       projectRootFile = "flake.nix";
 
       programs = {
         alejandra.enable = true;
-        deadnix.enable = false;
         shellcheck.enable = true;
+
+        prettier = {
+          enable = true;
+          package = pkgs.prettierd;
+          excludes = ["*.age"];
+          settings = {
+            editorconfig = true;
+          };
+        };
+
         shfmt = {
           enable = true;
           # https://flake.parts/options/treefmt-nix.html#opt-perSystem.treefmt.programs.shfmt.indent_size
