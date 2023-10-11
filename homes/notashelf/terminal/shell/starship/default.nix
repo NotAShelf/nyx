1702377ffae03816b96b686ce86090f0bb838d0b
NotAShelf <raf@notashelf.dev>
2023-10-11 13:20:20 +0300

    shell/starship: prettify prompt

--- homes/notashelf/terminal/shell/starship/default.nix
+++ homes/notashelf/terminal/shell/starship/default.nix
@@ -12,6 +12,79 @@
       add_newline = false;
       scan_timeout = 3;
 
+      format = let
+        elements = [
+          "$shell"
+          "$docker_context"
+          "$package"
+          "$c"
+          "$cmake"
+          "$cobol"
+          "$daml"
+          "$dart"
+          "$deno"
+          "$dotnet"
+          "$elixir"
+          "$elm"
+          "$erlang"
+          "$fennel"
+          "$golang"
+          "$guix_shell"
+          "$haskell"
+          "$haxe"
+          "$helm"
+          "$java"
+          "$julia"
+          "$kotlin"
+          "$gradle"
+          "$lua"
+          "$nim"
+          "$nodejs"
+          "$ocaml"
+          "$opa"
+          "$perl"
+          "$php"
+          "$pulumi"
+          "$purescript"
+          "$python"
+          "$raku"
+          "$rlang"
+          "$red"
+          "$ruby"
+          "$rust"
+          "$scala"
+          "$solidity"
+          "$swift"
+          "$terraform"
+          "$vlang"
+          "$vagrant"
+          "$zig"
+          "$buf"
+          "$conda"
+          "$meson"
+          "$spack"
+          "$memory_usage"
+          "$aws"
+          "$gcloud"
+          "$openstack"
+          "$azure"
+          "$env_var"
+          "$crystal"
+          "$custom"
+          "$sudo"
+          "$cmd_duration"
+          "$jobs"
+          "$battery"
+          "$status"
+          "$os"
+          "$container"
+          "$shell"
+          "$time"
+        ];
+
+        elemsConcatted = builtins.concatStringsSep "" elements;
+      in "[ ](bold green) $username$hosntame$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$nix_shell${elemsConcatted}\n$character";
+
       character = {
         error_symbol = "[](bold red)";
         success_symbol = "[](bold green)";
@@ -19,7 +92,7 @@
         format = "$symbol [|](bold bright-black) ";
       };
 
-      git_commit = {commit_hash_length = 5;};
+      git_commit = {commit_hash_length = 7;};
       line_break.disabled = false;
 
       lua.symbol = "[](blue) ";
