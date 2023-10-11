ffaa8f944d9fe93c1c78d3e0b62dbf0395843054
NotAShelf <raf@notashelf.dev>
2023-10-11 12:24:27 +0300

    wms/hyprland: pass everything to dbus

--- homes/notashelf/graphical/wms/hyprland/default.nix
+++ homes/notashelf/graphical/wms/hyprland/default.nix
@@ -27,9 +27,14 @@ in {
 
     wayland.windowManager.hyprland = {
       enable = true;
-      systemd.enable = true;
-      package = inputs'.hyprland.packages.default.override {
-        enableNvidiaPatches = (device.gpu == "nvidia") || (device.gpu == "hybrid-nv");
+      package = inputs'.hyprland.packages.default;
+
+      enableNvidiaPatches = (device.gpu == "nvidia") || (device.gpu == "hybrid-nv");
+      xwayland.enable = true;
+
+      systemd = {
+        enable = true;
+        variables = ["--all"];
       };
     };
   };
