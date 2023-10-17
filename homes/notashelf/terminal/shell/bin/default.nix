a7b07de4205f42ac68984745cdbab360c6ffbb02
NotAShelf <raf@notashelf.dev>
2023-10-18 00:21:11 +0300

    homees/notashelf: clean up `.local/bin` scripts

--- homes/notashelf/terminal/shell/bin/default.nix
+++ homes/notashelf/terminal/shell/bin/default.nix
@@ -3,47 +3,71 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  inherit (builtins) readFile;
+in {
   home = {
     sessionPath = [
       "${config.home.homeDirectory}/.local/bin"
     ];
 
     file = {
-      ".local/bin/updoot" = {
-        # Upload and get link
-        executable = true;
-        text = import ./updoot.nix {inherit lib pkgs;};
-      };
-
       ".local/bin/preview" = {
         # Preview script for fzf tab
-        executable = true;
-        text = import ./preview.nix {inherit lib pkgs;};
+        source = lib.getExe (pkgs.writeShellApplication {
+          name = "preview";
+          runtimeInputs = with pkgs; [fzf eza catimg];
+          text = readFile ./preview/preview.sh;
+        });
       };
 
       ".local/bin/show-zombie-parents" = {
-        # pretty self-explanatory
-        executable = true;
-        text = import ./show-zombie-parents.nix {inherit lib pkgs;};
+        # Show zombie processes and their parents
+        source = lib.getExe (pkgs.writeShellApplication {
+          name = "show-zombie-parents";
+          runtimeInputs = with pkgs; [fzf eza catimg];
+          text = readFile ./show-zombie-parents/show-zombie-parents.sh;
+        });
       };
 
       ".local/bin/tzip" = {
         # Find all latest .tmod files recursively in current directory and zip them
         # for tmodloader mods downloaded via steam workshop
-        executable = true;
-        text = import ./tzip.nix {inherit lib pkgs;};
+        source = lib.getExe (pkgs.writeShellApplication {
+          name = "show-zombie-parents";
+          runtimeInputs = with pkgs; [zip];
+          text = readFile ./tzip/tzip.sh;
+        });
       };
 
       ".local/bin/extract" = {
         # Extract the compressed file with the correct tool based on the extension
-        executable = true;
-        text = import ./extract.nix {inherit lib pkgs;};
+        source = lib.getExe (pkgs.writeShellApplication {
+          name = "show-zombie-parents";
+          runtimeInputs = with pkgs; [zip unzip gnutar p7zip];
+          text = readFile ./extract/extract.sh;
+        });
       };
 
       ".local/bin/compilec" = {
-        executable = true;
-        text = import ./compilec.nix {};
+        # Interactively find and compile C++ programs
+        source = lib.getExe (pkgs.writeShellApplication {
+          name = "show-zombie-parents";
+          runtimeInputs = with pkgs; [skim coreutils gcc];
+          text = readFile ./compilec/compilec.sh;
+        });
+      };
+
+      ".local/bin/addr" = {
+        # Get external IP address
+        source = lib.getExe (pkgs.writeShellApplication {
+          name = "addr";
+          runtimeInputs = with pkgs; [curl];
+          text = ''
+            #!${pkgs.stdenv.shell}
+            exec curl "$@" icanhazip.com
+          '';
+        });
       };
     };
   };
--- homes/notashelf/terminal/shell/bin/show-zombie-parents/show-zombie-parents.sh
+++ homes/notashelf/terminal/shell/bin/show-zombie-parents/show-zombie-parents.sh
@@ -1,5 +1,4 @@
-{pkgs, ...}:
+#!/usr/bin/env sh
+
 # https://www.linkedin.com/pulse/how-identify-kill-zombiedefunct-processes-linux-without-george-gabra/
-''
-  ps -A -ostat,ppid | grep -e '[zZ]'| awk '{ print $2 }' | uniq | xargs ps -p
-''
+ps -A -ostat,ppid | grep -e '[zZ]' | awk '{ print $2 }' | uniq | xargs ps -p
