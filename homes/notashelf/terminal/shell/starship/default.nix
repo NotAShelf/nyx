7a0124cc51e1d7f6aecc4ea77448511833b5a07e
NotAShelf <raf@notashelf.dev>
2023-10-18 21:54:52 +0300

    shell/starship: read a list of prompt elems from a separate file

--- homes/notashelf/terminal/shell/starship/default.nix
+++ homes/notashelf/terminal/shell/starship/default.nix
@@ -5,86 +5,19 @@
     };
   };
 
-  programs.starship = {
+  programs.starship = let
+    inherit (import ./elements.nix) elements;
+    elemsConcatted = builtins.concatStringsSep "" elements;
+  in {
     enable = true;
 
     settings = {
       add_newline = false;
       scan_timeout = 3;
 
-      format = let
-        elements = [
-          "$shell"
-          "$docker_context"
-          "$package"
-          "$c"
-          "$cmake"
-          "$cobol"
-          "$daml"
-          "$dart"
-          "$deno"
-          "$dotnet"
-          "$elixir"
-          "$elm"
-          "$erlang"
-          "$fennel"
-          "$golang"
-          "$guix_shell"
-          "$haskell"
-          "$haxe"
-          "$helm"
-          "$java"
-          "$julia"
-          "$kotlin"
-          "$gradle"
-          "$lua"
-          "$nim"
-          "$nodejs"
-          "$ocaml"
-          "$opa"
-          "$perl"
-          "$php"
-          "$pulumi"
-          "$purescript"
-          "$python"
-          "$raku"
-          "$rlang"
-          "$red"
-          "$ruby"
-          "$rust"
-          "$scala"
-          "$solidity"
-          "$swift"
-          "$terraform"
-          "$vlang"
-          "$vagrant"
-          "$zig"
-          "$buf"
-          "$conda"
-          "$meson"
-          "$spack"
-          "$memory_usage"
-          "$aws"
-          "$gcloud"
-          "$openstack"
-          "$azure"
-          "$env_var"
-          "$crystal"
-          "$custom"
-          "$sudo"
-          "$cmd_duration"
-          "$jobs"
-          "$battery"
-          "$status"
-          "$os"
-          "$container"
-          "$shell"
-          "$time"
-        ];
+      format = "[ ](bold green) $hostname$username$hosntame$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$nix_shell${elemsConcatted}\n$character";
 
-        elemsConcatted = builtins.concatStringsSep "" elements;
-      in "[ ](bold green) $username$hosntame$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$nix_shell${elemsConcatted}\n$character";
-
+      # configure specific elements
       character = {
         error_symbol = "[](bold red)";
         success_symbol = "[](bold green)";
@@ -92,20 +25,21 @@
         format = "$symbol [|](bold bright-black) ";
       };
 
-      git_commit = {commit_hash_length = 7;};
+      git_commit.commit_hash_length = 7;
       line_break.disabled = false;
 
+      hostname = {
+        format = "@[$hostname](bold blue)";
+        ssh_only = true;
+        disabled = false;
+      };
+
+      # language configurations
       lua.symbol = "[](blue) ";
       python.symbol = "[](blue) ";
       nix_shell.symbol = "[](blue) ";
       rust.symbol = "[](red) ";
       package.symbol = "📦  ";
-
-      hostname = {
-        ssh_only = true;
-        format = "@[$hostname](bold blue) ";
-        disabled = false;
-      };
     };
   };
 }
