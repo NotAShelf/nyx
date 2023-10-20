c0c44265d8950510c4069d8131f33ede2a51d4f2
NotAShelf <raf@notashelf.dev>
2023-10-20 23:09:53 +0300

    terminal/foot: separate color presets

---
+++ homes/notashelf/terminal/emulators/foot/presets/catppuccin-mocha/colors.nix
@@ -0,0 +1,24 @@
+{colorscheme}: let
+  inherit (colorscheme) colors;
+in {
+  foreground = "${colors.base05}"; # Text
+  background = "${colors.base00}"; # Base
+
+  regular0 = "${colors.base03}"; # Surface 1
+  regular1 = "${colors.base08}"; # red
+  regular2 = "${colors.base0B}"; # green
+  regular3 = "${colors.base0A}"; # yellow
+  regular4 = "${colors.base0D}"; # blue
+  regular5 = "${colors.base0F}"; # pink
+  regular6 = "${colors.base0C}"; # teal
+  regular7 = "${colors.base06}"; # Subtext 0
+  # Subtext 1 ???
+  bright0 = "${colors.base04}"; # Surface 2
+  bright1 = "${colors.base08}"; # red
+  bright2 = "${colors.base0B}"; # green
+  bright3 = "${colors.base0A}"; # yellow
+  bright4 = "${colors.base0D}"; # blue
+  bright5 = "${colors.base0F}"; # pink
+  bright6 = "${colors.base0C}"; # teal
+  bright7 = "${colors.base07}"; # Subtext 0
+}
