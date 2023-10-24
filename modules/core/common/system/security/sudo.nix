de4d06f60376b19d37af8a81f0dc6c5384b3be87
NotAShelf <raf@notashelf.dev>
2023-10-24 23:04:08 +0300

    security/sudo: allow sudo to switch configurations

--- modules/core/common/system/security/sudo.nix
+++ modules/core/common/system/security/sudo.nix
@@ -1,6 +1,7 @@
 {lib, ...}: {
   security = {
-    # no, you are not brekaing my system because of "muh rust" again
+    # https://github.com/NixOS/nixpkgs/pull/256491
+    # no nixpkgs, you are not breaking my system because of "muh rust" delusions again
     # sudo-rs is still a feature-incomplete sudo fork that can and will mess things up
     # also for the love of god stop rewriting things in rust
     sudo-rs.enable = lib.mkForce false;
@@ -13,19 +14,23 @@
       # this is especially useful if you are using --target-host option in nixos-rebuild switch
       # however it's also a massive security flaw - which is why it should be replaced with the
       # extraRules you will see below
-      wheelNeedsPassword = true;
+      wheelNeedsPassword = lib.mkDefault true;
 
       # only allow members of the wheel group to execute sudo
       # by setting the executable’s permissions accordingly
       execWheelOnly = true;
 
+      # additional sudo configuration
       extraConfig = ''
         Defaults lecture = never # rollback results in sudo lectures after each reboot, it's somewhat useless anyway
         Defaults pwfeedback # password input feedback - makes typed password visible as asterisks
-        Defaults env_keep += "EDITOR PATH" # variables that will be passed to the root account
+        Defaults env_keep += "EDITOR PATH DISPLAY" # variables that will be passed to the root account
         Defaults timestamp_timeout = 300 # makes sudo ask for password less often
       '';
 
+      # additional sudo rules
+      # this is a better approach for making certain commands sudo-less instead of
+      # allowing the wheel users to run *anything* without password
       extraRules = [
         {
           # allow wheel group to run nixos-rebuild without password
@@ -33,10 +38,13 @@
           groups = ["sudo" "wheel"];
           commands = [
             {
+              # whitelist switch-to-configuration, allows --target-host option
+              # to deploy to remote servers without reading password from STDIN
               command = "/run/current-system/bin/switch-to-configuration";
               options = ["NOPASSWD"];
             }
             {
+              # let wheel group collect garbage without password
               command = "/run/current-system/sw/bin/nix-collect-garbage";
               options = ["NOPASSWD"];
             }
