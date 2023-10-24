a78a3714de4c866c06655fec3b924b5ea21c19bf
NotAShelf <raf@notashelf.dev>
2023-10-24 14:58:06 +0300

    security/sudo: disable passwordless sudo

--- modules/core/common/system/security/sudo.nix
+++ modules/core/common/system/security/sudo.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{lib, ...}: {
   security = {
     # no, you are not brekaing my system because of "muh rust" again
     # sudo-rs is still a feature-incomplete sudo fork that can and will mess things up
@@ -17,17 +13,17 @@
       # this is especially useful if you are using --target-host option in nixos-rebuild switch
       # however it's also a massive security flaw - which is why it should be replaced with the
       # extraRules you will see below
-      wheelNeedsPassword = false;
+      wheelNeedsPassword = true;
 
       # only allow members of the wheel group to execute sudo
       # by setting the executable’s permissions accordingly
       execWheelOnly = true;
 
       extraConfig = ''
-        Defaults lecture = never # rollback results in sudo lectures after each reboot
-        Defaults pwfeedback
-        Defaults env_keep += "EDITOR PATH"
-        Defaults timestamp_timeout = 300
+        Defaults lecture = never # rollback results in sudo lectures after each reboot, it's somewhat useless anyway
+        Defaults pwfeedback # password input feedback - makes typed password visible as asterisks
+        Defaults env_keep += "EDITOR PATH" # variables that will be passed to the root account
+        Defaults timestamp_timeout = 300 # makes sudo ask for password less often
       '';
 
       extraRules = [
@@ -37,7 +33,11 @@
           groups = ["sudo" "wheel"];
           commands = [
             {
-              command = "${pkgs.nixos-rebuild}/bin/nixos-rebuild";
+              command = "/run/current-system/bin/switch-to-configuration";
+              options = ["NOPASSWD"];
+            }
+            {
+              command = "/run/current-system/sw/bin/nix-collect-garbage";
               options = ["NOPASSWD"];
             }
           ];
