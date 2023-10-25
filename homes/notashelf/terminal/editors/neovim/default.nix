3d48417e9f5308ec08e4424df661cb36cb5b4682
NotAShelf <raf@notashelf.dev>
2023-10-25 06:26:51 +0300

    editors/neovim-flake: configure specs.nvim

--- homes/notashelf/terminal/editors/neovim/default.nix
+++ homes/notashelf/terminal/editors/neovim/default.nix
@@ -59,6 +59,16 @@ in {
             setup = ''
               require('specs').setup {
                 show_jumps  = true,
+                popup = {
+                  delay_ms = 0, -- delay before popup displays
+                  inc_ms = 15, -- time increments used for fade/resize effects
+                  blend = 15, -- starting blend, between 0-100 (fully transparent), see :h winblend
+                  width = 10,
+                  winhl = "PMenu",
+                  fader = require('specs').linear_fader,
+                  resizer = require('specs').shrink_resizer
+                },
+
                 ignore_filetypes = {
                   'NvimTree',
                   'undotree',
