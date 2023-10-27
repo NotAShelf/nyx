e4222421eedb3ad29cc6555dcd980b32dfbd99ac
NotAShelf <raf@notashelf.dev>
2023-10-27 07:40:22 +0300

    system/nix: permit insecure electron

--- modules/core/common/system/nix/default.nix
+++ modules/core/common/system/nix/default.nix
@@ -40,7 +40,7 @@ with lib; {
       allowUnfree = true; # really a pain in the ass to deal with when disabled
       allowBroken = false;
       allowUnsupportedSystem = true;
-      permittedInsecurePackages = []; # default to none, add more as necessary
+      permittedInsecurePackages = ["electron-24.8.6"]; # default to none, add more as necessary
     };
 
     overlays = [
