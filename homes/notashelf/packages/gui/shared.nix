0d5b3804c29213b5f43362f3fd1f02ad0be271fc
NotAShelf <raf@notashelf.dev>
2023-10-27 07:39:45 +0300

    packages/gui: remove udiskie from packages

--- homes/notashelf/packages/gui/shared.nix
+++ homes/notashelf/packages/gui/shared.nix
@@ -12,38 +12,42 @@ with lib; let
   acceptedTypes = ["laptop" "desktop" "hybrid" "lite"];
 in {
   config = mkIf ((programs.gui.enable && sys.video.enable) && (builtins.elem device.type acceptedTypes)) {
-    home.packages = with pkgs;
-      [
-        schildichat-desktop
-        qbittorrent
-        hexchat
-        netflix
-        bitwarden
-        obsidian
-        nextcloud-client
-        udiskie
-        gnome.gnome-tweaks
-        gnome.gnome-calendar
-        easyeffects
-        librewolf
-        # zoom-us # I hate this
-        cinnamon.nemo
-        plasma5Packages.dolphin
-        plasma5Packages.dolphin-plugins
-        plasma5Packages.kio
-        plasma5Packages.kio-extras
-        plasma5Packages.kimageformats
-        plasma5Packages.kdegraphics-thumbnailers
-      ]
-      ++ [
-        (pkgs.symlinkJoin {
-          name = "gnome-control-center-wrapped";
-          paths = [pkgs.gnome.gnome-control-center];
-          nativeBuildInputs = [pkgs.makeWrapper];
-          postBuild = ''
-            wrapProgram "$out/bin/gnome-control-center" --set XDG_CURRENT_DESKTOP gnome
-          '';
-        })
-      ];
+    home.packages = with pkgs; [
+      schildichat-desktop
+      qbittorrent
+      hexchat
+      netflix
+      bitwarden
+      obsidian
+      nextcloud-client
+      gnome.gnome-tweaks
+      gnome.gnome-calendar
+      easyeffects
+      librewolf
+      # zoom-us # I hate this
+      cinnamon.nemo
+      plasma5Packages.dolphin
+      plasma5Packages.dolphin-plugins
+      plasma5Packages.kio
+      plasma5Packages.kio-extras
+      plasma5Packages.kimageformats
+      plasma5Packages.kdegraphics-thumbnailers
+
+      # override gnome-control-center to trick it into thinking we're running gnome
+      # <https://github.com/NixOS/nixpkgs/issues/230493>
+      # <https://gitlab.gnome.org/GNOME/gnome-control-center/-/merge_requests/736>
+      # get overriden idiot
+      gnome-control-center.overrideAttrs
+      (upstream: {
+        # gnome-control-center does not start without XDG_CURRENT_DESKTOP=gnome
+        preFixup =
+          ''
+            gappsWrapperArgs+=(
+              --set XDG_CURRENT_DESKTOP "gnome"
+            );
+          ''
+          + upstream.preFixup;
+      })
+    ];
   };
 }
