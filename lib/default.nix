5c51f4427c961a886b9760797c2e6c0d56d51bec
NotAShelf <raf@notashelf.dev>
2023-10-29 15:23:04 +0300

    lib/aliass: init

--- lib/default.nix
+++ lib/default.nix
@@ -4,11 +4,17 @@
   ...
 }: let
   inherit (nixpkgs) lib;
+  inherit (lib) foldl recursiveUpdate;
 
   builders = import ./builders.nix {inherit inputs lib;};
   services = import ./services.nix {inherit lib;};
   validators = import ./validators.nix {inherit lib;};
   helpers = import ./helpers.nix {inherit lib;};
   hardware = import ./hardware.nix {inherit lib;};
+  aliases = import ./aliases.nix {inherit lib;};
+
+  # recursively merges two attribute sets
+  mergeRecursively = lhs: rhs: recursiveUpdate lhs rhs;
 in
-  nixpkgs.lib.extend (_: _: builders // services // validators // helpers // hardware)
+  # extend nixpkgs lib
+  lib.extend (_: _: foldl mergeRecursively {} [builders services validators helpers hardware aliases])
