8f14b57e018b5d14a94f27a2f470fc14f635b78b
NotAShelf <raf@notashelf.dev>
2023-10-30 09:38:30 +0300

    gpu/amd: mess around with drivers

--- modules/core/common/system/hardware/gpu/amd/default.nix
+++ modules/core/common/system/hardware/gpu/amd/default.nix
@@ -3,30 +3,44 @@
   lib,
   pkgs,
   ...
-}:
-with lib; let
-  device = config.modules.device;
+}: let
+  inherit (lib) mkIf;
+
+  dev = config.modules.device;
 in {
-  config = mkIf (device.gpu == "amd" || device.gpu == "hybrid-amd") {
+  config = mkIf (dev.gpu == "amd" || dev.gpu == "hybrid-amd") {
     # enable amdgpu xorg drivers in case Hyprland breaks again
     services.xserver.videoDrivers = ["amdgpu"];
 
     # enable amdgpu kernel module
     boot = {
-      kernelModules = ["amdgpu"];
-      initrd.kernelModules = ["amdgpu"];
+      initrd.kernelModules = ["amdgpu"]; # load amdgpu kernel module as early as initrd
+      kernelModules = ["amdgpu"]; # if loading somehow fails during initrd but the boot continues, try again later
     };
 
     # enables AMDVLK & OpenCL support
     hardware.opengl.extraPackages = with pkgs; [
+      amdvlk
       # opencl drivers
       rocm-opencl-icd
       rocm-opencl-runtime
     ];
 
-    /*
     hardware.opengl.extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+      rocm-opencl-icd
+      rocm-opencl-runtime
     ];
-    */
+
+    environment.systemPackages = with pkgs; [
+      blender-hip
+      vulkan-tools
+      mesa
+      libdrm
+      driversi686Linux.mesa
+      haskellPackages.vulkan
+      vulkan-loader
+      vulkan-utility-libraries
+    ];
   };
 }
