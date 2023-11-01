3259f21e7fd13e15e63fe4ab9a16e16b4f481bc1
NotAShelf <raf@notashelf.dev>
2023-11-01 21:53:16 +0300

    system/activation: link user ssh config to root user
    Nix build attempts to connect to the remote builders as the root user, for whatever reason, and fails when the keys are missing.
    On activation, link my own user's ssh keys to the root user's ssh directory in order to allow for reliable remote builds.

--- modules/core/common/system/activation/default.nix
+++ modules/core/common/system/activation/default.nix
@@ -3,14 +3,36 @@
   lib,
   ...
 }: {
-  system.activationScripts.diff = lib.mkIf config.modules.system.activation.diffGenerations {
-    supportsDryActivation = true;
-    text = ''
-      if [[ -e /run/current-system ]]; then
-        echo "=== diff to current-system ==="
-        ${config.nix.package}/bin/nix --extra-experimental-features nix-command store diff-closures /run/current-system "$systemConfig"
-        echo "=== end of the system diff ==="
-      fi
-    '';
+  system.activationScripts = {
+    # if system declares that it wants closure diffs, then run the diff script on activation
+    # this is useless if you are using nh, which does this for you in a different way
+    diff = lib.mkIf config.modules.system.activation.diffGenerations {
+      supportsDryActivation = true;
+      text = ''
+        if [[ -e /run/current-system ]]; then
+          echo "=== diff to current-system ==="
+          ${config.nix.package}/bin/nix --extra-experimental-features nix-command store diff-closures /run/current-system "$systemConfig"
+          echo "=== end of the system diff ==="
+        fi
+      '';
+    };
+
+    # https://github.com/colemickens/nixcfg/blob/main/mixins/ssh.nix
+    # symlink root's ssh config to ours
+    # to fix nix-daemon's ability to remote build since it sshs from the root account
+    root_ssh_config = let
+      sshDir = "/home/notashelf/.ssh";
+    in {
+      text = ''
+        (
+          # symlink root ssh config to ours so daemon can use our agent/keys/etc...
+          mkdir -p /root/.ssh
+          ln -sf ${sshDir}/config /root/.ssh/config
+          ln -sf ${sshDir}/known_hosts /root/.ssh/known_hosts
+          ln -sf ${sshDir}/known_hosts /root/.ssh/known_hosts
+        )
+      '';
+      deps = [];
+    };
   };
 }
