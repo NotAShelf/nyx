1b8cf8b4066f54653a4ed1c8a0c3eea53c88dd79
NotAShelf <raf@notashelf.dev>
2023-11-07 23:52:56 +0300

    apps/webcord: bump catppuccin theme version

--- homes/notashelf/graphical/apps/webcord/default.nix
+++ homes/notashelf/graphical/apps/webcord/default.nix
@@ -9,19 +9,19 @@ with lib; let
   catppuccin-mocha = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "discord";
-    rev = "d4a7027faeb4abef59aee9362504699d92751fbc";
-    hash = "sha256-ZSuvGzQqJj/EZBmB2F0hqd9TBNtUoaHTijn/z9ujRVg=";
+    rev = "20abe29b3f0f7c59c4878b1bf6ceae41aeac9afd";
+    hash = "sha256-Gjrv1VayPfjcsfSmGJdJTA8xEX6gXhpgTLJ2xrSNcEo=";
   };
 
-  device = osConfig.modules.device;
-  video = osConfig.modules.system.video;
+  dev = osConfig.modules.device;
+  sys = osConfig.modules.system;
   acceptedTypes = ["desktop" "laptop" "hybrid"];
 in {
   imports = [
     inputs.arrpc.homeManagerModules.default
   ];
 
-  config = mkIf ((builtins.elem device.type acceptedTypes) && (video.enable)) {
+  config = mkIf ((builtins.elem dev.type acceptedTypes) && sys.video.enable) {
     home.packages = with pkgs; [
       webcord-vencord # webcord with vencord extension installed
     ];
