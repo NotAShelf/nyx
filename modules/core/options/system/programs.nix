3bc9db2aea016fdf3e39180da1f4c6c266738cef
NotAShelf <raf@notashelf.dev>
2023-11-09 11:50:22 +0300

    options/system: move old programs module options to the new one

--- modules/core/options/system/programs.nix
+++ modules/core/options/system/programs.nix
@@ -1,4 +1,8 @@
-{lib, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   inherit (lib) mkEnableOption mkOption types;
 in {
   options.modules = {
@@ -15,21 +19,6 @@ in {
       vscode.enable = mkEnableOption "Visual Studio Code";
       zathura.enable = mkEnableOption "Zathura document viewer";
       steam.enable = mkEnableOption "Steam game client";
-
-      media = {
-        mpv.enable = mkEnableOption "mpv media player";
-        addDefaultPackages = mkOption {
-          type = types.bool;
-          default = true;
-          description = "Add default mpv packages";
-        };
-
-        extraDefaultPackages = mkOption {
-          type = with types; listOf package;
-          default = [];
-          description = "Add extra mpv packages";
-        };
-      };
 
       chromium = {
         enable = mkEnableOption "Chromium browser";
@@ -58,6 +47,65 @@ in {
         kitty.enable = mkEnableOption "Kitty terminal emulator";
         wezterm.enable = mkEnableOption "WezTerm terminal emulator";
         foot.enable = mkEnableOption "Foot terminal emulator";
+      };
+
+      gaming = {
+        enable = mkEnableOption "Enable packages required for the device to be gaming-ready";
+        emulation.enable = mkEnableOption "Enable programs required to emulate other platforms";
+        chess.enable = mkEnableOption "Chess programs and engines" // {default = config.modules.programs.gaming.enable;};
+        gamescope.enable = mkEnableOption "Gamescope compositing manager" // {default = config.modules.programs.gaming.enable;};
+      };
+
+      media = {
+        mpv.enable = mkEnableOption "mpv media player";
+        addDefaultPackages = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Add default mpv packages";
+        };
+
+        extraDefaultPackages = mkOption {
+          type = with types; listOf package;
+          default = [];
+          description = "Add extra mpv packages";
+        };
+      };
+
+      git = {
+        signingKey = mkOption {
+          type = types.str;
+          default = "";
+          description = "The default gpg key used for signing commits";
+        };
+      };
+
+      # default program options
+      default = {
+        # what program should be used as the default terminal
+        terminal = mkOption {
+          type = types.enum ["foot" "kitty" "wezterm"];
+          default = "kitty";
+        };
+
+        fileManager = mkOption {
+          type = types.enum ["thunar" "dolphin" "nemo"];
+          default = "dolphin";
+        };
+
+        browser = mkOption {
+          type = types.enum ["firefox" "librewolf" "chromium"];
+          default = "firefox";
+        };
+
+        editor = mkOption {
+          type = types.enum ["neovim" "helix" "emacs"];
+          default = "neovim";
+        };
+
+        launcher = mkOption {
+          type = types.enum ["rofi" "wofi" "anyrun"];
+          default = "rofi";
+        };
       };
     };
   };
