75db0c5dd38e6777d7c5d603e565fd3b8fa25628
NotAShelf <raf@notashelf.dev>
2023-11-24 01:42:33 +0300

    environment/aliases: add a gpl3 alias to global aliases

--- modules/core/common/system/os/environment/aliases.nix
+++ modules/core/common/system/os/environment/aliases.nix
@@ -1,10 +1,13 @@
-_: {
+{pkgs, ...}: {
   environment.shellAliases = {
-    # aliases
+    # nix aliases
     rebuild = "nix-store --verify; pushd ~dotfiles ; nixos-rebuild switch --flake .#$1 --use-remote-sudo && notify-send \"Done\" ; popd";
     deploy = "nixos-rebuild switch --flake .#$1 --target-host $1 --use-remote-sudo -Lv";
 
     # things I do to keep my home directory clean
     wget = "wget --hsts-file='\${XDG_DATA_HOME}/wget-hsts'";
+
+    # init a LICENSE file with my go-to gpl3 license
+    "gpl" = "${pkgs.curl}/bin/curl https://www.gnu.org/licenses/gpl-3.0.txt -o LICENSE";
   };
 }
