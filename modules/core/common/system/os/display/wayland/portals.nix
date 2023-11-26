1218c9653aa924bccf1bd276a81337d96fb8da9d
NotAShelf <raf@notashelf.dev>
2023-11-26 06:23:36 +0300

    display/wayland: explicit portal configuration

--- modules/core/common/system/os/display/wayland/portals.nix
+++ modules/core/common/system/os/display/wayland/portals.nix
@@ -16,6 +16,21 @@ in {
         pkgs.xdg-desktop-portal-gtk
       ];
 
+      config = {
+        common = let
+          portal =
+            if env.desktop == "Hyprland"
+            then "hyprland"
+            else "wlr";
+        in {
+          # for flameshot to work
+          # https://github.com/flameshot-org/flameshot/issues/3363#issuecomment-1753771427
+          default = "gtk";
+          "org.freedesktop.impl.portal.Screencast" = "${portal}";
+          "org.freedesktop.impl.portal.Screenshot" = "${portal}";
+        };
+      };
+
       # xdg-desktop-wlr (this section) is no longer needed, xdg-desktop-portal-hyprland
       # will (and should) override this one
       # however in case I run a different compositor on a Wayland host, it can be enabled
