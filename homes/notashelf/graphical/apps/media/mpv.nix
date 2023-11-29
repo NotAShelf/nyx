4b66b49eecabae4cfb495d9b00660801521235a6
NotAShelf <raf@notashelf.dev>
2023-11-29 09:38:17 +0300

    media/mpv: add more keybinds

--- homes/notashelf/graphical/apps/media/mpv.nix
+++ homes/notashelf/graphical/apps/media/mpv.nix
@@ -17,14 +17,37 @@ in {
       enable = true;
       bindings =
         {
-          Y = "add sub-scale +0.1"; # increase subtitle font size
-          G = "add sub-scale -0.1"; # decrease subtitle font size
-          y = "sub_step -1"; # immediately display next subtitle
-          g = "sub_step +1"; # previous
-          R = "cycle_values window-scale 2 0.5 1"; # switch between 2x, 1/2, unresized window size
+          "Y" = "add sub-scale +0.1"; # increase subtitle font size
+          "G" = "add sub-scale -0.1"; # decrease subtitle font size
+          "y" = "sub_step -1"; # immediately display next subtitle
+          "g" = "sub_step +1"; # previous
+          "R" = "cycle_values window-scale 2 0.5 1"; # switch between 2x, 1/2, unresized window size
+
+          "l" = "seek 5";
+          "h" = "seek -5";
+          "j" = "seek -60";
+          "k" = "seek 60";
+
+          "s" = "cycle sub";
+          "a" = "cycle audio";
+
+          "Alt+h" = "add chapter -1";
+          "Alt+l" = "add chapter 1";
+          "Ctrl+SPACE" = "add chapter 1";
+
+          "Alt+j" = "add video-zoom -0.25";
+          "Alt+k" = "add video-zoom 0.25";
+
+          "Alt+J" = "add sub-pos -1";
+          "Alt+K" = "add sub-pos +1";
+
+          "Ctrl+h" = "multiply speed 1/1.1";
+          "Ctrl+l" = "multiply speed 1.1";
+          "Ctrl+H" = "set speed 1.0";
         }
         // low1k;
       config = {
+        ytdl-format = "bestvideo+bestaudio/best";
         audio-display = false;
         force-window = true;
         hidpi-window-scale = false;
@@ -41,6 +64,7 @@ in {
         osd-bar = "no";
         border = "no";
       };
+
       scripts = with pkgs.mpvScripts; [
         #cutter
         mpris
