39cb709592829a9f3dff1df0d4d260d3132697eb
NotAShelf <raf@notashelf.dev>
2023-12-07 22:03:52 +0300

    services/mpd: organize relevant services and programs

--- homes/notashelf/services/shared/media/mpd/default.nix
+++ homes/notashelf/services/shared/media/mpd/default.nix
@@ -1,16 +1,13 @@
 {
-  config,
-  pkgs,
   osConfig,
+  config,
   lib,
+  pkgs,
   ...
 }: let
-  musicDir = "${config.home.homeDirectory}/Media/Music";
   dev = osConfig.modules.device;
-
   acceptedTypes = ["desktop" "laptop" "lite" "hybrid"];
 in {
-  imports = [./beets];
   config = lib.mkIf (builtins.elem dev.type acceptedTypes) {
     home.packages = with pkgs; [
       playerctl # CLI interface for playerctld
@@ -23,10 +20,21 @@ in {
       mpris-proxy.enable = true;
       mpd-mpris.enable = true;
 
+      # MPRIS 2 support to mpd
+      mpdris2 = {
+        enable = true;
+        notifications = true;
+        multimediaKeys = true;
+        mpd = {
+          # for some reason config.xdg.userDirs.music is not a "path" - possibly because it has $HOME in its name?
+          inherit (config.services.mpd) musicDirectory;
+        };
+      };
+
       # mpd service
       mpd = {
         enable = true;
-        musicDirectory = musicDir;
+        musicDirectory = "${config.home.homeDirectory}/Media/Music";
         network = {
           startWhenNeeded = true;
           listenAddress = "127.0.0.1";
@@ -35,15 +43,19 @@ in {
 
         extraConfig = ''
           audio_output {
-            type    "pipewire"
-            name    "pipewire"
+            type "pipewire"
+            name "PipeWire"
+            auto_resample "no"
+            use_mmap "yes"
           }
+
           audio_output {
             type                    "fifo"
             name                    "fifo"
             path                    "/tmp/mpd.fifo"
             format                  "44100:16:2"
           }
+
           auto_update "yes"
         '';
       };
@@ -60,105 +72,14 @@ in {
           };
         };
       };
-
-      # MPRIS 2 support to mpd
-      mpdris2 = {
-        enable = true;
-        notifications = true;
-        multimediaKeys = true;
-        mpd = {
-          # for some reason config.xdg.userDirs.music is not a "path" - possibly because it has $HOME in its name?
-          inherit (config.services.mpd) musicDirectory;
-        };
-      };
     };
 
     programs = {
+      # music tagger and organizer
+      beets = import ./beets.nix {inherit config;};
+
       # ncmpcpp configuration, has cool stuff like visualiser
-      ncmpcpp = {
-        enable = true;
-        package = pkgs.ncmpcpp.override {
-          visualizerSupport = true;
-        };
-        mpdMusicDir = config.services.mpd.musicDirectory;
-        settings = {
-          # Miscelaneous
-          ncmpcpp_directory = "${config.xdg.configHome}/ncmpcpp";
-          ignore_leading_the = true;
-          external_editor = "nvim";
-          message_delay_time = 1;
-          playlist_disable_highlight_delay = 2;
-          autocenter_mode = "yes";
-          centered_cursor = "yes";
-          allow_for_physical_item_deletion = "no";
-          lines_scrolled = "0";
-          follow_now_playing_lyrics = "yes";
-          lyrics_fetchers = "musixmatch";
-
-          # visualizer
-          visualizer_data_source = "/tmp/mpd.fifo";
-          visualizer_output_name = "mpd_visualizer";
-          visualizer_type = "ellipse";
-          visualizer_look = "●● ";
-          visualizer_color = "blue, green";
-
-          # appearance
-          colors_enabled = "yes";
-          playlist_display_mode = "classic";
-          user_interface = "classic";
-          volume_color = "white";
-
-          # window
-          song_window_title_format = "Music";
-          statusbar_visibility = "no";
-          header_visibility = "no";
-          titles_visibility = "no";
-          # progress bar
-          progressbar_look = "‎‎‎";
-          progressbar_color = "black";
-          progressbar_elapsed_color = "blue";
-
-          # song list
-          song_status_format = "$7%t";
-          song_list_format = "$(008)%t$R  $(247)%a$R$5  %l$8";
-          song_columns_list_format = "(53)[blue]{tr} (45)[blue]{a}";
-
-          current_item_prefix = "$b$2| ";
-          current_item_suffix = "$/b$5";
-
-          now_playing_prefix = "$b$5| ";
-          now_playing_suffix = "$/b$5";
-
-          song_library_format = "{{%a - %t} (%b)}|{%f}";
-
-          # colors
-          main_window_color = "blue";
-
-          current_item_inactive_column_prefix = "$b$5";
-          current_item_inactive_column_suffix = "$/b$5";
-
-          color1 = "white";
-          color2 = "blue";
-        };
-        bindings = [
-          {
-            key = "j";
-            command = "scroll_down";
-          }
-          {
-            key = "k";
-            command = "scroll_up";
-          }
-          {
-            key = "J";
-            command = ["select_item" "scroll_down"];
-          }
-          {
-            key = "K";
-            command = ["select_item" "scroll_up"];
-          }
-        ];
-      };
+      ncmpcpp = import ./ncmpcpp.nix {inherit config pkgs;};
 
       /*
       # yams service
