afa70b5cbab6737d888f6ddcbb2f159e749f1762
NotAShelf <raf@notashelf.dev>
2023-12-11 13:49:08 +0300

    security/kernel: document blacklisted filesystems

--- modules/core/common/system/security/kernel.nix
+++ modules/core/common/system/security/kernel.nix
@@ -173,39 +173,42 @@ in {
 
         # Old or rare or insufficiently audited filesystems
         [
-          "adfs"
-          "affs"
-          "bfs"
-          "befs"
-          "cramfs"
-          "efs"
-          "erofs"
-          "exofs"
-          "freevxfs"
-          "f2fs"
-          "vivid"
-          "gfs2"
-          "ksmbd"
-          "nfsv4"
-          "nfsv3"
-          "cifs"
-          "nfs"
-          "cramfs"
-          "freevxfs"
-          "jffs2"
-          "hfs"
-          "hfsplus"
-          "squashfs"
-          "udf"
-          "hpfs"
-          "jfs"
-          "minix"
-          "nilfs2"
-          "omfs"
-          "qnx4"
-          "qnx6"
-          "sysv"
-          "vivid" # Video Test Driver (unnecessary)
+          "adfs" # Active Directory Federation Services
+          "affs" # Amiga Fast File System
+          "befs" # "Be File System"
+          "bfs" # BFS, used by SCO UnixWare OS for the /stand slice
+          "cifs" # Common Internet File System
+          "cramfs" # compressed ROM/RAM file system
+          "efs" # Extent File System
+          "erofs" # Enhanced Read-Only File System
+          "exofs" # EXtended Object File System
+          "freevxfs" # Veritas filesystem driver
+          "f2fs" # Flash-Friendly File System
+          "vivid" # Virtual Video Test Driver (unnecessary)
+          "gfs2" # Global File System 2
+          "hpfs" # High Performance File System (used by OS/2)
+          "hfs" # Hierarchical File System (Macintosh)
+          "hfsplus" # " same as above, but with extended attributes
+          "jffs2" # Journalling Flash File System (v2)
+          "jfs" # Journaled File System - only useful for VMWare sessions
+          "ksmbd" # SMB3 Kernel Server
+          "minix" # minix fs - used by the minix OS
+          "nfsv3" # " (v3)
+          "nfsv4" # Network File System (v4)
+          "nfs" # Network File System
+          "nilfs2" # New Implementation of a Log-structured File System
+          "omfs" # Optimized MPEG Filesystem
+          "qnx4" #  extent-based file system used by the QNX4 and QNX6 OSes
+          "qnx6" # "
+          "squashfs" # compressed read-only file system (used by live CDs)
+          "sysv" # implements all of Xenix FS, SystemV/386 FS and Coherent FS.
+          "udf" # https://docs.kernel.org/5.15/filesystems/udf.html
+        ]
+
+        # Disable Thunderbolt and FireWire to prevent DMA attacks
+        [
+          "thunderbolt"
+          "firewire-core"
         ]
 
         # you might possibly want your webcam to work
