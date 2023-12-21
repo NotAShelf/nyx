15bcba031220a98b6be20de1c6965d95d32cc42a
NotAShelf <raf@notashelf.dev>
2023-12-21 16:38:16 +0300

    services/headscale: cleanup

--- modules/core/roles/server/system/services/networking/headscale.nix
+++ modules/core/roles/server/system/services/networking/headscale.nix
@@ -31,23 +31,20 @@ in {
             override_local_dns = true;
             magic_dns = true;
             base_domain = "notashelf.dev";
-            domains = ["ts.notashelf.dev"];
+            domains = [];
             nameservers = [
               "9.9.9.9" # no cloudflare, nice
             ];
 
+            /*
             extra_records = [
               {
                 name = "idm.notashelf.dev";
                 type = "A";
                 value = "100.64.0.1"; # NOTE: this should be the address of the "host" node - which is the server
               }
-              {
-                name = "helios";
-                type = "A";
-                value = "100.64.0.1";
-              }
             ];
+            */
           };
 
           derp.server = {
