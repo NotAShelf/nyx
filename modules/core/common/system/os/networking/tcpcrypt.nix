7a37f72e9b259f47d0fe025068b482cad39f6088
NotAShelf <raf@notashelf.dev>
2023-12-21 11:08:20 +0300

    os/networking: enable tcpcrypt
    seems fixed, but watch for future breakages

--- modules/core/common/system/os/networking/tcpcrypt.nix
+++ modules/core/common/system/os/networking/tcpcrypt.nix
@@ -8,9 +8,7 @@ in {
   # enable opportunistic TCP encryption
   # this is NOT a pancea, however, if the receiver supports encryption and the attacker is passive
   # privacy will be more plausible (but not guaranteed, unlike what the option docs suggest)
-  # TODO: technically this is redundant as I already have some TCP security improvements, but it's a nice addition
-  # I should check and see if the reason for the breakage in the service below is because of my own changes
-  networking.tcpcrypt.enable = false; # FIXME: the systemd service does something wrong, investigate
+  networking.tcpcrypt.enable = true;
 
   users = mkIf config.networking.tcpcrypt.enable {
     groups.tcpcryptd = {};
