f1ed8eaece2efd0ed4e1a91e753ab1614b71be94
NotAShelf <raf@notashelf.dev>
2023-12-21 16:32:24 +0300

    networking/tcpcrypt: enable only if device is not a server

--- modules/core/common/system/os/networking/tcpcrypt.nix
+++ modules/core/common/system/os/networking/tcpcrypt.nix
@@ -4,11 +4,13 @@
   ...
 }: let
   inherit (lib) mkIf;
+
+  dev = config.modules.dev;
 in {
   # enable opportunistic TCP encryption
   # this is NOT a pancea, however, if the receiver supports encryption and the attacker is passive
   # privacy will be more plausible (but not guaranteed, unlike what the option docs suggest)
-  networking.tcpcrypt.enable = true;
+  networking.tcpcrypt.enable = dev.type != "server";
 
   users = mkIf config.networking.tcpcrypt.enable {
     groups.tcpcryptd = {};
