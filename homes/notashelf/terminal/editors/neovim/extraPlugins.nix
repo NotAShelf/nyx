ef80f7c32a391f718be824954c01710505382e4c
NotAShelf <raf@notashelf.dev>
2023-12-30 02:25:09 +0300

    editors/neovim: use npins to manage out-of-tree plugins

--- homes/notashelf/terminal/editors/neovim/extraPlugins.nix
+++ homes/notashelf/terminal/editors/neovim/extraPlugins.nix
@@ -1,56 +1,65 @@
-{
-  pkgs,
-  lib,
-  ...
-}: let
+{pkgs, ...}: let
   inherit (pkgs.vimPlugins) friendly-snippets aerial-nvim nvim-surround undotree mkdir-nvim ssr-nvim direnv-vim;
-  inherit (pkgs.vimUtils) buildVimPlugin;
-
-  htms = buildVimPlugin {
-    name = "htms.nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "calops";
-      repo = "hmts.nvim";
-      rev = "v0.1.0";
-      hash = "sha256-SBNTtuzwSmGgwALD/JqLwXGLow+Prn7dJrQNODPeOAY=";
-    };
-  };
-
-  regexplainer = buildVimPlugin {
-    name = "nvim-regexplainer";
-    src = pkgs.fetchFromGitHub {
-      owner = "bennypowers";
-      repo = "nvim-regexplainer";
-      rev = "4250c8f3c1307876384e70eeedde5149249e154f";
-      hash = "sha256-15DLbKtOgUPq4DcF71jFYu31faDn52k3P1x47GL3+b0=";
-    };
-  };
-
-  specs-nvim = buildVimPlugin {
-    name = "specs.nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "edluffy";
-      repo = "specs.nvim";
-      rev = "2743e412bbe21c9d73954c403d01e8de7377890d";
-      hash = "sha256-mYTzltCEKO8C7BJ3WrB/iFa1Qq1rgJlcjW6NYHPfmPk=";
-    };
-  };
-
-  deferred-clipboard = buildVimPlugin {
-    name = "deferred-clipboard";
-    src = pkgs.fetchFromGitHub {
-      owner = "EtiamNullam";
-      repo = "deferred-clipboard.nvim";
-      rev = "810a29d166eaa41afc220cc7cd85eeaa3c43b37f";
-      hash = "sha256-nanNQEtpjv0YKEkkrPmq/5FPxq+Yj/19cs0Gf7YgKjU=";
-    };
-  };
+  pluginSources = import ./pluginSources.nix {inherit pkgs;};
 in {
+  # plugins that are pulled from nixpkgs
+  direnv = {package = direnv-vim;};
   friendly-snippets = {package = friendly-snippets;};
   mkdir-nvim = {package = mkdir-nvim;};
-  regexplainer = {package = regexplainer;};
+  aerial = {
+    package = aerial-nvim;
+    setup = "require('aerial').setup {}";
+  };
+
+  nvim-surround = {
+    package = nvim-surround;
+    setup = "require('nvim-surround').setup {}";
+  };
+
+  undotree = {
+    package = undotree;
+    setup = ''
+      vim.g.undotree_ShortIndicators = true
+      vim.g.undotree_TreeVertShape = '│'
+    '';
+  };
+
+  ssr-nvim = {
+    package = ssr-nvim;
+    setup = "require('ssr').setup {}";
+  };
+
+  # plugins that are built from their sources
+  regexplainer = {package = pluginSources.regexplainer;};
+  data-view = {
+    package = pluginSources.data-viewer-nvim;
+    setup = ''
+      -- open data files in data-viewer.nvim
+      vim.api.nvim_exec([[
+        autocmd BufReadPost,BufNewFile *.sqlite,*.csv,*.tsv DataViewer
+      ]], false)
+
+
+      -- keybinds
+      vim.api.nvim_set_keymap('n', '<leader>dv', ':DataViewer<CR>', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<leader>dvn', ':DataViewerNextTable<CR>', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<leader>dvp', ':DataViewerPrevTable<CR>', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<leader>dvc', ':DataViewerClose<CR>', {noremap = true})
+    '';
+  };
+
+  hmts = {
+    package = pluginSources.hmts;
+    after = ["treesitter"];
+  };
+
+  smart-splits = {
+    package = pluginSources.smart-splits;
+    setup = "require('smart-splits').setup {}";
+  };
+
   specs-nvim = {
-    package = specs-nvim;
+    package = pluginSources.specs-nvim;
     setup = ''
       require('specs').setup {
         show_jumps = true,
@@ -75,44 +84,6 @@ in {
       -- bind specs to navigation keys
       vim.api.nvim_set_keymap('n', 'n', 'n:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', 'N', 'N:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
-    '';
-  };
-
-  aerial = {
-    package = aerial-nvim;
-    setup = "require('aerial').setup {}";
-  };
-
-  nvim-surround = {
-    package = nvim-surround;
-    setup = "require('nvim-surround').setup {}";
-  };
-
-  htms = {
-    package = htms;
-    after = ["treesitter"];
-  };
-
-  undotree = {
-    package = undotree;
-    setup = ''
-      vim.g.undotree_ShortIndicators = true
-      vim.g.undotree_TreeVertShape = '│'
-    '';
-  };
-
-  ssr-nvim = {
-    package = ssr-nvim;
-    setup = "require('ssr').setup {}";
-  };
-
-  direnv = {package = direnv-vim;};
-  deferred-clipboard = {
-    package = deferred-clipboard;
-    setup = ''
-      require('deferred-clipboard').setup {
-        fallback = 'unnamedplus'
-      }
     '';
   };
 }
---
+++ homes/notashelf/terminal/editors/neovim/pluginSources.nix
@@ -0,0 +1,54 @@
+{pkgs, ...}: let
+  inherit (pkgs.vimUtils) buildVimPlugin;
+  pins = import ./npins;
+in {
+  hmts = buildVimPlugin {
+    name = "hmts.nvim";
+    src = pins."hmts.nvim";
+  };
+
+  smart-splits = buildVimPlugin {
+    name = "smart-splits";
+    src = pins."smart-splits.nvim";
+  };
+
+  regexplainer = buildVimPlugin {
+    name = "nvim-regexplainer";
+    src = pkgs.fetchFromGitHub {
+      owner = "bennypowers";
+      repo = "nvim-regexplainer";
+      rev = "4250c8f3c1307876384e70eeedde5149249e154f";
+      hash = "sha256-15DLbKtOgUPq4DcF71jFYu31faDn52k3P1x47GL3+b0=";
+    };
+  };
+
+  specs-nvim = buildVimPlugin {
+    name = "specs.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "edluffy";
+      repo = "specs.nvim";
+      rev = "2743e412bbe21c9d73954c403d01e8de7377890d";
+      hash = "sha256-mYTzltCEKO8C7BJ3WrB/iFa1Qq1rgJlcjW6NYHPfmPk=";
+    };
+  };
+
+  deferred-clipboard = buildVimPlugin {
+    name = "deferred-clipboard";
+    src = pkgs.fetchFromGitHub {
+      owner = "EtiamNullam";
+      repo = "deferred-clipboard.nvim";
+      rev = "810a29d166eaa41afc220cc7cd85eeaa3c43b37f";
+      hash = "sha256-nanNQEtpjv0YKEkkrPmq/5FPxq+Yj/19cs0Gf7YgKjU=";
+    };
+  };
+
+  data-viewer-nvim = buildVimPlugin {
+    name = "data-viewer.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "VidocqH";
+      repo = "data-viewer.nvim";
+      rev = "40ddf37bb7ab6c04ff9e820812d1539afe691668";
+      hash = "sha256-D5hvLhsYski11H9qiDDL2zlZMtYmbpHgpewiWR6C7rE=";
+    };
+  };
+}
