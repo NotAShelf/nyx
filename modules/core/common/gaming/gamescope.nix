6f9926ff6130298dd361a050dc5a65739a128ebd
NotAShelf <raf@notashelf.dev>
2023-12-30 20:32:52 +0300

    common/gaming: security wrapper for gamescope

--- modules/core/common/gaming/gamescope.nix
+++ modules/core/common/gaming/gamescope.nix
@@ -1,10 +1,14 @@
 {pkgs, ...}: {
-  security.wrappers = {
-    gamescope = {
-      owner = "root";
-      group = "root";
-      source = "${pkgs.gamescope}/bin/gamescope";
-      capabilities = "cap_sys_nice+pie";
+  config = {
+    security.wrappers = {
+      gamescope = {
+        owner = "root";
+        group = "root";
+        source = "${pkgs.gamescope}/bin/gamescope";
+        capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
+      };
     };
+
+    programs.gamescope.enable = true;
   };
 }
