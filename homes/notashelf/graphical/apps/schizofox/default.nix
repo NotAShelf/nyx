c42a8ef11d945e5f26526ab66c89503f1c88c1ca
NotAShelf <raf@notashelf.dev>
2024-01-03 07:18:38 +0300

    apps/schizofox: enable session restore manually

--- homes/notashelf/graphical/apps/schizofox/default.nix
+++ homes/notashelf/graphical/apps/schizofox/default.nix
@@ -41,6 +41,7 @@ in {
       security = {
         sanitizeOnShutdown = false;
         sandbox = true;
+        noSessionRestore = false;
         userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0";
       };
 
