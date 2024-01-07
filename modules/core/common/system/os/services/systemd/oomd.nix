b96d2d1d4406a3f093e8b40625a9e4a3879d0073
NotAShelf <raf@notashelf.dev>
2024-01-07 16:40:34 +0300

    os/systemd: adjust nix-daemon OOM score

--- modules/core/common/system/os/services/systemd/oomd.nix
+++ modules/core/common/system/os/services/systemd/oomd.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   systemd = {
     # Systemd OOMd
     # Fedora enables these options by default. See the 10-oomd-* files here:
@@ -6,14 +10,22 @@
     # by default it only kills cgroups. So either systemd services marked for killing under OOM
     # or (disabled by default, enabled by us) the entire user slice. Fedora used to kill root
     # and system slices, but their oomd configuration has since changed.
+    # TODO: maybe disable user slice by default?
     oomd = {
       enable = !config.systemd.enableUnifiedCgroupHierarchy;
-      enableRootSlice = false;
-      enableSystemSlice = false;
-      enableUserSlices = false;
+      enableRootSlice = true;
+      enableSystemSlice = true;
+      enableUserSlices = true;
       extraConfig = {
         "DefaultMemoryPressureDurationSec" = "20s";
       };
     };
+
+    # make it that nix builds are more likely killed than important services.
+    # 100 is the default for user slices and 500 is systemd-coredumpd@
+    # this is important because as my system got huge, nix flake check started
+    # causing OOMs and killing my desktop environment - which I do not like
+    # nuke nix-daemon if it gets too memory hungry
+    services.nix-daemon.serviceConfig.OOMScoreAdjust = lib.mkDefault 350;
   };
 }
