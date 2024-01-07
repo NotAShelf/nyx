a13f3ccef0a9c3e2e2e8b24cd11506556be37c78
NotAShelf <raf@notashelf.dev>
2024-01-07 16:54:54 +0300

    security/sudo: force execWheelOnly

--- modules/core/common/system/security/sudo.nix
+++ modules/core/common/system/security/sudo.nix
@@ -1,26 +1,26 @@
 {lib, ...}: let
-  inherit (lib) mkDefault;
+  inherit (lib) mkForce mkDefault;
 in {
   security = {
     # https://github.com/NixOS/nixpkgs/pull/256491
     # no nixpkgs, you are not breaking my system because of "muh rust" delusions again
     # sudo-rs is still a feature-incomplete sudo fork that can and will mess things up
     # also for the love of god stop rewriting things in rust
-    sudo-rs.enable = lib.mkForce false;
+    sudo-rs.enable = mkForce false;
 
     # the ol' reliable
     sudo = {
       enable = true;
 
-      # wheelNeedsPassword = false means wheel group can execute commands without a password
+      # WARNING: wheelNeedsPassword = false means wheel group can execute commands without a password
       # this is especially useful if you are using --target-host option in nixos-rebuild switch
       # however it's also a massive security flaw - which is why it should be replaced with the
       # extraRules you will see below
-      wheelNeedsPassword = lib.mkDefault false;
+      wheelNeedsPassword = mkDefault false;
 
       # only allow members of the wheel group to execute sudo
       # by setting the executable’s permissions accordingly
-      execWheelOnly = true;
+      execWheelOnly = mkForce true;
 
       # additional sudo configuration
       extraConfig = ''
@@ -40,7 +40,7 @@ in {
           # this is a less vulnerable alternative to having wheelNeedsPassword = false
           # whitelist switch-to-configuration, allows --target-host option
           # to deploy to remote servers without reading password from STDIN
-          groups = ["sudo" "wheel"];
+          groups = ["wheel"];
           commands = let
             currentSystem = "/run/current-system/";
             storePath = "/nix/store/";
@@ -66,7 +66,6 @@ in {
               command = "${currentSystem}/sw/bin/nix-collect-garbage";
               options = ["SETENV" "NOPASSWD"];
             }
-
             {
               # let wheel group interact with systemd without password
               command = "${currentSystem}/sw/bin/systemctl";
