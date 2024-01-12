3c4952625ac81337db6c6c3a04f92f38c1eb8f0d
NotAShelf <raf@notashelf.dev>
2024-01-12 04:55:09 +0300

    core/profiles: init gaming profile

---
+++ modules/core/profiles/gaming.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf;
+in {
+  config.modules.system.programs = mkIf config.modules.profiles.workstation.enable {
+    steam.enable = true;
+    gaming.enable = true;
+  };
+}
