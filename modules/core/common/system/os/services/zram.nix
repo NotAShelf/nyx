bfe90688e3103c4bab4b09de4a765196044d5b15
NotAShelf <raf@notashelf.dev>
2024-01-21 17:35:07 +0300

    services/zram: document relevant sysctl parameters

--- modules/core/common/system/os/services/zram.nix
+++ modules/core/common/system/os/services/zram.nix
@@ -13,12 +13,22 @@ in {
     memoryPercent = 90; # defaults to 50
   };
 
+  # <https://www.kernel.org/doc/html/latest/admin-guide/sysctl/vm.html>
   boot.kernel.sysctl = mkIf config.zramSwap.enable {
     # zram is relatively cheap, prefer swap
-    "vm.swappiness" = 180;
-    "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
+    #  swappiness refers to the kernel's willingness prefer swap
+    #  over memory. higher values mean that we'll utilize swap more often
+    #  which preserves memory, but will cause performance issues as well
+    #  as wear on the drive
+    "vm.swappiness" = 180; # 0-200
+    # level of reclaim when memory is being fragmented
+    "vm.watermark_boost_factor" = 0; # 0 to disable
+    # aggressiveness of kswapd
+    # it defines the amount of memory left in a node/system before kswapd is woken up
+    "vm.watermark_scale_factor" = 125; # 0-300
     # zram is in memory, no need to readahead
+    # page-cluster refers to the number of pages up to which
+    # consecutive pages are read in from swap in a single attempt
     "vm.page-cluster" = 0;
   };
 }
