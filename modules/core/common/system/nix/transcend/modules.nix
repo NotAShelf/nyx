cf10bee0600139cf433e96a6aa0d961103440984
NotAShelf <raf@notashelf.dev>
2024-01-24 12:43:58 +0300

    nix/transcend: remove merged nix-settings PR

--- modules/core/common/system/nix/transcend/modules.nix
+++ modules/core/common/system/nix/transcend/modules.nix
@@ -1,17 +1,10 @@
 {
   # the name here is arbitrary, and is used as an identifier
   # what matters is the presence of owner, rev and module
-  nix-gc = {
+  "nix-gc" = {
     # https://github.com/NixOS/nixpkgs/pull/260620
     owner = "nobbz";
     rev = "10ec045f1dc82c72630c85906e1ae1d54340a7e0";
     module = "/services/misc/nix-gc.nix";
-  };
-
-  nix-20 = {
-    # https://github.com/NixOS/nixpkgs/pull/271423
-    owner = "hercules-ci";
-    rev = "2d0f4a7ec19082248094eb04a35c93c94b1d35d5";
-    module = "/config/nix.nix";
   };
 }
