266331dbd6f52b8dc983a4f64223e33d9e88e0bc
NotAShelf <raf@notashelf.dev>
2024-01-26 00:24:25 +0300

    hosts/enyo: enable swaylock

--- hosts/enyo/modules/usrEnv.nix
+++ hosts/enyo/modules/usrEnv.nix
@@ -2,10 +2,11 @@
   config.modules.usrEnv = {
     desktop = "Hyprland";
     useHomeManager = true;
-
     launchers = {
       anyrun.enable = true;
       tofi.enable = true;
     };
+
+    screenlock.swaylock.enable = true;
   };
 }
