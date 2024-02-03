9250d605d2d489a4b65b82e6ddadd3cfdfecc93c
NotAShelf <raf@notashelf.dev>
2024-02-03 23:25:43 +0300

    themes/gtk: configure gtk3/gtk4 natively

--- homes/notashelf/themes/gtk.nix
+++ homes/notashelf/themes/gtk.nix
@@ -60,15 +60,29 @@ in {
       };
 
       gtk3.extraConfig = {
+        gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
+        gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+        gtk-decoration-layout = "appmenu:none";
+        gtk-button-images = 1;
+        gtk-menu-images = 1;
+        gtk-enable-event-sounds = 0;
+        gtk-enable-input-feedback-sounds = 0;
         gtk-xft-antialias = 1;
         gtk-xft-hinting = 1;
         gtk-xft-hintstyle = "hintslight";
-        gtk-xft-rgba = "rgb";
-        gtk-application-prefer-dark-theme = 1;
+        gtk-error-bell = 0;
+        gtk-application-prefer-dark-theme = true;
       };
 
       gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
+        gtk-decoration-layout = "appmenu:none";
+        gtk-enable-event-sounds = 0;
+        gtk-enable-input-feedback-sounds = 0;
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintslight";
+        gtk-error-bell = 0;
+        gtk-application-prefer-dark-theme = true;
       };
     };
   };
