0b1625631fc136199c971471ce26a8e9cf78c45e
NotAShelf <raf@notashelf.dev>
2024-02-07 06:46:12 +0300

    hardware/tpm: explain tciEnvironment.enable

--- modules/core/common/system/hardware/tpm.nix
+++ modules/core/common/system/hardware/tpm.nix
@@ -10,13 +10,17 @@ in {
   config = mkIf dev.hasTPM {
     security.tpm2 = {
       # enable Trusted Platform Module 2 support
-      enable = mkDefault true;
+      enable = true;
+
       # enable Trusted Platform 2 userspace resource manager daemon
       abrmd.enable = mkDefault false;
-      # set TCTI environment variables to the specified values if enabled
-      # - TPM2TOOLS_TCTI
-      # - TPM2_PKCS11_TCTI
+
+      # The TCTI is the "Transmission Interface" that is used to communicate with a
+      # TPM. this option sets TCTI environment variables to the specified values if enabled
+      #  - TPM2TOOLS_TCTI
+      #  - TPM2_PKCS11_TCTI
       tctiEnvironment.enable = mkDefault true;
+
       # enable TPM2 PKCS#11 tool and shared library in system path
       pkcs11.enable = mkDefault false;
     };
