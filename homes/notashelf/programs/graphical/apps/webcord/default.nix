b05bf4b35ce40c76cddbf919ea40ee49ae43d53f
NotAShelf <raf@notashelf.dev>
2024-02-07 09:54:54 +0300

    apps/webcord: build arRPC from source

--- homes/notashelf/programs/graphical/apps/webcord/default.nix
+++ homes/notashelf/programs/graphical/apps/webcord/default.nix
@@ -28,6 +28,36 @@ in {
       };
     };
 
-    services.arrpc.enable = true;
+    # TODO: maybe this should be under services/global because technically it's not an app
+    # however arrpc is useless on its own (i.e. without webcord) and here it's merely a
+    # companion app that we enable for rich presence.
+    services.arrpc = {
+      enable = true;
+      # NOTE: we can't yet use overrideAttrs on arrpc becausae the derivation is
+      # vastly different between v3.2.0 and v3.3.0. this builds the package
+      # from source very similarly to the active PR that I have made
+      # <https://github.com/NixOS/nixpkgs/pull/286659>
+      package = pkgs.buildNpmPackage {
+        pname = "arrpc";
+        version = "3.3.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "OpenAsar";
+          repo = "arrpc";
+          # not tagged for some reason.
+          # revision for 3.3.0
+          rev = "c6e23e7eb733ad396d3eebc328404cc656fed581";
+          hash = "sha256-OeEFNbmGp5SWVdJJwXZUkkNrei9jyuPc+4E960l8VRA=";
+        };
+
+        npmDepsHash = "sha256-YlSUGncpY0MyTiCfZcPsfcNx3fR+SCtkOFWbjOPLUzk=";
+        dontNpmBuild = true;
+
+        meta = {
+          # required for the systemd service to work properly
+          mainProgram = "arrpc";
+        };
+      };
+    };
   };
 }
