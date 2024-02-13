53db2a9f8e1b08521c04f1e613d2fa9aa6898da5
NotAShelf <raf@notashelf.dev>
2024-02-13 09:28:05 +0300

    emulators/wezterm: rewrite config; better colors

---
+++ homes/notashelf/programs/terminal/emulators/wezterm/colorSchemes.nix
@@ -0,0 +1,68 @@
+{colors}:
+with colors; {
+  followSystem = {
+    # basic colors
+    background = "#${base00}";
+    foreground = "#${base05}";
+    cursor_bg = "#${base05}";
+    cursor_border = "#${base05}";
+    cursor_fg = "#${base08}";
+    selection_bg = "#${base0E}";
+    selection_fg = "#${base00}";
+    split = "#${base01}";
+
+    # base16
+    ansi = [
+      "#${base03}"
+      "#${base08}"
+      "#${base0B}"
+      "#${base0A}"
+      "#${base0D}"
+      "#${base0F}"
+      "#${base0C}"
+      "#${base06}"
+    ];
+
+    brights = [
+      "#${base04}"
+      "#${base08}"
+      "#${base0B}"
+      "#${base0A}"
+      "#${base0D}"
+      "#${base0F}"
+      "#${base0C}"
+      "#${base07}"
+    ];
+
+    # tabbar
+    tab_bar = {
+      background = "#${base08}";
+      active_tab = {
+        bg_color = "#${base00}";
+        fg_color = "#${base05}";
+      };
+
+      inactive_tab = {
+        bg_color = "#${base08}";
+        fg_color = "#${base05}";
+      };
+
+      inactive_tab_hover = {
+        bg_color = "#${base00}";
+        fg_color = "#${base05}";
+      };
+
+      inactive_tab_edge = "#${base00}";
+
+      new_tab = {
+        bg_color = "#${base08}";
+        fg_color = "#${base07}";
+      };
+
+      new_tab_hover = {
+        bg_color = "#${base00}";
+        fg_color = "#${base05}";
+      };
+    };
+  };
+}
--- homes/notashelf/programs/terminal/emulators/wezterm/default.nix
+++ homes/notashelf/programs/terminal/emulators/wezterm/default.nix
@@ -15,7 +15,58 @@ in {
     programs.wezterm = {
       enable = true;
       package = pkgs.wezterm;
-      extraConfig = import ./config.nix {inherit colors;};
+      colorSchemes = import ./colorSchemes.nix {inherit colors;};
+      extraConfig = ''
+        local wez = require("wezterm")
+
+        return {
+         -- general
+         check_for_updates = false,
+         exit_behavior = "CloseOnCleanExit",
+         enable_scroll_bar = false,
+
+         -- tab bar
+         enable_tab_bar = true,
+         use_fancy_tab_bar = false,
+         hide_tab_bar_if_only_one_tab = true,
+         show_tab_index_in_tab_bar = false,
+
+         -- font config
+         font_size = 14.0,
+         font = wez.font_with_fallback({
+          "Iosevka Nerd Font",
+          "Material Design Icons",
+         }),
+
+         audible_bell = "Disabled",
+         default_cursor_style = "SteadyBar",
+         window_background_opacity = 0.85,
+
+         window_padding = {
+          left = 16,
+          right = 16,
+          top = 16,
+          bottom = 16,
+         },
+
+         -- perf
+         front_end = "WebGpu",
+         enable_wayland = true,
+         scrollback_lines = 1024,
+
+         -- colors
+         -- the followSystem theme is defined in colorSchemes.nix
+         -- as per my base16 theming options
+         color_scheme = "followSystem",
+
+         -- misc
+         window_close_confirmation = "NeverPrompt",
+         inactive_pane_hsb = {
+          saturation = 1.0,
+          brightness = 0.8
+         },
+        }
+      '';
     };
   };
 }
