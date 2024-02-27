f44e67b3f95f8629b264111b1bd9a0783fce33ca
NotAShelf <raf@notashelf.dev>
2024-02-28 01:09:01 +0300

    editors/neovim: enable tailwindcss LSP

--- homes/notashelf/programs/terminal/editors/neovim/plugins/settings/languages.nix
+++ homes/notashelf/programs/terminal/editors/neovim/plugins/settings/languages.nix
@@ -15,6 +15,7 @@
       nix.enable = true;
       html.enable = true;
       css.enable = true;
+      tailwind.enable = true;
       ts.enable = true;
       go.enable = true;
       python.enable = true;
