00d2d740256324cc6195ca76f9a733d1ec2a0244
NotAShelf <raf@notashelf.dev>
2024-03-02 16:35:23 +0300

    os/services: add doc comments to fstrim and fwupd

--- modules/core/common/system/os/services/fstrim.nix
+++ modules/core/common/system/os/services/fstrim.nix
@@ -1,4 +1,12 @@
 {
   # discard blocks that are not in use by the filesystem, good for SSDs
-  services.fstrim.enable = true;
+  services.fstrim = {
+    # we may enable this unconditionally across all systems becuase it's performance
+    # impact is negligible on systems without a SSD - which means it's a no-op with
+    # almost no downsides aside from the service firing once per week
+    enable = true;
+
+    # the default value, good enough for average-load systems
+    interval = "weekly";
+  };
 }
--- modules/core/common/system/os/services/fwupd.nix
+++ modules/core/common/system/os/services/fwupd.nix
@@ -3,5 +3,11 @@
   services.fwupd = {
     enable = true;
     daemonSettings.EspLocation = config.boot.loader.efi.efiSysMountPoint;
+
+    # newer hardware may have their firmware in testing
+    # e.g. Framework devices don't have their firmware in stable yet
+    # TODO: make a system-level option that sets this value for hosts
+    # that have testing firmware
+    # extraRemotes = [ "lvfs-testing" ];
   };
 }
