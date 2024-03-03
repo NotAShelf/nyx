8c1c4a1d8e59c0d8c43559eaa83dddbcccaf6705
NotAShelf <raf@notashelf.dev>
2024-03-03 15:26:47 +0300

    security/apparmor: provide apparmor utils while enabled

--- modules/core/common/system/security/apparmor.nix
+++ modules/core/common/system/security/apparmor.nix
@@ -3,55 +3,69 @@
   pkgs,
   lib,
   ...
-}: {
-  services.dbus.apparmor = "disabled";
+}: let
+  inherit (lib.modules) mkIf;
+in {
+  config = mkIf (lib.isx86Linux pkgs) {
+    services.dbus.apparmor = "enabled";
 
-  # apparmor configuration
-  security.apparmor = {
-    enable = lib.isx86Linux pkgs;
+    environment.systemPackages = with pkgs; [
+      apparmor-pam
+      apparmor-utils
+      apparmor-parser
+      apparmor-profiles
+      apparmor-bin-utils
+      apparmor-kernel-patches
+      libapparmor
+    ];
 
-    # whether to enable the AppArmor cache
-    # in /var/cache/apparmore
-    enableCache = true;
+    # apparmor configuration
+    security.apparmor = {
+      enable = true;
 
-    # whether to kill processes which have an AppArmor profile enabled
-    # but are not confined
-    killUnconfinedConfinables = true;
+      # whether to enable the AppArmor cache
+      # in /var/cache/apparmore
+      enableCache = true;
 
-    # packages to be added to AppArmor’s include path
-    packages = [pkgs.apparmor-profiles];
+      # whether to kill processes which have an AppArmor profile enabled
+      # but are not confined (AppArmor can only confine new processes)
+      killUnconfinedConfinables = true;
 
-    # apparmor policies
-    policies = {
-      "default_deny" = {
-        enforce = false;
-        enable = false;
-        profile = ''
-          profile default_deny /** { }
-        '';
+      # packages to be added to AppArmor’s include path
+      packages = [pkgs.apparmor-profiles];
+
+      # apparmor policies
+      policies = {
+        "default_deny" = {
+          enforce = false;
+          enable = false;
+          profile = ''
+            profile default_deny /** { }
+          '';
+        };
+
+        "sudo" = {
+          enforce = false;
+          enable = false;
+          profile = ''
+            ${pkgs.sudo}/bin/sudo {
+              file /** rwlkUx,
+            }
+          '';
+        };
+
+        "nix" = {
+          enforce = false;
+          enable = false;
+          profile = ''
+            ${config.nix.package}/bin/nix {
+              unconfined,
+            }
+          '';
+        };
       };
 
-      "sudo" = {
-        enforce = false;
-        enable = false;
-        profile = ''
-          ${pkgs.sudo}/bin/sudo {
-            file /** rwlkUx,
-          }
-        '';
-      };
-
-      "nix" = {
-        enforce = false;
-        enable = false;
-        profile = ''
-          ${config.nix.package}/bin/nix {
-            unconfined,
-          }
-        '';
-      };
+      # TODO: includes
     };
-
-    # TODO: includes
   };
 }
