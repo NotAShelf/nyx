26a26acf936ec294b8767d3e083deea97730841c
NotAShelf <raf@notashelf.dev>
2024-03-06 20:07:36 +0300

    os/users: tweak root and builder accounts

--- modules/core/common/system/os/users/nix-builder.nix
+++ modules/core/common/system/os/users/nix-builder.nix
@@ -1,14 +1,17 @@
-_: {
-  users.users.nix-builder = {
-    isSystemUser = true;
-    description = "Nix Remote Build";
-    home = "/var/tmp/nix-builder";
-    createHome = true;
-    useDefaultShell = true;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3Oglg7aVYQJzGa4JhnvDhRYx0jkLHwDT/9IyiLUNS2"
-    ];
-    group = "nix";
+{
+  users = {
+    groups.nix = {};
+
+    users.nix-builder = {
+      useDefaultShell = true;
+      isSystemUser = true;
+      createHome = true;
+      group = "nix";
+      home = "/var/tmp/nix-builder";
+      openssh.authorizedKeys = {
+        keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3Oglg7aVYQJzGa4JhnvDhRYx0jkLHwDT/9IyiLUNS2 notashelf@enyo"];
+        # keyFiles = []; # TODO: can this be used with agenix?
+      };
+    };
   };
-  users.groups.nix = {};
 }
--- modules/core/common/system/os/users/root.nix
+++ modules/core/common/system/os/users/root.nix
@@ -1,11 +1,3 @@
-_: {
-  users.users.root = {
-    # I am *not* locking myself out of the system again
-    # thank you very much.
-    initialPassword = "changeme";
-
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABG2T60uEoq4qTZtAZfSBPtlqWs2b4V4O+EptQ6S/ru"
-    ];
-  };
+{
+  users.users.root.hashedPassword = "*"; # lock root account
 }
