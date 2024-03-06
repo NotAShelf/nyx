69348eed9093473401039b21d7946a778e5a66a6
NotAShelf <raf@notashelf.dev>
2024-03-06 21:21:12 +0300

    services/adb: add android udev rules

--- modules/core/roles/workstation/system/services/adb.nix
+++ modules/core/roles/workstation/system/services/adb.nix
@@ -1,8 +1,12 @@
-{
+{pkgs, ...}: {
   programs.adb.enable = true;
 
-  services = {
-    udev.extraRules = ''
+  services.udev = {
+    packages = [
+      pkgs.android-udev-rules
+    ];
+
+    extraRules = ''
       # add my android device to adbusers
       SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", MODE="0666", GROUP="adbusers"
     '';
