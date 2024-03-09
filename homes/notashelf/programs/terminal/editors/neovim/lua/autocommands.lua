2a37e99e15bda89ab417b5bc780c9252828f7ea5
NotAShelf <raf@notashelf.dev>
2024-03-09 06:48:10 +0300

    editors/neovim: clean up autocmds

--- homes/notashelf/programs/terminal/editors/neovim/lua/autocommands.lua
+++ homes/notashelf/programs/terminal/editors/neovim/lua/autocommands.lua
@@ -1,9 +1,12 @@
+-- alias for vim.api.nvim_create_autocmd
+local create_autocmd = vim.api.nvim_create_autocmd
+
 -- taken from https://github.com/sitiom/nvim-numbertoggle
 -- I would much rather avoid fetching yet another plugin for something
 -- that should be done locally - and not as a plugin
 local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
 
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
 	pattern = "*",
 	group = augroup,
 	callback = function()
@@ -13,7 +16,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "Cmdline
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
 	pattern = "*",
 	group = augroup,
 	callback = function()
@@ -21,5 +24,23 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEn
 			vim.opt.relativenumber = false
 			vim.cmd "redraw"
 		end
+	end,
+})
+
+
+-- enable spell checking & line wrapping
+-- for git commit messages
+create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+-- Highlight yank after yanking
+create_autocmd({ "TextYankPost" }, {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
 	end,
 })
--- homes/notashelf/programs/terminal/editors/neovim/lua/misc.lua
+++ homes/notashelf/programs/terminal/editors/neovim/lua/misc.lua
@@ -1,2 +1,4 @@
+-- disables "how to disable mouse" message
+-- in right click popups
 vim.cmd.aunmenu [[PopUp.How-to\ disable\ mouse]]
 vim.cmd.aunmenu [[PopUp.-1-]]
