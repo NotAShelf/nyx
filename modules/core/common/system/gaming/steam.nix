cda9ce6d7a933a352847cbdac007b7af0e78f256
NotAShelf <raf@notashelf.dev>
2024-03-12 17:50:18 +0300

    modules: deprecate local and nix-gaming sourced steamCompat modules

--- modules/core/common/system/gaming/steam.nix
+++ modules/core/common/system/gaming/steam.nix
@@ -7,7 +7,6 @@
 }: let
   cfg = config.modules.system.programs;
 in {
-  imports = [inputs.nix-gaming.nixosModules.steamCompat];
   config = lib.mkIf cfg.gaming.enable {
     nixpkgs = {
       config = {
@@ -58,17 +57,17 @@ in {
       ];
     };
 
-    # enable steam
     programs.steam = {
+      # enable steam
       enable = true;
+
       # Open ports in the firewall for Steam Remote Play
       remotePlay.openFirewall = true;
+
       # Open ports in the firewall for Source Dedicated Server
       dedicatedServer.openFirewall = false;
+
       # Compatibility tools to install
-      # this option used to be provided by modules/shared/nixos/steam
-      # I removed it while porting it to nix-gaming
-      # withProtonGE = true;
       extraCompatPackages = [
         inputs.nix-gaming.packages.${pkgs.system}.proton-ge
       ];
