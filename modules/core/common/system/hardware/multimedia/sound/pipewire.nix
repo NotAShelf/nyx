f2c2ddef5acf37fdb82980fbb23a3d02944e22b2
NotAShelf <raf@notashelf.dev>
2024-03-14 06:53:08 +0300

    sound/pipewire: drop nix-gaming low-latency modulee

--- modules/core/common/system/hardware/multimedia/sound/pipewire.nix
+++ modules/core/common/system/hardware/multimedia/sound/pipewire.nix
@@ -1,35 +1,41 @@
 {
-  inputs,
   config,
-  lib,
   pkgs,
+  lib,
   ...
 }: let
-  inherit (lib) mkIf isx86Linux;
+  inherit (lib) isx86Linux;
+  inherit (lib.modules) mkIf;
+  inherit (lib.lists) optionals;
+  inherit (lib.generators) toLua;
 
   cfg = config.modules.system.sound;
   dev = config.modules.device;
 in {
-  imports = [inputs.nix-gaming.nixosModules.pipewireLowLatency];
   config = mkIf (cfg.enable && dev.hasSound) {
-    # if for some reason pipewire is disabled, we may enable pulseaudio as backup
-    # I don't like PA, but I won't discard it altogether
+    # if the device advertises sound enabled, and pipewire is disabled
+    # for whatever reason, we may fall back to PulseAudio to ensure
+    # that we still have audio. I do not like PA, but bad audio
+    # is better than no audio. Though we should always use
+    # PipeWire where available
     hardware.pulseaudio.enable = !config.services.pipewire.enable;
 
     # able to change scheduling policies, e.g. to SCHED_RR
+    # sounds server use RealtimeKit (rtkti) to acquire
+    # realtime priority
     security.rtkit.enable = config.services.pipewire.enable;
 
+    # enable pipewire and configure it for low latency
+    # the below configuration may not fit every use case
+    # and you are recommended to experiment with the values
+    # in order to find the perfect configuration
     services = {
-      pipewire = {
+      pipewire = let
+        quantum = 64;
+        rate = 48000;
+        qr = "${toString quantum}/${toString rate}";
+      in {
         enable = true;
-
-        lowLatency = {
-          enable = true;
-          # defaults
-          # the values below are the defaults, but I'm just putting them here for reference
-          quantum = 64;
-          rate = 48000;
-        };
 
         # emulation layers
         audio.enable = true;
@@ -40,19 +46,78 @@ in {
           support32Bit = isx86Linux pkgs; # if we're on x86 linux, we can support 32 bit
         };
 
-        # session manager
-        wireplumber = {
-          inherit (config.services.pipewire) enable;
-          configPackages = lib.optionals dev.hasBluetooth [
-            (pkgs.writeTextDir "share/bluetooth.lua.d/51-bluez-config.lua" ''
-              bluez_monitor.properties = {
-                ["bluez5.enable-sbc-xq"] = true,
-                ["bluez5.enable-msbc"] = true,
-                ["bluez5.enable-hw-volume"] = true,
-                ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        extraConfig.pipewire."99-lowlatency" = {
+          context = {
+            properties.default.clock.min-quantum = quantum;
+            modules = [
+              {
+                name = "libpipewire-module-rtkit";
+                flags = ["ifexists" "nofail"];
+                args = {
+                  nice.level = -15;
+                  rt = {
+                    prio = 88;
+                    time.soft = 200000;
+                    time.hard = 200000;
+                  };
+                };
               }
-            '')
-          ];
+              {
+                name = "libpipewire-module-protocol-pulse";
+                args = {
+                  server.address = ["unix:native"];
+                  pulse.min = {
+                    req = qr;
+                    quantum = qr;
+                    frag = qr;
+                  };
+                };
+              }
+            ];
+
+            stream.properties = {
+              node.latency = qr;
+              resample.quality = 1;
+            };
+          };
+        };
+
+        wireplumber = {
+          enable = true;
+          configPackages = let
+            # generate "matches" section of the rules
+            matches = toLua {
+              multiline = false; # looks better while inline
+              indent = false;
+            } [[["node.name" "matches" "alsa_output.*"]]]; # nested lists are to produce `{{{ }}}` in the output
+
+            # generate "apply_properties" section of the rules
+            apply_properties = toLua {} {
+              "audio.format" = "S32LE";
+              "audio.rate" = rate * 2;
+              "api.alsa.period-size" = 2;
+            };
+          in
+            [
+              (pkgs.writeTextDir "share/lowlatency.lua.d/99-alsa-lowlatency.lua" ''
+                alsa_monitor.rules = {
+                  {
+                    matches = ${matches};
+                    apply_properties = ${apply_properties};
+                  }
+                }
+              '')
+            ]
+            ++ optionals dev.hasBluetooth [
+              (pkgs.writeTextDir "share/bluetooth.lua.d/51-bluez-config.lua" ''
+                bluez_monitor.properties = {
+                  ["bluez5.enable-sbc-xq"] = true,
+                  ["bluez5.enable-msbc"] = true,
+                  ["bluez5.enable-hw-volume"] = true,
+                  ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+                }
+              '')
+            ];
         };
       };
     };
