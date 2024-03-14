6bcccefac9bcba9c3678e15c5edf50595016c074
NotAShelf <raf@notashelf.dev>
2024-03-14 06:52:35 +0300

    services/logrotate: configure rotation frequency

--- modules/core/common/system/os/services/logrotate.nix
+++ modules/core/common/system/os/services/logrotate.nix
@@ -4,9 +4,25 @@
   ...
 }: {
   services.logrotate.settings.header = {
+    # general
+    global = true;
+    dateext = true;
+    dateformat = "-%Y-%m-%d";
+    nomail = true;
+    missingok = true;
+    copytruncate = true;
+
+    # rotation frequency
+    priority = 1;
+    frequency = "weekly";
+    rotate = 7; # special value, means every 7 days
+    minage = 7; # avoid removing logs that are less than 7 days old
+
+    # compression
     compress = true; # lets compress logs to save space
     compresscmd = "${lib.getExe' pkgs.zstd "zstd"}";
     compressoptions = " -Xcompression-level 10";
     compressext = "zst";
+    uncompresscmd = "${lib.getExe' pkgs.zstd "unzstd"}";
   };
 }
