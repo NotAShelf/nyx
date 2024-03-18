d7d7fe7312353d12c6f3735e3c459885f4eb699c
NotAShelf <raf@notashelf.dev>
2024-03-18 13:25:24 +0300

    options/media: add history.lua to list of default extensions

--- modules/options/system/programs/media.nix
+++ modules/options/system/programs/media.nix
@@ -38,6 +38,20 @@ in {
           uosc # proximity UI
           quality-menu # ytdl-format quality menu
           seekTo # seek to spefici pos.
+          (pkgs.writeTextDir "share/history.lua" ''
+            local HISTFILE = (os.getenv('XDG_DATA_HOME') or os.getenv('HOME'))..'/mpv/history.log';
+
+            mp.register_event('file-loaded', function()
+                local title, fp;
+
+                title = mp.get_property('media-title');
+                title = (title == mp.get_property('filename') and "" or (' (%s)'):format(title));
+
+                fp = io.open(HISTFILE, 'a+');
+                fp:write(('[%s] %s%s\n'):format(os.date('%Y-%m-%d %X'), mp.get_property('path'), title));
+                fp:close();
+            end)
+          '')
         ];
       };
     };
