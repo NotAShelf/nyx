4fb187919820d8b781cf026f7a4fe73c21375c65
NotAShelf <raf@notashelf.dev>
2024-03-18 21:16:48 +0300

    options/media: get mpv-history script from `nyxpkgs`

--- modules/options/system/programs/media.nix
+++ modules/options/system/programs/media.nix
@@ -1,4 +1,5 @@
 {
+  inputs',
   pkgs,
   lib,
   ...
@@ -38,20 +39,7 @@ in {
           uosc # proximity UI
           quality-menu # ytdl-format quality menu
           seekTo # seek to spefici pos.
-          (pkgs.writeTextDir "share/history.lua" ''
-            local HISTFILE = (os.getenv('XDG_DATA_HOME') or os.getenv('HOME'))..'/mpv/history.log';
-
-            mp.register_event('file-loaded', function()
-                local title, fp;
-
-                title = mp.get_property('media-title');
-                title = (title == mp.get_property('filename') and "" or (' (%s)'):format(title));
-
-                fp = io.open(HISTFILE, 'a+');
-                fp:write(('[%s] %s%s\n'):format(os.date('%Y-%m-%d %X'), mp.get_property('path'), title));
-                fp:close();
-            end)
-          '')
+          inputs'.nyxpkgs.packages.mpv-history # save a history of played files with timestamps
         ];
       };
     };
