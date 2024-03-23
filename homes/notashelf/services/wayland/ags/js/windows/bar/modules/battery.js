616d285d6bd7cc0166534049c9b755742da6e572
NotAShelf <raf@notashelf.dev>
2024-03-23 03:17:16 +0300

    services/ags: add percentage to battery tooltip

--- homes/notashelf/services/wayland/ags/js/windows/bar/modules/battery.js
+++ homes/notashelf/services/wayland/ags/js/windows/bar/modules/battery.js
@@ -1,17 +1,66 @@
 import { Widget, Battery } from "../../../imports.js";
-import { getBatteryTime, getBatteryIcon } from "../../../utils/battery.js";
-const { Box, Label } = Widget;
+import {
+    getBatteryPercentage,
+    getBatteryTooltip,
+    getBatteryIcon,
+} from "../../../utils/battery.js";
+const { Button, Box, Label, Revealer } = Widget;
 
 const BatIcon = () =>
     Label({ className: "batIcon" })
         // NOTE: label needs to be used instead of icon here
         .bind("label", Battery, "available", getBatteryIcon)
-        .bind("tooltip-text", Battery, "percent", getBatteryTime); // TODO: add battery percentage in here
+        .bind("tooltip-text", Battery, "percent", getBatteryTooltip);
+
+const BatStatus = () =>
+    Revealer({
+        transition: "slide_down",
+        transition_duration: 200,
+        child: Label().bind("label", Battery, "percent", getBatteryPercentage),
+    });
 
 export const BatteryWidget = () =>
+    Button({
+        onPrimaryClick: (self) => {
+            self.child.children[1].revealChild =
+                !self.child.children[1].revealChild;
+        },
+        child: Box({
+            className: "battery",
+            cursor: "pointer",
+            vertical: true,
+            children: [BatIcon(), BatStatus()],
+            visible: Battery.bind("available"),
+        }),
+    });
+
+/*
     Box({
         className: "battery",
         cursor: "pointer",
-        child: BatIcon(),
+        vertical: true,
+        children: [BatIcon(), BatStatus()],
         visible: Battery.bind("available"),
     });
+*/
+
+/*
+Button({
+        className: name + "Button",
+        onPrimaryClick: onPrimary,
+        child: Box({
+            className: name,
+            vertical: true,
+            children: [
+                CircularProgress({
+                    className: name + "Progress",
+                    // binds: [["value", process]],
+                    rounded: true,
+                    inverted: false,
+                    startAt: 0.27,
+                }).bind("value", process),
+                ...extraChildren,
+            ],
+        }),
+    });
+*/
