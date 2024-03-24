e8f34917a15f6698db22dc9263cf32660c582d46
NotAShelf <raf@notashelf.dev>
2024-03-24 17:40:44 +0300

    hosts/enyo: add zstd compression for modules; enable cpu freq statistics

--- hosts/enyo/kernel.nix
+++ hosts/enyo/kernel.nix
@@ -3,6 +3,9 @@
   inherit (lib.attrsets) mapAttrs;
   inherit (lib.modules) mkForce;
 in {
+  # after booting to the new kernel
+  # use zcat /proc/config.gz | grep -i "<value>"
+  # to check if the kernel options are set correctly
   boot.kernelPatches = [
     {
       # enable lockdown LSM
@@ -21,6 +24,7 @@ in {
         # enable compiler optimizations for AMD
         MNATIVE_AMD = yes;
         X86_USE_PPRO_CHECKSUM = yes;
+        X86_AMD_PSTATE = yes;
 
         X86_EXTENDED_PLATFORM = no; # disable support for other x86 platforms
         X86_MCE_INTEL = no; # disable support for intel mce
@@ -34,12 +38,22 @@ in {
         # so it should be set if and only if the kernel package
         # is not xanmod under sys.boot
         # CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
+        CPU_FREQ_STAT = yes;
       };
 
       # enable only support for upto 32 CPU threads in the kernel
       # this host has 16 cores and 32 threads
       extraConfig = ''
         NR_CPUS 32
+      '';
+    }
+    {
+      name = "module-compression";
+      patch = null;
+      extraConfig = ''
+        KERNEL_ZSTD y
+        MODULE_COMPRESS_XZ n
+        MODULE_COMPRESS_ZSTD y
       '';
     }
   ];
