5e74b714d6da4946cb851c1b847464bfe2be033c
NotAShelf <raf@notashelf.dev>
2024-03-27 13:55:10 +0300

    gaming/gamescope: use the gamescope package option for the wrapper

--- modules/core/common/system/gaming/gamescope.nix
+++ modules/core/common/system/gaming/gamescope.nix
@@ -1,14 +1,19 @@
-{pkgs, ...}: {
-  config = {
-    security.wrappers = {
-      gamescope = {
-        owner = "root";
-        group = "root";
-        source = "${pkgs.gamescope}/bin/gamescope";
-        capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
-      };
-    };
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.gamescope = {
+    enable = true;
+    package = pkgs.gamescope; # the default, here in case I want to override it
+  };
 
-    programs.gamescope.enable = true;
+  # workaround attempt for letting gamescope bypass YAMA LSM
+  # doesn't work, but doesn't hurt to keep this here
+  security.wrappers.gamescope = {
+    owner = "root";
+    group = "root";
+    source = "${config.programs.gamescope.package}/bin/gamescope";
+    capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
   };
 }
