87856b90ef50afcdc5a6f7cc48e360262e1e0eae
NotAShelf <raf@notashelf.dev>
2024-03-28 12:18:23 +0300

    lib/xdg: fix typo in state_dir

--- lib/xdg/template.nix
+++ lib/xdg/template.nix
@@ -3,7 +3,7 @@ system: let
   XDG_CONFIG_HOME = "$HOME/.config";
   XDG_CACHE_HOME = "$HOME/.cache";
   XDG_DATA_HOME = "$HOME/.local/share";
-  XDG_STATE_HOME = "$HOME/.local/shate";
+  XDG_STATE_HOME = "$HOME/.local/state";
   XDG_BIN_HOME = "$HOME}/.local/bin";
   XDG_RUNTIME_DIR = "/run/user/$UID";
 in {
