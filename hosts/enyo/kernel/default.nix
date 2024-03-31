54ae2effe0db7ab955504d6b53180a21339bf043
NotAShelf <raf@notashelf.dev>
2024-03-31 13:25:34 +0300

    hosts/enyo: call custom kernel as a package

--- hosts/enyo/kernel/default.nix
+++ hosts/enyo/kernel/default.nix
@@ -1,6 +1,8 @@
-{
-  imports = [
-    ./config
-    ./package.nix
-  ];
+{pkgs, ...}: let
+  inherit (pkgs.callPackage ./package.nix {}) xanmod_custom;
+in {
+  imports = [./config];
+  config = {
+    modules.system.boot.kernel = pkgs.linuxPackagesFor xanmod_custom;
+  };
 }
--- hosts/enyo/kernel/package.nix
+++ hosts/enyo/kernel/package.nix
@@ -1,45 +1,51 @@
 {
-  pkgs,
   lib,
+  fetchFromGitHub,
+  linuxKernel,
+  ccacheStdenv,
+  buildPackages,
   ...
 }: let
   inherit (lib.kernel) yes no;
-  inherit (lib.attrsets) mapAttrs;
+  inherit (lib.attrsets) mapAttrs recursiveUpdate;
   inherit (lib.modules) mkForce;
-in {
-  modules.system.boot.kernel = let
-    version = "6.8.2";
-    suffix = "xanmod1";
-    modDirVersion = "${version}-${suffix}";
-  in
-    pkgs.linuxKernel.kernels.linux_xanmod_latest.override {
-      inherit version suffix modDirVersion;
 
-      # https://github.com/xanmod/linux
-      src = pkgs.fetchFromGitHub {
-        owner = "xanmod";
-        repo = "linux";
-        rev = "refs/tags/${version}-xanmod1";
-        hash = "sha256-JddPg/EWJZq5EIemcaULM5c6yLGkfb2E6shxxq37N3M=";
-      };
+  version = "6.8.2";
+  suffix = "xanmod1";
+  modDirVersion = "${version}-${suffix}";
 
-      # poor attempt to make kernel builds use ccache
-      stdenv = pkgs.ccacheStdenv;
-      buildPackages = pkgs.buildPackages // {stdenv = pkgs.ccacheStdenv;};
+  xanmod_custom = linuxKernel.kernels.linux_xanmod_latest.override {
+    inherit version suffix modDirVersion;
 
-      extraMakeFlags = ["KCFLAGS=-DAMD_PRIVATE_COLOR"];
-      ignoreConfigErrors = true;
-
-      # after booting to the new kernel
-      # use zcat /proc/config.gz | grep -i "<value>"
-      # to check if the kernel options are set correctly
-      extraStructuredConfig = mapAttrs (_: mkForce) {
-        EXPERT = yes;
-        DEBUG_KERNEL = no;
-        WERROR = no;
-
-        GCC_PLUGINS = yes;
-        BUG_ON_DATA_CORRUPTION = yes;
-      };
+    # https://github.com/xanmod/linux
+    src = fetchFromGitHub {
+      owner = "xanmod";
+      repo = "linux";
+      rev = "refs/tags/${version}-xanmod1";
+      hash = "sha256-JddPg/EWJZq5EIemcaULM5c6yLGkfb2E6shxxq37N3M=";
     };
+
+    # poor attempt to make kernel builds use ccache
+    stdenv = ccacheStdenv;
+    buildPackages = recursiveUpdate buildPackages {
+      stdenv = ccacheStdenv;
+    };
+
+    extraMakeFlags = ["KCFLAGS=-DAMD_PRIVATE_COLOR"];
+    ignoreConfigErrors = true;
+
+    # after booting to the new kernel
+    # use zcat /proc/config.gz | grep -i "<value>"
+    # to check if the kernel options are set correctly
+    extraStructuredConfig = mapAttrs (_: mkForce) {
+      EXPERT = yes;
+      DEBUG_KERNEL = no;
+      WERROR = no;
+
+      GCC_PLUGINS = yes;
+      BUG_ON_DATA_CORRUPTION = yes;
+    };
+  };
+in {
+  inherit xanmod_custom;
 }
