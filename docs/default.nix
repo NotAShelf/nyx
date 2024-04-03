a226e2b5c4d4e0b3e6a6d6b98fef07fc175a1d84
NotAShelf <raf@notashelf.dev>
2024-04-03 17:07:32 +0300

    docs: merge generation scripts

--- docs/default.nix
+++ docs/default.nix
@@ -1,19 +1,11 @@
 {
   systems = ["x86_64-linux"];
   perSystem = {pkgs, ...}: let
-    inherit (pkgs) callPackage runCommandLocal;
-
-    gen-notes = callPackage ./tools/gen.nix {};
-    gen-rss = callPackage ./tools/rss.nix {};
+    inherit (pkgs) runCommandLocal;
   in {
     packages = {
-      inherit gen-notes gen-rss;
-
       static-notes = runCommandLocal "static-notes" {} ''
         mkdir -p $out
-
-        ${gen-notes}/bin/generate-static-notes
-        ${gen-rss}/bin/generate-rss
       '';
     };
   };
---
+++ docs/gen.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+workingdir="$(pwd)"
+outdir="out"
+posts_dir="$outdir/posts"
+json_file="$outdir/posts.json"
+rss_file="$outdir/feed.xml"
+
+rss_title="NotAShelf/nyx"
+rss_link="https://notashelf.github.io/nyx/"
+rss_description="NotAShelf's notes on various topics"
+
+create_directory() {
+  if [ ! -d "$1" ]; then
+    echo "Creating directory: $1"
+    mkdir -p "$1"
+  fi
+}
+
+generate_json() {
+  echo "Generating JSON..."
+  json='{"posts":['
+  first=true
+  for file in "$1"/notes/*.md; do
+    filename=$(basename "$file")
+    if [[ $filename != "*-todo.md" && $filename != "cheatsheet.md" && $filename != "README.md" ]]; then
+      if [[ $filename =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2} ]]; then
+        if [ "$first" = true ]; then
+          first=false
+        else
+          json="$json,"
+        fi
+        json="$json{\"title\":\"$filename\",\"url\":\"posts/$(basename "$file" .md).html\"}"
+      fi
+    fi
+  done
+  json="$json]}"
+  echo "$json" >"$2"
+}
+
+generate_index_page() {
+  echo "Generating index page..."
+  pandoc --standalone \
+    --from markdown --to html \
+    --template "$1"/template.html \
+    --css "$1"/style.css \
+    --variable="index:true" \
+    "$1/notes/README.md" -o "$2/index.html"
+}
+
+generate_other_pages() {
+  echo "Generating other pages..."
+  for file in "$1"/notes/*.md; do
+    filename=$(basename "$file")
+    if [[ $filename != "*-todo.md" && $filename != "cheatsheet.md" && $filename != "README.md" ]]; then
+      if [[ $filename =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2} ]]; then
+        pandoc --from markdown --to html \
+          --standalone --template "$2"/template.html \
+          --css "$2"/style.css \
+          "$file" -o "$3/posts/$(basename "$file" .md).html"
+      else
+        pandoc --from markdown --to html \
+          --standalone --template "$2"/template.html \
+          --css "$2"/style.css \
+          "$file" -o "$3/$(basename "$file" .md).html"
+      fi
+    fi
+  done
+}
+
+generate_rss_feed() {
+  echo "Generating RSS feed..."
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+  <rss version=\"2.0\">
+  <channel>
+  <title>$1</title>
+  <link>$2</link>
+  <description>$3</description>" >"$4"
+
+  jq -r '.posts[] | select(.url != null) | "<item><title>\(.title)</title><link>\(.url)</link><description></description></item>"' "$5" >>"$4"
+
+  echo "</channel>
+  </rss>" >>"$4"
+}
+
+create_directory "$outdir"
+create_directory "$posts_dir"
+generate_json "$workingdir" "$json_file"
+generate_index_page "$workingdir" "$outdir"
+generate_other_pages "$workingdir" "$workingdir" "$outdir"
+generate_rss_feed "$rss_title" "$rss_link" "$rss_description" "$rss_file" "$json_file"
+
+echo "All tasks completed successfully."
