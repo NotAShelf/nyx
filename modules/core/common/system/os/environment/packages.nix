cf324b8908399033b4d7e634dd71e4cd9e5f8c62
NotAShelf <raf@notashelf.dev>
2024-04-05 11:12:42 +0300

    environment/packages: describe `defaultPackages` and `systemPackages` better

--- modules/core/common/system/os/environment/packages.nix
+++ modules/core/common/system/os/environment/packages.nix
@@ -4,11 +4,15 @@
   ...
 }: {
   environment = {
-    # disable all packages installed by default, so that my system doesn't have anything
-    # that I myself haven't added
+    # nixos ships a bunch of packages by default under environment.defaultPackages
+    # those do not add much to the system closure, but for a little added extra security
+    # and in an attempt to reduce my system closure size, I would like those to be
+    # removed from my packages
     defaultPackages = lib.mkForce [];
 
-    # packages I want pre-installed on all systems
+    # packages that will be shared across all users and and all systems
+    # this should generally include tools used for debugging
+    # or system administration
     systemPackages = with pkgs; [
       git
       curl
