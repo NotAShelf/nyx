cd2bbf7514852956dc919af4bda6bc340e22857c
NotAShelf <raf@notashelf.dev>
2024-04-09 22:00:59 +0300

    editors/neovim: nvf v0.6 fix for nvim-session-manager setupOpts

--- homes/notashelf/programs/terminal/editors/neovim/plugins/settings/session.nix
+++ homes/notashelf/programs/terminal/editors/neovim/plugins/settings/session.nix
@@ -2,7 +2,7 @@
   programs.neovim-flake.settings.vim = {
     session.nvim-session-manager = {
       enable = false;
-      autoloadMode = "Disabled"; # misbehaves with dashboard
+      setupOpts.autoload_mode = "Disabled"; # misbehaves with dashboard
     };
   };
 }
