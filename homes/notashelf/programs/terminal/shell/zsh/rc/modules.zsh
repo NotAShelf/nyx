7e86338564f1f2b35d824d5796aed704b6b8abb1
NotAShelf <raf@notashelf.dev>
2024-04-09 23:31:00 +0300

    shell/zsh: describe loaded modules

--- homes/notashelf/programs/terminal/shell/zsh/rc/modules.zsh
+++ homes/notashelf/programs/terminal/shell/zsh/rc/modules.zsh
@@ -1,8 +1,11 @@
 # zsh Line Editor (ZLE) module
+# interactive line editing in the shell
 zmodload zsh/zle
 
 # zsh pseudo-terminal (PTY) module
+# control interactive processes
 zmodload zsh/zpty
 
 # zsh completion list (complist) module
+# manage and display completion lists
 zmodload zsh/complist
