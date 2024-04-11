496255ec6b4e883dc47f6cb9a3e110dc213494bb
NotAShelf <raf@notashelf.dev>
2024-04-11 22:38:18 +0300

    flake/apps: shellcheck

--- flake/apps/default.nix
+++ flake/apps/default.nix
@@ -8,11 +8,11 @@
         program = writeShellApplication {
           name = "nix-flake-check";
           text = ''
-            booted=$(readlink -f /run/booted-system/kernel)
-            current=$(readlink -f /run/current-system/kernel)
+            booted="$(readlink -f /run/booted-system/kernel)"
+            current="$(readlink -f /run/current-system/kernel)"
 
-            booted_kernel=$(basename $booted)
-            current_kernel=$(basename $current)
+            booted_kernel=$(basename "$booted")
+            current_kernel=$(basename "$current")
 
             if [[ "$booted_kernel" != "$current_kernel" ]]; then
                 echo "Restart required!"
