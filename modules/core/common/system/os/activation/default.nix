f37c515670fc395f480a1a83657e08adaaef89b6
NotAShelf <raf@notashelf.dev>
2024-04-12 18:44:23 +0300

    os/activation: get mkIf from lib.modules

--- modules/core/common/system/os/activation/default.nix
+++ modules/core/common/system/os/activation/default.nix
@@ -3,11 +3,13 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  inherit (lib.modules) mkIf;
+in {
   system.activationScripts = {
     # if system declares that it wants closure diffs, then run the diff script on activation
     # this is useless if you are using nh, which does this for you in a different way
-    diff = lib.mkIf config.modules.system.activation.diffGenerations {
+    diff = mkIf config.modules.system.activation.diffGenerations {
       supportsDryActivation = true;
       text = ''
         if [[ -e /run/current-system ]]; then
