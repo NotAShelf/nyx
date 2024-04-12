958113bc5f24bed0ed793ce69edaa9c77c0abb57
NotAShelf <raf@notashelf.dev>
2024-04-12 15:45:59 +0300

    apps/webcord: update catppuccin theme file

--- homes/notashelf/programs/graphical/apps/webcord/default.nix
+++ homes/notashelf/programs/graphical/apps/webcord/default.nix
@@ -13,8 +13,8 @@
   catppuccin-mocha = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "discord";
-    rev = "20abe29b3f0f7c59c4878b1bf6ceae41aeac9afd";
-    hash = "sha256-Gjrv1VayPfjcsfSmGJdJTA8xEX6gXhpgTLJ2xrSNcEo=";
+    rev = "fa735cd9a89732f4b27ff14309c2af1581622ae5";
+    hash = "sha256-1H5l2E0VBUL1/k7tf21gXGVT158koARug/KeLixCukU=";
   };
 in {
   config = mkIf prg.webcord.enable {
