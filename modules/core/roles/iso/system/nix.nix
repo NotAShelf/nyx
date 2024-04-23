46f016c0c801b422f067d55f4dc785f33696ab6d
NotAShelf <raf@notashelf.dev>
2024-04-23 12:46:18 +0300

    roles/iso: disable channels

--- modules/core/roles/iso/system/nix.nix
+++ modules/core/roles/iso/system/nix.nix
@@ -1,5 +1,9 @@
 {config, ...}: {
   nix = {
+    channel.enable = false;
+    nixPath = ["nixpkgs=${config.nix.registry.nixpkgs.to.path}"];
+
+    # settings
     settings = {
       experimental-features = [
         "nix-command"
@@ -14,7 +18,6 @@
       auto-optimise-store = true;
 
       flake-registry = "";
-      nixPath = ["nixpkgs=${config.nix.registry.nixpkgs.to.path}"];
     };
   };
 }
