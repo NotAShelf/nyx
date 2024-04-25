2f99fe4cecf0d0d8c91bb005c80cb69a444a764e
NotAShelf <raf@notashelf.dev>
2024-04-25 06:19:33 +0300

    flake: clean up args

--- flake/args.nix
+++ flake/args.nix
@@ -1,4 +1,28 @@
-{inputs, ...}: {
+{
+  inputs,
+  self,
+  ...
+}: let
+  # an extension of nixpkgs.lib, containing my custom functions
+  # used within the flake. for convenience and to allow those functions
+  # to be used in other flake expressions, it has been added to a custom
+  # `lib` output from the flake.
+  extendedLib = import (self + /lib) {
+    inherit inputs;
+  };
+
+  # add `pins` to self so that the flake may refer it freely
+  # pins can be updated via `npins update` in current directory
+  # which will automatically bump all sources in the `npins`
+  # directory.
+  pinnedSources = import ./npins;
+
+  # add a collection of SSH keys to the keys so that
+  #  1. my public keys are more easily obtainable from outside
+  #  2. it's easy to share key names and values internally especially
+  #  for setting them for users and such
+  publicKeys = import ./keys;
+in {
   perSystem = {
     config,
     system,
@@ -8,32 +32,26 @@
       {
         _module.args = {
           pkgs = config.legacyPackages;
-          pins = import ./npins;
+          lib = extendedLib;
+          pins = pinnedSources;
+          keys = publicKeys;
         };
       }
     ];
 
     legacyPackages = import inputs.nixpkgs {
       inherit system;
-      config.allowUnfree = true;
-      config.allowUnsupportedSystem = true;
       overlays = [];
+      config = {
+        allowUnfree = true;
+        allowUnsupportedSystem = true;
+      };
     };
   };
 
   flake = {
-    # an extension of nixpkgs.lib, containing my custom functions
-    # used within the flake. for convenience and to allow those functions
-    # to be used in other flake expressions, it has been added to a custom
-    # `lib` output from the flake.
-    lib = import (inputs.self + /lib) {inherit inputs;};
-
-    # add `pins` to self so that the flake may refer it freely
-    pins = import ./npins;
-
-    # add a collection of SSH keys to the keys so that
-    #  1. my public keys are more easily obtainable from outside
-    #  2. it's easy to share key names and values internally, for users and such
-    keys = import ./keys;
+    lib = extendedLib;
+    pins = pinnedSources;
+    keys = publicKeys;
   };
 }
