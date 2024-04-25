febcb405e6e64a07ab2e8acc9b9ecec678804ed4
NotAShelf <raf@notashelf.dev>
2024-04-25 06:19:05 +0300

    editors/neovim: audotread buffer state

---
+++ homes/notashelf/programs/terminal/editors/neovim/lua/autoread.lua
@@ -0,0 +1,13 @@
+-- If the cursor has been idle for some time, check if the current buffer
+-- has been modified externally. prompt the user to reload it if has.
+local bufnr = vim.api.nvim_get_current_buf()
+
+-- luacheck: ignore
+vim.opt_local.autoread = true
+vim.api.nvim_create_autocmd("CursorHold", {
+	group = vim.api.nvim_create_augroup("Autoread", { clear = true }),
+	buffer = bufnr,
+	callback = function()
+		vim.cmd("silent! checktime")
+	end,
+})
