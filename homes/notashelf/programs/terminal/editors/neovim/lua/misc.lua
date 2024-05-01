04d18eb87db734e0586f077689c9adacf241af5a
NotAShelf <raf@notashelf.dev>
2024-05-01 21:33:48 +0300

    editors/neovim: add more abbreviations

--- homes/notashelf/programs/terminal/editors/neovim/lua/misc.lua
+++ homes/notashelf/programs/terminal/editors/neovim/lua/misc.lua
@@ -2,3 +2,19 @@
 -- in right click popups
 vim.cmd.aunmenu [[PopUp.How-to\ disable\ mouse]]
 vim.cmd.aunmenu [[PopUp.-1-]]
+
+local abbreviations = {
+	Wq = 'wq',
+	WQ = 'wq',
+	Wqa = 'wqa',
+	W = 'w',
+	Q = 'q',
+	Qa = 'qa',
+	Bd = 'bd',
+	E = 'e'
+}
+
+-- add more abbreviations
+for left, right in pairs(abbreviations) do
+	vim.cmd.cnoreabbrev(('%s %s'):format(left, right))
+end
