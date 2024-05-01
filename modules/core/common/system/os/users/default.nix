5f98cf55901d36a8833cbf7cd3691624bcdb9270
NotAShelf <raf@notashelf.dev>
2024-05-01 22:16:42 +0300

    os/users: set default user options

--- modules/core/common/system/os/users/default.nix
+++ modules/core/common/system/os/users/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   # we want to handle user configurations on a per-file basis
   # users that are not in users/<username>.nix don't get to be a real user
   imports = [
@@ -6,4 +6,12 @@
     ./nix-builder.nix
     ./root.nix
   ];
+
+  config = {
+    users = {
+      allowNoPasswordLogin = false;
+      defaultUserShell = pkgs.zsh;
+      enforceIdUniqueness = true;
+    };
+  };
 }
--- modules/core/common/system/os/users/notashelf.nix
+++ modules/core/common/system/os/users/notashelf.nix
@@ -1,6 +1,6 @@
 {
-  pkgs,
   config,
+  pkgs,
   lib,
   ...
 }: let
