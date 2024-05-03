f1b24a03f0b6a0d71b689ca1b6032db68b725302
NotAShelf <raf@notashelf.dev>
2024-05-03 20:31:26 +0300

    editors/neovim: add new entries to the right click menu

--- homes/notashelf/programs/terminal/editors/neovim/lua/misc.lua
+++ homes/notashelf/programs/terminal/editors/neovim/lua/misc.lua
@@ -3,6 +3,11 @@
 vim.cmd.aunmenu [[PopUp.How-to\ disable\ mouse]]
 vim.cmd.aunmenu [[PopUp.-1-]]
 
+vim.cmd.amenu([[PopUp.Inspect <Cmd>Inspect<CR>]])
+vim.cmd.amenu([[PopUp.Telescope <Cmd>Telescope<CR>]])
+vim.cmd.amenu([[PopUp.Code\ action <Cmd>lua vim.lsp.buf.code_action()<CR>]])
+vim.cmd.amenu([[PopUp.LSP\ Hover <Cmd>lua vim.lsp.buf.hover()<CR>]])
+
 local abbreviations = {
 	Wq = 'wq',
 	WQ = 'wq',
