7f5586fa8b787ff14997d2f018248dd33a5d1120
NotAShelf <raf@notashelf.dev>
2024-05-04 16:53:56 +0300

    flake/pkgs: remove modprobed-db
    It's in nixpkgs now

--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -5,7 +5,8 @@
   ];
 
   perSystem = {pkgs, ...}: let
-    inherit (pkgs) callPackage;
+    inherit (pkgs) callPackage writeScriptBin;
+    inherit (pkgs.writers) writePython3Bin;
   in {
     packages = {
       schizofox-startpage = callPackage ./startpage {};
@@ -14,8 +15,54 @@
       spotify-wrapped = callPackage ./spotify-wrapped.nix {};
       nicksfetch = callPackage ./nicksfetch.nix {};
       present = callPackage ./present.nix {};
-      modprobed-db = callPackage ./modprobed-db.nix {};
       nixfmt-rfc = callPackage ./nixfmt-rfc.nix {inherit inputs;};
+
+      build-all-hosts =
+        writePython3Bin "build-all-hosts" {
+          flakeIgnore = ["E501"];
+          libraries = with pkgs.python311Packages; [tqdm];
+        } ''
+          import subprocess
+          import json
+          from tqdm import tqdm
+
+          command = [
+              "nix",
+              "flake",
+              "show",
+              "--all-systems",
+              "--json"
+          ]
+
+          output = subprocess.run(
+              command,
+              capture_output=True,
+              text=True
+          )
+
+          data = json.loads(output.stdout)
+          configurations = data.get("nixosConfigurations", {}).keys()
+
+          # Calculate the total number of configurations
+          total_configurations = len(configurations)
+
+          # Initialize the progress bar
+          progress_bar = tqdm(total=total_configurations, desc="Building Hosts", unit="Host", ascii=True)
+
+          for configuration in configurations:
+              # Silence the output of nixos-rebuild build
+              subprocess.run(["nixos-rebuild", "build", "--flake", f".#{configuration}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+              # Update the progress bar
+              progress_bar.update(1)
+
+          # Close the progress bar
+          progress_bar.close()
+        '';
+
+      prefetch-url-sha256 = writeScriptBin "prefetch-url-sha256" ''
+        hash=$(nix-prefetch-url "$1")
+        nix hash to-sri --type sha256 $hash
+      '';
     };
   };
 }
