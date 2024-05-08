3ff41102374958a077bb63d3ec524fa8171538de
NotAShelf <raf@notashelf.dev>
2024-05-08 20:44:52 +0300

    hardware/sound: clean up low latency configuration

--- modules/core/common/system/hardware/multimedia/sound/pipewire.nix
+++ modules/core/common/system/hardware/multimedia/sound/pipewire.nix
@@ -7,7 +7,6 @@
   inherit (lib) isx86Linux;
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.lists) optionals;
-  inherit (lib.generators) toLua;
 
   cfg = config.modules.system.sound;
   dev = config.modules.device;
@@ -33,89 +32,67 @@ in {
     # the below configuration may not fit every use case
     # and you are recommended to experiment with the values
     # in order to find the perfect configuration
-    services = {
-      pipewire = let
-        quantum = 64;
-        rate = 48000;
-        qr = "${toString quantum}/${toString rate}"; # 64/48000
-      in {
+    services.pipewire = let
+      quantum = 64;
+      rate = 48000;
+      qr = "${toString quantum}/${toString rate}"; # 64/48000
+    in {
+      enable = true;
+
+      # Additional emulation layers to enable on top of PipeWire. The backwards
+      # compatibility provided by below options are impeccable and therefore
+      # I choose to keep them. On a minimal system, they can (and probably should)
+      # be omitted
+      audio.enable = true;
+      pulse.enable = true; # PulseAudio server emulation
+      jack.enable = true; # JACK audio emulation
+      alsa = {
         enable = true;
+        support32Bit = isx86Linux pkgs; # if we're on x86 linux, we can support 32 bit
+      };
 
-        # emulation layers
-        audio.enable = true;
-        pulse.enable = true; # PA server emulation
-        jack.enable = true; # JACK audio emulation
-        alsa = {
-          enable = true;
-          support32Bit = isx86Linux pkgs; # if we're on x86 linux, we can support 32 bit
-        };
-
-        extraConfig = {
-          # See <https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-PipeWire#quantum-ranges>
-          # Useful commands:
-          #  pw-top                                            # see live stats
-          #  journalctl -b0 --user -u pipewire                 # see logs (spa resync in "bad")
-          #  pw-metadata -n settings 0                         # see current quantums
-          #  pw-metadata -n settings 0 clock.force-quantum 128 # override quantum
-          #  pw-metadata -n settings 0 clock.force-quantum 0   # disable override
-          pipewire."92-low-latency" = {
-            context = {
-              properties.default.clock.min-quantum = quantum;
-              modules = [
-                {
-                  name = "libpipewire-module-rtkit";
-                  flags = ["ifexists" "nofail"];
-                  args = {
-                    nice.level = -15;
-                    rt = {
-                      prio = 88;
-                      time.soft = 200000;
-                      time.hard = 200000;
-                    };
-                  };
-                }
-                {
-                  name = "libpipewire-module-protocol-pulse";
-                  args = {
-                    server.address = ["unix:native"];
-                    pulse.min = {
-                      req = qr;
-                      quantum = qr;
-                      frag = qr;
-                    };
-                  };
-                }
-              ];
-
-              stream.properties = {
-                node.latency = qr;
-                resample.quality = 1;
-              };
+      # Additional configuration files that will be placed in /etc/pipewire/pipewire.conf.d/
+      # with the given file name. According to the documentation, those files take JSON therefore
+      # nixpkgs' toJSON should be suitable to write the configuration files via Nix expressions.
+      # P.S. Using extraConfig already converts the expression to JSON, so toJSON is not necessary
+      # Also see: <https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-PipeWire#quantum-ranges>
+      # Useful commands:
+      #  pw-top                                            # see live stats
+      #  journalctl -b0 --user -u pipewire                 # see logs (spa resync in "bad")
+      #  pw-metadata -n settings 0                         # see current quantums
+      #  pw-metadata -n settings 0 clock.force-quantum 128 # override quantum
+      #  pw-metadata -n settings 0 clock.force-quantum 0   # disable override
+      extraConfig = {
+        pipewire."92-low-latency" = {
+          context = {
+            properties.default.clock = {
+              inherit quantum rate;
+              max-quantum = quantum;
+              min-quantum = quantum;
+              allowed-rates = [rate];
             };
-          };
 
-          pipewire-pulse."92-low-latency" = {
-            context.modules = [
+            modules = [
               {
                 name = "libpipewire-module-rtkit";
                 flags = ["ifexists" "nofail"];
                 args = {
                   nice.level = -15;
-                  rt.prio = 88;
-                  rt.time.soft = 200000;
-                  rt.time.hard = 200000;
+                  rt = {
+                    prio = 88;
+                    time.soft = 200000;
+                    time.hard = 200000;
+                  };
                 };
               }
               {
                 name = "libpipewire-module-protocol-pulse";
                 args = {
                   server.address = ["unix:native"];
-                  pulse = {
-                    default.req = qr;
-                    min.req = qr;
-                    max.req = qr;
-                    min.quantum = qr;
-                    max.quantum = qr;
+                  pulse.min = {
+                    quantum = qr;
+                    req = qr;
+                    frag = qr;
                   };
                 };
               }
@@ -128,69 +105,97 @@ in {
           };
         };
 
-        wireplumber = {
-          enable = true;
-
-          extraConfig = mkMerge [
+        pipewire-pulse."92-low-latency" = {
+          context.modules = [
             {
+              name = "libpipewire-module-protocol-pulse";
+              args.pulse = {
+                min.req = qr;
+                default.req = qr;
+                max.req = qr;
+                min.quantum = qr;
+                max.quantum = qr;
+              };
+            }
+          ];
+
+          stream.properties = {
+            node.latency = qr;
+            resample.quality = 4;
+          };
+        };
+      };
+
+      wireplumber = {
+        enable = true;
+        extraConfig = mkMerge [
+          {
+            # Tell wireplumber to be more verbose
+            "log-level-debug" = {
+              "context.properties" = {
+                "log.level" = "D"; # output debug logs
+              };
+            };
+
+            # Configure each device/card/output to use the low latency configuration
+            "92-low-latency" = {
               # Some applications still use the alsa channels, so the configuration
               # for Wireplumber doesn't properly apply to them. In that case, they should
               # follow the ALSA configuration instead.
-              main."92-low-latency" = {
-                "monitor.alsa.rules" = [
-                  {
-                    matches = [
-                      {"device.name" = "~alsa_card.*";}
-                      {
-                        # Matches all sources.
-                        node.name = "~alsa_input.*";
-                      }
-                      {
-                        # Matches all sinks.
-                        node.name = "~alsa_output.*";
-                      }
-                    ];
-                    actions = {
-                      update-props = {
-                        "audio.rate" = rate;
-                        "audio.format" = "S32LE";
-                        "api.alsa.period-size" = 2;
-                        "resample.quality" = 4;
-                        "resample.disable" = false;
-                        "session.suspend-timeout-seconds" = 0;
-                        # Default: 0
-                        "api.alsa.headroom" = 128;
-                        # Default: 2
-                        "api.alsa.period-num" = 2;
-                        ## generally, USB soundcards use the batch mode
-                        "api.alsa.disable-batch" = false;
-                      };
-                    };
-                  }
-                ];
-              };
-            }
+              "monitor.alsa.rules" = [
+                {
+                  matches = [
+                    # Matches all devices.
+                    {device.name = "~alsa_card.*";}
+                    # Matches all sources.
+                    {node.name = "~alsa_input.*";}
+                    # Matches all sinks.
+                    {node.name = "~alsa_output.*";}
+                  ];
 
-            (mkIf dev.hasBluetooth {
-              bluetooth."10-bluez" = {
-                "monitor.bluez.rules" = [
-                  {
-                    matches = [{"device.name" = "~bluez_card.*";}];
-                    actions = {
-                      update-props = {
-                        "bluez5.roles" = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]";
-                        "bluez5.a2dp.ldac.quality" = "hq";
-                        "bluez5.enable-msbc" = true;
-                        "bluez5.enable-sbc-xq" = true;
-                        "bluez5.enable-hw-volume" = true;
-                      };
+                  actions.update-props = {
+                    # Give a human-readable name to the matching devices/sources/sinks.
+                    "node.nick" = "ALSA Low Latency";
+
+                    # Low latency configuration
+                    "audio.rate" = rate;
+                    "audio.format" = "S32LE";
+                    "resample.quality" = 4;
+                    "resample.disable" = false;
+                    "session.suspend-timeout-seconds" = 0;
+                    "api.alsa.period-size" = 2;
+                    # Default: 0
+                    "api.alsa.headroom" = 128;
+                    # Default: 2
+                    "api.alsa.period-num" = 2;
+                    ## generally, USB soundcards use the batch mode
+                    "api.alsa.disable-batch" = false;
+                  };
+                }
+              ];
+            };
+          }
+
+          (mkIf dev.hasBluetooth {
+            bluetooth."10-bluez" = {
+              "monitor.bluez.rules" = [
+                {
+                  matches = [{"device.name" = "~bluez_card.*";}];
+                  actions = {
+                    update-props = {
+                      "bluez5.roles" = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]";
+                      # Set quality to high quality instead of the default of auto
+                      "bluez5.a2dp.ldac.quality" = "hq";
+                      "bluez5.enable-msbc" = true;
+                      "bluez5.enable-sbc-xq" = true;
+                      "bluez5.enable-hw-volume" = true;
                     };
-                  }
-                ];
-              };
-            })
-          ];
-        };
+                  };
+                }
+              ];
+            };
+          })
+        ];
       };
     };
 
