282844db125beeca91a9c69d2211d1466b598221
NotAShelf <raf@notashelf.dev>
2024-05-10 17:36:39 +0300

    hosts/gaea: remove lualine; write statusline manually

--- hosts/gaea/system/programs/neovim/default.nix
+++ hosts/gaea/system/programs/neovim/default.nix
@@ -5,18 +5,51 @@ in {
     neovim = {
       enable = true;
 
+      withRuby = false;
+      withPython3 = false;
+      withNodeJs = false;
+
       viAlias = true;
       vimAlias = true;
+
       defaultEditor = true;
 
       configure = {
         customRC = ''
           " -- init --
           ${readFile ./config/init.vim}
+
           " -- mappings --
           ${readFile ./config/maps.vim}
+
           " -- plugin configs --
           ${readFile ./config/plugins.vim}
+
+          " -- lua configuration --
+          lua EOF <<
+          -- set statusline colors
+          vim.cmd([[
+            hi VertSplit guifg=#151515
+            hi User1 guifg=#999999 guibg=#151515
+            hi User2 guifg=#eea040 guibg=#151515
+            hi User3 guifg=#0072ff guibg=#151515
+            hi User4 guifg=#ffffff guibg=#151515
+            hi User5 guifg=#777777 guibg=#151515
+          ]])
+
+          -- set statusline
+          vim.o.statusline = table.concat({
+            "%1* %n %*",       -- buffer number
+            "%3* %y %*",       -- file type
+            -- "%4* %<%F %*",  -- full path
+            "%4* %<%f %*",     -- file name
+            "%2* %m %*",       -- modified flag
+            "%1* %= %5l %*",   -- current line
+            "%2* / %L %*",       -- total lines
+            "%1* %4v %*",      -- virtual column number
+            "%2* 0x%04B %*",   -- character under cursor
+            "%5* %{&ff} %*",   -- file format
+          })
         '';
 
         packages.myVimPackage = with pkgs.vimPlugins; {
@@ -26,7 +59,6 @@ in {
             dressing-nvim # better UI components
 
             leap-nvim # navigation
-            lualine-nvim # statusline
             tabular # align text according to regexp
             undotree # undo history
             vim-css-color # highlight CSS colors
