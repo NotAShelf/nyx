c885dc784549ed77d37747ecb097a5cefef4b8da
NotAShelf <raf@notashelf.dev>
2024-05-10 17:44:54 +0300

    homes/notashelf: flatten dev packages directory

--- homes/notashelf/packages/dev/default.nix
+++ homes/notashelf/packages/dev/default.nix
@@ -1,5 +1,15 @@
-{
-  imports = [
-    ./packages
-  ];
+{pkgs, ...}: {
+  config = {
+    home.packages = with pkgs; [
+      # LaTeX
+      texlive.combined.scheme-full
+      (pkgs.writeShellApplication {
+        name = "pdflatexmk";
+        runtimeInputs = with pkgs; [texlive.combined.scheme-full];
+        text = ''
+          latexmk -pdf "$@" && latexmk -c "$@"
+        '';
+      })
+    ];
+  };
 }
