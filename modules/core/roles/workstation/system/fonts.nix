61599b655ccd38d66f639ef7422a4b9a7b72327e
NotAShelf <raf@notashelf.dev>
2024-05-13 00:50:27 +0300

    system/fonts: enable hinting and antialiasing

--- modules/core/roles/workstation/system/fonts.nix
+++ modules/core/roles/workstation/system/fonts.nix
@@ -5,82 +5,72 @@
 }: let
   inherit (lib.attrsets) mapAttrs;
 in {
-  config = {
-    fonts = {
-      enableDefaultPackages = false;
+  fonts = {
+    enableDefaultPackages = false;
 
-      fontconfig = {
-        enable = true;
-        defaultFonts = let
-          # fonts that should be in each font family
-          # if applicable
-          common = [
-            "Iosevka Nerd Font"
-            "Symbols Nerd Font"
-            "Noto Color Emoji"
+    fontconfig = {
+      enable = true;
+      hinting.enable = true;
+      antialias = true;
+      defaultFonts = let
+        # fonts that should be in each font family
+        # if applicable
+        common = [
+          "Iosevka Nerd Font"
+          "Symbols Nerd Font"
+          "Noto Color Emoji"
+        ];
+      in
+        mapAttrs (_: fonts: fonts ++ common) {
+          serif = ["Noto Serif"];
+          sansSerif = ["Lexend"];
+          emoji = ["Noto Color Emoji"];
+          monospace = [
+            "Source Code Pro Medium"
+            "Source Han Mono"
           ];
-        in
-          mapAttrs (_: fonts: fonts ++ common) {
-            monospace = [
-              "Source Code Pro Medium"
-              "Source Han Mono"
-            ];
-
-            sansSerif = [
-              "Lexend"
-            ];
-
-            serif = [
-              "Noto Serif"
-            ];
-
-            emoji = [
-              "Noto Color Emoji"
-            ];
-          };
-      };
-
-      fontDir = {
-        enable = true;
-        decompressFonts = true;
-      };
-
-      # font packages that should be installed
-      packages = with pkgs; [
-        # programming fonts
-        sarasa-gothic
-
-        # desktop fonts
-        corefonts # MS fonts
-        b612 # high legibility
-        material-icons
-        material-design-icons
-        roboto
-        work-sans
-        comic-neue
-        source-sans
-        inter
-        lato
-        lexend
-        dejavu_fonts
-        noto-fonts
-        noto-fonts-cjk
-
-        # emojis
-        noto-fonts-color-emoji
-        twemoji-color-font
-        openmoji-color
-        openmoji-black
-
-        # defaults worth keeping
-        dejavu_fonts
-        freefont_ttf
-        gyre-fonts
-        liberation_ttf # for PDFs, Roman
-        unifont
-
-        (nerdfonts.override {fonts = ["Iosevka" "JetBrainsMono" "NerdFontsSymbolsOnly"];})
-      ];
+        };
     };
+
+    fontDir = {
+      enable = true;
+      decompressFonts = true;
+    };
+
+    # font packages that should be installed
+    packages = with pkgs; [
+      # defaults worth keeping
+      dejavu_fonts
+      freefont_ttf
+      gyre-fonts
+      liberation_ttf # for PDFs, Roman
+      unifont
+      roboto
+
+      # programming fonts
+      sarasa-gothic
+      (nerdfonts.override {fonts = ["Iosevka" "JetBrainsMono" "NerdFontsSymbolsOnly"];})
+
+      # desktop fonts
+      corefonts # MS fonts
+      b612 # high legibility
+      material-icons # used in widgets and such
+      material-design-icons
+      work-sans
+      comic-neue
+      source-sans
+      inter
+      lato
+      lexend
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-cjk
+
+      # emojis
+      noto-fonts-color-emoji
+      twemoji-color-font
+      openmoji-color
+      openmoji-black
+    ];
   };
 }
