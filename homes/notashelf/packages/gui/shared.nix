ef622f6e593cdfe5161464b3a197f67465d63ab7
NotAShelf <raf@notashelf.dev>
2024-05-12 22:42:42 +0300

    packages/gui: add ps2pdf to Okular's path

--- homes/notashelf/packages/gui/shared.nix
+++ homes/notashelf/packages/gui/shared.nix
@@ -15,12 +15,11 @@ in {
       nextcloud-client
       easyeffects
       qbittorrent
-      hexchat
-      netflix
       helvum
 
       # Electron applications
       # zoom-us # may we never return to online education
+      netflix
       bitwarden
       (symlinkJoin {
         # wrap obsidian with pandoc for the pandoc plugin dependency
@@ -38,30 +37,20 @@ in {
       plasma5Packages.kio-extras
       plasma5Packages.kimageformats
       plasma5Packages.kdegraphics-thumbnailers
-      plasma5Packages.okular
+
+      (symlinkJoin {
+        # wrap obsidian with pandoc for the pandoc plugin dependency
+        name = "Okular";
+        paths = with pkgs; [
+          plasma5Packages.okular
+          ghostscript_headless
+        ];
+      })
 
       # gnome packages
       gnome.gnome-tweaks
       gnome.gnome-calendar
       komikku
-
-      # override gnome-control-center to trick it into thinking we're running gnome
-      # <https://github.com/NixOS/nixpkgs/issues/230493>
-      # <https://gitlab.gnome.org/GNOME/gnome-control-center/-/merge_requests/736>
-      # get overridden idiot
-      /*
-      (gnome.gnome-control-center.overrideAttrs
-        (old: {
-          # gnome-control-center does not start without XDG_CURRENT_DESKTOP=gnome
-          preFixup =
-            ''
-              gappsWrapperArgs+=(
-                --set XDG_CURRENT_DESKTOP "gnome"
-              );
-            ''
-            + old.preFixup;
-        }))
-      */
     ];
   };
 }
