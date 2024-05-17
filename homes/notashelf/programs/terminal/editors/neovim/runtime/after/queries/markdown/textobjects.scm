025b0a2fdc49295ae3b2da77c3ec83a083cb14e6
NotAShelf <raf@notashelf.dev>
2024-05-17 14:38:08 +0300

    editors/neovim: add textobjects, highlights and injections for markdown and rust

---
+++ homes/notashelf/programs/terminal/editors/neovim/runtime/after/queries/markdown/textobjects.scm
@@ -0,0 +1,5 @@
+;extends
+
+(fenced_code_block (code_fence_content) @class.inner) @class.outer
+
+(paragraph) @function.outer @function.inner
---
+++ homes/notashelf/programs/terminal/editors/neovim/runtime/after/queries/markdown_inline/highlights.scm
@@ -0,0 +1,12 @@
+;extends
+
+[
+  (shortcut_link)
+] @nospell
+
+(strikethrough
+(emphasis_delimiter)
+(strikethrough
+  (emphasis_delimiter)
+  (emphasis_delimiter))
+(emphasis_delimiter))@markup.doublestrikethrough
