ef574b162c7f6747d2f45e99538f0e9ec529c22c
NotAShelf <raf@notashelf.dev>
2024-05-18 17:58:54 +0300

    multimedia/rtkit: force binding to the status of PipeWire service

--- modules/core/common/system/hardware/multimedia/sound/rtkit.nix
+++ modules/core/common/system/hardware/multimedia/sound/rtkit.nix
@@ -1,6 +1,12 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkForce;
+in {
   # able to change scheduling policies, e.g. to SCHED_RR
   # sounds server use RealtimeKit (rtkit) to acquire
   # realtime priority
-  security.rtkit.enable = config.services.pipewire.enable;
+  security.rtkit.enable = mkForce config.services.pipewire.enable;
 }
