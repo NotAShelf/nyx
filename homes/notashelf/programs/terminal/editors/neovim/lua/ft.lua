54a0d064f75d8f15443883739f4ef31604f87459
NotAShelf <raf@notashelf.dev>
2024-05-19 06:14:24 +0300

    editors/neovim: set syntax for `astro` and `mdx` files

--- homes/notashelf/programs/terminal/editors/neovim/lua/ft.lua
+++ homes/notashelf/programs/terminal/editors/neovim/lua/ft.lua
@@ -2,10 +2,12 @@
 vim.g.markdown_fenced_languages = { 'shell=bash' }
 
 local file_syntax_map = {
-  { pattern = '*.rasi', syntax = 'scss' },
+  { pattern = '*.rasi',     syntax = 'scss' },
   { pattern = 'flake.lock', syntax = 'json' },
-  { pattern = '*.ignore', syntax = 'gitignore' }, -- also ignore for fd/ripgrep
-  { pattern = '*.ojs', syntax = 'javascript' },
+  { pattern = '*.ignore',   syntax = 'gitignore' }, -- also ignore for fd/ripgrep
+  { pattern = '*.ojs',      syntax = 'javascript' },
+  { pattern = '*.astro',    syntax = 'astro' },
+  { pattern = '*.mdx',      syntax = 'mdx' }
 }
 
 for _, elem in ipairs(file_syntax_map) do
