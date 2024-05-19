fc1a2ef6610849f2d97d07d6b051451ae2503bc4
NotAShelf <raf@notashelf.dev>
2024-05-19 05:49:00 +0300

    services/ags: minor tweaks; first attempt at colored tray bg

--- homes/notashelf/services/wayland/ags/config.js
+++ homes/notashelf/services/wayland/ags/config.js
@@ -1,18 +1,6 @@
 import { App, Notifications } from "./js/imports.js";
 const css = App.configDir + "/style.css";
 
-/**
- * Custom logger function. Behaves like console.log, allowing multiple arguments.
- * Added in order to avoid using `console.log` or `print`, which are both
- * ignored by my LSP for some reason.
- *
- * @param {...any} args The arguments to be logged.
- */
-function log(...args) {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}]`, ...args);
-}
-
 // Windows
 import { AppLauncher } from "./js/windows/launcher/index.js";
 import { Bar } from "./js/windows/bar/index.js";
@@ -21,7 +9,7 @@ import { Popups } from "./js/windows/popups/index.js";
 import { Notifs } from "./js/windows/notifications/index.js";
 import { Media } from "./js/windows/music/index.js";
 
-App.connect("config-parsed", () => print("Config parsed!"));
+App.connect("config-parsed", () => console.log("Config parsed!"));
 
 Notifications.popupTimeout = 5000;
 Notifications.forceTimeout = false;
@@ -46,7 +34,7 @@ function addWindows(windows) {
 try {
     addWindows([AppLauncher(), Bar(), Media(), Desktop(), Popups(), Notifs()]);
 } catch (e) {
-    log(e);
+    console.log(e);
     App.quit();
 }
 
