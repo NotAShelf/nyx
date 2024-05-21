f43e2e5474ca37d895c305b3b3db7426e1344229
NotAShelf <raf@notashelf.dev>
2024-05-22 00:23:28 +0300

    apps:/build-all-hosts: avoid redirecting stdout to devnull

--- flake/apps/build-all-hosts/default.nix
+++ flake/apps/build-all-hosts/default.nix
@@ -3,11 +3,9 @@
   program =
     pkgs.writers.writePython3Bin "build-all-hosts" {
       flakeIgnore = ["E501"];
-      libraries = with pkgs.python311Packages; [tqdm];
     } ''
       import subprocess
       import json
-      from tqdm import tqdm
 
       command = [
           "nix",
@@ -26,19 +24,7 @@
       data = json.loads(output.stdout)
       configurations = data.get("nixosConfigurations", {}).keys()
 
-      # Calculate the total number of configurations
-      total_configurations = len(configurations)
-
-      # Initialize the progress bar
-      progress_bar = tqdm(total=total_configurations, desc="Building Hosts", unit="Host", ascii=True)
-
       for configuration in configurations:
-          # Silence the output of nixos-rebuild build
-          subprocess.run(["nixos-rebuild", "build", "--flake", f".#{configuration}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-          # Update the progress bar
-          progress_bar.update(1)
-
-      # Close the progress bar
-      progress_bar.close()
+          subprocess.run(["nixos-rebuild", "build", "--flake", f".#{configuration}"])
     '';
 }
