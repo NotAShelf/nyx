6975ebd41ef0a8adf285763504c32fa6ef18b46b
NotAShelf <raf@notashelf.dev>
2024-05-24 17:34:24 +0300

    services/ags: fix transparent right click menus for tray items

--- homes/notashelf/services/wayland/ags/js/utils/tray.js
+++ homes/notashelf/services/wayland/ags/js/utils/tray.js
@@ -2,8 +2,10 @@ import { Widget, SystemTray } from "../imports.js";
 const { Button, Icon } = Widget;
 
 export const getTrayItems = (self) => {
-    self.children = SystemTray.items.map((item) =>
-        Button({
+    self.children = SystemTray.items.map((item) => {
+        if (item.menu) item.menu.class_name = "trayMenu";
+
+        return Button({
             className: "trayIcon",
             child: Icon({
                 setup: (self) => self.bind("icon", item, "icon"),
@@ -12,6 +14,6 @@ export const getTrayItems = (self) => {
                 self.bind("tooltip-markup", item, "tooltip-markup"),
             onPrimaryClick: (_, event) => item.activate(event),
             onSecondaryClick: (_, event) => item.openMenu(event),
-        }),
-    );
+        });
+    });
 };
