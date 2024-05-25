478606253ae68c74cdf80d5031be4e7f68bba0fd
NotAShelf <raf@notashelf.dev>
2024-05-25 09:03:35 +0300

    modules/usrEnv: allow desktop options to specify their own packages
    This should make it easier to share desktop packages inbetween desktops. This is a problem since I get Hyprland package from the flake inputs, and don't want to type the package name every single time. Currently, the Hyprland package is duplicated since I depend on it for my gamemoderun script.

--- modules/options/usrEnv/desktop.nix
+++ modules/options/usrEnv/desktop.nix
@@ -1,17 +1,19 @@
 {
+  inputs',
   config,
   pkgs,
   lib,
   ...
 }: let
-  inherit (lib) mkOption types;
+  inherit (lib.options) mkOption;
+  inherit (lib.types) bool enum package;
 
   cfg = config.modules.usrEnv;
   sys = config.modules.system;
 in {
   options.modules.usrEnv = {
     desktop = mkOption {
-      type = types.enum ["none" "Hyprland" "sway" "awesomewm" "i3"];
+      type = enum ["none" "Hyprland" "sway" "awesomewm" "i3"];
       default = "none";
       description = ''
         The desktop environment to be used.
@@ -19,57 +21,95 @@ in {
     };
 
     desktops = {
-      hyprland.enable = mkOption {
-        type = types.bool;
-        default = cfg.desktop == "Hyprland";
-        description = ''
-          Whether to enable Hyprland wayland compositor.
+      hyprland = {
+        enable = mkOption {
+          type = bool;
+          default = cfg.desktop == "Hyprland";
+          description = ''
+            Whether to enable Hyprland wayland compositor.
 
-          Will be enabled automatically when `modules.usrEnv.desktop` is set to "Hyprland".
+            Will be enabled automatically when `modules.usrEnv.desktop`
+            is set to "Hyprland".
 
-        '';
+          '';
+        };
+
+        package = mkOption {
+          type = package;
+          default = inputs'.hyprland.packages.hyprland;
+          description = ''
+            The Hyprland package to be used.
+          '';
+        };
       };
 
-      sway.enable = mkOption {
-        type = types.bool;
-        default = cfg.desktop == "sway";
-        description = ''
-          Whether to enable Sway wayland compositor.
+      sway = {
+        enable = mkOption {
+          type = bool;
+          default = cfg.desktop == "sway";
+          description = ''
+            Whether to enable Sway wayland compositor.
 
-          Will be enabled automatically when `modules.usrEnv.desktop` is set to "sway".
-        '';
+            Will be enabled automatically when `modules.usrEnv.desktop`
+            is set to "sway".
+          '';
+        };
+
+        package = mkOption {
+          type = package;
+          default = pkgs.sway;
+          description = ''
+            The Sway package to be used.
+          '';
+        };
       };
 
       awesomwm.enable = mkOption {
-        type = types.bool;
+        type = bool;
         default = cfg.desktop == "awesomewm";
         description = ''
           Whether to enable Awesome window manager
 
-          Will be enabled automatically when `modules.usrEnv.desktop` is set to "awesomewm".
+          Will be enabled automatically when `modules.usrEnv.desktop`
+          is set to "awesomewm".
         '';
       };
 
-      i3.enable = mkOption {
-        type = types.bool;
-        default = cfg.desktop == "i3";
-        description = ''
-          Whether to enable i3 window manager
+      i3 = {
+        enable = mkOption {
+          type = bool;
+          default = cfg.desktop == "i3";
+          description = ''
+            Whether to enable i3 window manager
 
-          Will be enabled automatically when `modules.usrEnv.desktop` is set to "i3".
-        '';
+            Will be enabled automatically when `modules.usrEnv.desktop`
+            is set to "i3".
+          '';
+        };
+
+        package = mkOption {
+          type = package;
+          default = pkgs.i3;
+          description = ''
+            The i3 package to be used.
+          '';
+        };
       };
     };
 
     useHomeManager = mkOption {
-      type = types.bool;
+      type = bool;
       default = true;
       description = ''
-        Whether to enable the usage of home-manager for user home management. Maps the list
-        of users to their home directories inside the `homes/` directory in the repository
-        root.
+        Whether to enable the usage of home-manager for user home
+        management. My home-manager module will map the list of users to
+        their home directories inside the `homes/` directory in the
+        repository root.
 
-        Username via `modules.system.mainUser` must be set if this option is enabled.
+        ::: {.warning}
+        Username via `modules.system.mainUser` must be set if
+        this option is enabled.
+        :::
       '';
     };
   };
