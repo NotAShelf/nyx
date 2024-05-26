cc6ca62e5833bb4e2bd8643c41689273ab0c2745
NotAShelf <raf@notashelf.dev>
2024-05-27 00:50:30 +0300

    device/hardware: assert when `device.type` is left empty

--- modules/options/device/hardware.nix
+++ modules/options/device/hardware.nix
@@ -1,9 +1,14 @@
-{lib, ...}: let
-  inherit (lib) mkOption mkEnableOption types;
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.options) mkOption mkEnableOption;
+  inherit (lib.types) nullOr listOf enum str;
 in {
   options.modules.device = {
     type = mkOption {
-      type = types.enum ["laptop" "desktop" "server" "hybrid" "lite" "vm"];
+      type = enum ["laptop" "desktop" "server" "hybrid" "lite" "vm"];
       default = "";
       description = ''
         The type/purpose of the device that will be used within the rest of the configuration.
@@ -21,7 +26,7 @@ in {
     # TODO: make this a list - apparently more than one cpu on a device is still doable
     cpu = {
       type = mkOption {
-        type = with types; nullOr (enum ["pi" "intel" "vm-intel" "amd" "vm-amd"]);
+        type = nullOr (enum ["pi" "intel" "vm-intel" "amd" "vm-amd"]);
         default = null;
         description = ''
           The manifaturer/type of the primary system CPU.
@@ -35,7 +40,7 @@ in {
         zenpower = {
           enable = mkEnableOption "AMD Zenpower Driver";
           args = mkOption {
-            type = types.str;
+            type = str;
             default = "-p 0 -v 3C -f A0"; # Pstate 0, 1.175 voltage, 4000 clock speed
             description = ''
               The percentage of the maximum clock speed that the CPU will be limited to.
@@ -50,7 +55,7 @@ in {
 
     gpu = {
       type = mkOption {
-        type = with types; nullOr (enum ["pi" "amd" "intel" "nvidia" "hybrid-nv" "hybrid-amd"]);
+        type = nullOr (enum ["pi" "amd" "intel" "nvidia" "hybrid-nv" "hybrid-amd"]);
         default = null;
         description = ''
           The manifaturer/type of the primary system GPU. Allows the correct GPU
@@ -60,7 +65,7 @@ in {
     };
 
     monitors = mkOption {
-      type = with types; listOf str;
+      type = listOf str;
       default = [];
       description = ''
         A list of monitors connected to the system.
@@ -79,4 +84,13 @@ in {
       '';
     };
   };
+
+  config.assertions = [
+    {
+      assertion = config.modules.device.type != "";
+      message = ''
+        ${config.meta.hostname} is missing a device type. Please define it!
+      '';
+    }
+  ];
 }
