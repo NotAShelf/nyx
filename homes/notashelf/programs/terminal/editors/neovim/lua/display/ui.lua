81489c58838c6a51b3911768ce0a7127ac28a004
NotAShelf <raf@notashelf.dev>
2024-06-03 18:35:33 +0300

    editors/neovim: replace tab character

--- homes/notashelf/programs/terminal/editors/neovim/lua/display/ui.lua
+++ homes/notashelf/programs/terminal/editors/neovim/lua/display/ui.lua
@@ -4,16 +4,16 @@ local opt = vim.opt
 -- When true, all the windows are automatically made the same size after splitting or closing a window.
 -- When false, splitting a window will reduce the size of the current window and leave the other windows the same.
 opt.equalalways = false
-opt.cmdheight = 1 -- Better display for messages
+opt.cmdheight = 1      -- Better display for messages
 opt.colorcolumn = '+0' -- Align text at 'textwidth'
-opt.showtabline = 2 -- Always show the tabs line
-opt.helpheight = 0 -- Disable help window resizing
-opt.winwidth = 30 -- Minimum width for active window
-opt.winminwidth = 1 -- Minimum width for inactive windows
-opt.winheight = 1 -- Minimum height for active window
-opt.winminheight = 1 -- Minimum height for inactive window
-opt.pumheight = 10 -- Maximum number of items to show in the popup menu
-opt.winminwidth = 1 -- min width of inactive window
+opt.showtabline = 2    -- Always show the tabs line
+opt.helpheight = 0     -- Disable help window resizing
+opt.winwidth = 30      -- Minimum width for active window
+opt.winminwidth = 1    -- Minimum width for inactive windows
+opt.winheight = 1      -- Minimum height for active window
+opt.winminheight = 1   -- Minimum height for inactive window
+opt.pumheight = 10     -- Maximum number of items to show in the popup menu
+opt.winminwidth = 1    -- min width of inactive window
 -- opt.pumblend = 100  -- Popup blend, 100 means transparent
 
 opt.cursorline = true
@@ -58,10 +58,11 @@ opt.listchars = normal_listchars
 -- Show listchars while in Insert mode.
 local insert_listchars = {
   eol = nil,
+  tab = '▎·',
   lead = '·',
   space = '·',
+  trail = '.',
   multispace = '… ',
-  tab = '→ ',
   nbsp = '¤',
 }
 
