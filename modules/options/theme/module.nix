60bb7db1437631c29638aed9f466e2db68de3be4
NotAShelf <raf@notashelf.dev>
2024-06-06 18:12:13 +0300

    options/theme: use boolean mkOption instead of mkEnableOption

--- modules/options/theme/module.nix
+++ modules/options/theme/module.nix
@@ -3,7 +3,8 @@
   lib,
   ...
 }: let
-  inherit (lib) mkOption mkEnableOption types;
+  inherit (lib.options) mkOption;
+  inherit (lib.types) either listOf package str int bool;
 in {
   imports = [
     ./gtk.nix
@@ -12,33 +13,42 @@ in {
   ];
 
   options.modules.style = {
-    forceGtk = mkEnableOption "Force GTK applications to use the GTK theme";
-    useKvantum = mkEnableOption "Use Kvantum to theme QT applications";
+    forceGtk = mkOption {
+      type = bool;
+      default = false;
+      description = "Whether to force QT applications to try and use the GTK theme.";
+    };
+
+    useKvantum = mkOption {
+      type = bool;
+      default = false;
+      description = "Whether to use Kvantum for theming QT applications";
+    };
 
     pointerCursor = {
       package = mkOption {
-        type = types.package;
-        description = "The package providing the cursors";
+        type = package;
         default = pkgs.catppuccin-cursors.mochaDark;
+        description = "The package providing the cursors";
       };
 
       name = mkOption {
-        type = types.str;
+        type = str;
+        default = "catppuccin-mocha-dark-cursors";
         description = "The name of the cursor inside the package";
-        default = "Catppuccin-Mocha-Dark-Cursors";
       };
 
       size = mkOption {
-        type = types.int;
-        description = "The size of the cursor";
+        type = int;
         default = 24;
+        description = "The size of the cursor";
       };
     };
 
     wallpapers = mkOption {
-      type = with types; either str (listOf str);
-      description = "Wallpaper or wallpapers to use";
+      type = either str (listOf str);
       default = [];
+      description = "Wallpaper or wallpapers to use";
     };
   };
 }
