8d0c5740c3726f8e446078630a00600a45e62f6b
NotAShelf <raf@notashelf.dev>
2024-06-06 18:11:44 +0300

    homes/themes: remove redundant comments

--- homes/notashelf/themes/global.nix
+++ homes/notashelf/themes/global.nix
@@ -4,8 +4,8 @@ in {
   # cursor theme
   home = {
     pointerCursor = {
-      package = cfg.pointerCursor.package; # pkgs.bibata-cursors;
-      name = cfg.pointerCursor.name; # "Bibata-Modern-Classic";
+      package = cfg.pointerCursor.package;
+      name = cfg.pointerCursor.name;
       size = cfg.pointerCursor.size;
       gtk.enable = true;
       x11.enable = true;
