42136f19f5e6f026ff05322d5f8dae09220c20a5
NotAShelf <raf@notashelf.dev>
2024-06-13 00:11:26 +0300

    services/gnome: disable unneeded gnome dependencies

--- modules/core/roles/workstation/system/services/gnome.nix
+++ modules/core/roles/workstation/system/services/gnome.nix
@@ -1,25 +1,12 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   config = {
     services = {
-      udev.packages = with pkgs; [
-        gnome.gnome-settings-daemon
-      ];
-
+      udev.packages = [pkgs.gnome.gnome-settings-daemon];
       gnome = {
-        glib-networking.enable = true;
-        evolution-data-server.enable = true;
-
-        # optional to use google/nextcloud calendar
-        gnome-online-accounts.enable = true;
-
-        # optional to use google/nextcloud calendar
+        # Whether to enable gnome-keyring. This is usually necessary for storing
+        # secrets for programming applications such as VSCode or GitHub desktop.
+        # It is also optional to use google/nextcloud calendar.
         gnome-keyring.enable = true;
-
-        gnome-remote-desktop.enable = lib.mkForce false;
       };
     };
   };
