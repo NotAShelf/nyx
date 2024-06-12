6ae5bcaa09014b41f09170157de7b19d5967514a
NotAShelf <raf@notashelf.dev>
2024-06-13 00:11:03 +0300

    services/systemd: force TTYs to be disabled

--- modules/core/roles/workstation/system/services/systemd.nix
+++ modules/core/roles/workstation/system/services/systemd.nix
@@ -1,4 +1,7 @@
-{
+{lib, ...}: let
+  inherit (lib.modules) mkForce;
+  inherit (lib.attrsets) mapAttrs;
+in {
   systemd = let
     extraConfig = ''
       DefaultTimeoutStartSec=15s
@@ -9,7 +12,11 @@
   in {
     inherit extraConfig;
     user = {inherit extraConfig;};
-    services = {
+
+    # Disable all virtual terminals. I usually don't need to switch between
+    # TTYs, however, I may get locked out of my desktop session and this
+    # may come to bite me in the ass when that happens.
+    services = mapAttrs (_: mkForce) {
       "getty@tty1".enable = false;
       "autovt@tty1".enable = false;
       "getty@tty7".enable = false;
