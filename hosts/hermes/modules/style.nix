11831a84b442046e06a313a3336662c1e96d4e1b
NotAShelf <raf@notashelf.dev>
2024-06-26 00:00:06 +0300

    hosts: remove hardcoded styling options; use shared opts

--- hosts/hermes/modules/style.nix
+++ hosts/hermes/modules/style.nix
@@ -1,48 +1,7 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
   config.modules.style = {
     forceGtk = true;
-
-    gtk = {
-      usePortal = true;
-      theme = {
-        name = "Catppuccin-Mocha-Standard-Blue-Dark";
-        package = pkgs.catppuccin-gtk.override {
-          size = "standard";
-          accents = ["blue"];
-          variant = "mocha";
-          tweaks = ["normal"];
-        };
-      };
-
-      iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.catppuccin-papirus-folders.override {
-          accent = "blue";
-          flavor = "mocha";
-        };
-      };
-
-      font = {
-        name = "Lexend";
-        size = 14;
-      };
-    };
-
-    qt = {
-      theme = {
-        name = "Catppuccin-Mocha-Dark";
-        package = pkgs.catppuccin-kde.override {
-          flavour = ["mocha"];
-          accents = ["blue"];
-          winDecStyles = ["modern"];
-        };
-      };
-
-      kdeglobals.source = "${config.modules.style.qt.theme.package}" + "/share/color-schemes/CatppuccinMochaBlue.colors";
-    };
+    useKvantum = false;
+    gtk.usePortal = true;
   };
 }
