cc561e5d16dc52513a00035e277cfcca8d51cda4
NotAShelf <raf@notashelf.dev>
2024-06-26 00:01:07 +0300

    system/misc: inline fs opts for firefox cache; use explicit lib

--- modules/core/roles/workstation/system/misc.nix
+++ modules/core/roles/workstation/system/misc.nix
@@ -1,17 +1,12 @@
 {lib, ...}: let
-  inherit (lib) mkForce;
+  inherit (lib.modules) mkForce;
 in {
   # Firefox cache on tmpfs
   fileSystems."/home/notashelf/.cache/mozilla/firefox" = {
     device = "tmpfs";
     fsType = "tmpfs";
     noCheck = true;
-    options = [
-      "noatime"
-      "nodev"
-      "nosuid"
-      "size=128M"
-    ];
+    options = ["noatime" "nodev" "nosuid" "size=128M"];
   };
 
   # enable the unified cgroup hierarchy (cgroupsv2)
