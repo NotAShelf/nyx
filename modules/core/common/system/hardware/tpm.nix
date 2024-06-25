6fc7e7949a63740e262fa0d0c36b070a8b1b43d9
NotAShelf <raf@notashelf.dev>
2024-06-26 00:05:41 +0300

    hardware/tpm: minor cleanup; default module options to true

--- modules/core/common/system/hardware/tpm.nix
+++ modules/core/common/system/hardware/tpm.nix
@@ -1,28 +1,38 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }: let
-  inherit (lib) mkIf mkDefault;
+  inherit (lib.modules) mkIf;
 
   dev = config.modules.device;
 in {
   config = mkIf dev.hasTPM {
+    boot.kernelModules = ["uhid"];
+
     security.tpm2 = {
-      # enable Trusted Platform Module 2 support
+      # Enable Trusted Platform Module 2 support
       enable = true;
 
-      # enable Trusted Platform 2 userspace resource manager daemon
-      abrmd.enable = mkDefault false;
+      applyUdevRules = true;
+
+      # Enable Trusted Platform 2 userspace resource manager daemon
+      # Setting this option to true will have TMP2 as a userspace daemon
+      # and set the `security.tmp2.tssUser` that the daemon will run as.
+      abrmd.enable = true;
 
       # The TCTI is the "Transmission Interface" that is used to communicate with a
       # TPM. this option sets TCTI environment variables to the specified values if enabled
       #  - TPM2TOOLS_TCTI
       #  - TPM2_PKCS11_TCTI
-      tctiEnvironment.enable = mkDefault true;
+      tctiEnvironment.enable = true;
 
       # enable TPM2 PKCS#11 tool and shared library in system path
-      pkcs11.enable = mkDefault false;
+      pkcs11.enable = true;
     };
+
+    # Utilities to work with TPM2 on Linux.
+    environment.systemPackages = with pkgs; [tpm2-tools tpm2-tss tpm2-abrmd];
   };
 }
