ced07e9d6abb1a7a550bb8f52ccdaee4c7fa28c7
NotAShelf <raf@notashelf.dev>
2024-06-26 14:12:24 +0300

    shell/zsh: refactor plugin format

--- homes/notashelf/programs/terminal/shell/zsh/plugins.nix
+++ homes/notashelf/programs/terminal/shell/zsh/plugins.nix
@@ -1,37 +1,36 @@
-{pkgs, ...}: let
-  inherit (pkgs) fetchFromGitHub;
-in {
+{pkgs, ...}: {
   programs.zsh.plugins = [
     {
-      # Must be before plugins that wrap widgets, such as zsh-autosuggestions or fast-syntax-highlighting
+      # Must be before plugins that wrap widgets
+      # such as zsh-autosuggestions or fast-syntax-highlighting
       name = "fzf-tab";
+      file = "fzf-tab.plugin.zsh";
       src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
     }
     {
-      name = "zsh-nix-shell";
-      src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh";
+      name = "nix-shell";
+      file = "nix-shell.plugin.zsh";
+      src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
     }
     {
       name = "zsh-vi-mode";
-      src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      file = "zsh-vi-mode.plugin.zsh";
+      src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
     }
     {
       name = "fast-syntax-highlighting";
+      file = "fast-syntax-highlighting.plugin.zsh";
       src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
     }
     {
       name = "zsh-autosuggestions";
-      src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+      file = "zsh-autosuggestions.zsh";
+      src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
     }
     {
       name = "zsh-autopair";
       file = "zsh-autopair.plugin.zsh";
-      src = fetchFromGitHub {
-        owner = "hlissner";
-        repo = "zsh-autopair";
-        rev = "2ec3fd3c9b950c01dbffbb2a4d191e1d34b8c58a";
-        hash = "sha256-Y7fkpvCOC/lC2CHYui+6vOdNO8dNHGrVYTGGNf9qgdg=";
-      };
+      src = pkgs.zsh-autopair;
     }
   ];
 }
