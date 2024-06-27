d38669367f6d1113b3d2af85c625bb79bd80ec0c
NotAShelf <raf@notashelf.dev>
2024-06-27 12:31:21 +0300

    flake/args: consume local overlay

--- flake/args.nix
+++ flake/args.nix
@@ -1,14 +1,15 @@
 {inputs, ...}: let
-  # add `pins` to self so that the flake may refer it freely
-  # pins can be updated via `npins update` in current directory
+  # Add `pins` to self so that the flake may refer it freely by accessing
+  # `pings` in an argset (e.g. `{pkgs, lib, pins, ...}`).
+  # Pinned sources can be updated via `npins update` in `flake/`
   # which will automatically bump all sources in the `npins`
-  # directory.
+  # directory relatvie to this file.
   pinnedSources = import ./npins;
 
-  # add a collection of SSH keys to the keys so that
-  #  1. my public keys are more easily obtainable from outside
-  #  2. it's easy to share key names and values internally especially
-  #  for setting them for users and such
+  # Add a collection of SSH keys to the keys so that
+  #  1. My public keys are more easily obtainable from outside
+  #  2. It's easy to share key names and values internally especially
+  #  for setting them for users, services, etc.
   publicKeys = import ./keys.nix;
 in {
   perSystem = {
@@ -19,20 +20,27 @@ in {
     imports = [
       {
         _module.args = {
-          pkgs = config.legacyPackages;
           pins = pinnedSources;
           keys = publicKeys;
+
+          # Unify all instances of nixpkgs into a single `pkgs` set
+          # Wthat includes our own overlays within `perSystem`. This
+          # is not done by flake-parts, so we do it ourselves.
+          # See:
+          #  <https://github.com/hercules-ci/flake-parts/issues/106#issuecomment-1399041045>
+          pkgs = config.legacyPackages;
         };
       }
     ];
 
     legacyPackages = import inputs.nixpkgs {
       inherit system;
-      overlays = [];
       config = {
         allowUnfree = true;
         allowUnsupportedSystem = true;
       };
+
+      overlays = [inputs.self.overlays.default];
     };
   };
 
