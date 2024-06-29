1440947f1c65b061a6e1d084b392a41f498cda7f
NotAShelf <raf@notashelf.dev>
2024-06-29 22:38:11 +0300

    flake: get rid of `nixConfig`; add system caches to ISO role

--- flake.nix
+++ flake.nix
@@ -278,21 +278,4 @@
       inputs.hyprland.follows = "hyprland";
     };
   };
-
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://cache.privatevoid.net"
-      "https://nyx.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
-      "notashelf.cachix.org-1:VTTBFNQWbfyLuRzgm2I7AWSDJdqAa11ytLXHBhrprZk="
-      "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
-    ];
-  };
 }
--- modules/core/roles/iso/system/nix.nix
+++ modules/core/roles/iso/system/nix.nix
@@ -1,23 +1,51 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   nix = {
+    package = pkgs.nixVersions.latest;
     channel.enable = false;
     nixPath = ["nixpkgs=${config.nix.registry.nixpkgs.to.path}"];
 
-    # settings
     settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-        "repl-flake"
-      ];
+      # Enable flakes. This is mandatory to install this configuration.
+      experimental-features = ["nix-command" "flakes"];
 
-      log-lines = 30;
+      log-lines = 50;
       warn-dirty = false;
       http-connections = 50;
-      accept-flake-config = true;
-      auto-optimise-store = true;
+      accept-flake-config = false;
+      auto-optimise-store = false;
 
+      # Disable built-in registry
       flake-registry = "";
+
+      # Make building installed systems faster
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cache.privatevoid.net"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://anyrun.cachix.org"
+        "https://nyx.cachix.org"
+        "https://neovim-flake.cachix.org"
+        "https://cache.notashelf.dev"
+        "https://ags.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        "notashelf.cachix.org-1:VTTBFNQWbfyLuRzgm2I7AWSDJdqAa11ytLXHBhrprZk="
+        "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
+        "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
+      ];
     };
   };
 }
