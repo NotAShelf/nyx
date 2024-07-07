49f2bf00dc9cf8894524e219b5e0ed206455ec14
NotAShelf <raf@notashelf.dev>
2024-07-07 04:18:57 +0300

    system/nix: move NOM patch to global overlay

--- modules/core/common/system/nix/overlays/default.nix
+++ modules/core/common/system/nix/overlays/default.nix
@@ -5,6 +5,8 @@
   ...
 }: let
   inherit (lib.trivial) const;
+  inherit (lib.lists) length zipListsWith;
+  inherit (lib.strings) concatStringsSep escapeShellArg;
 in {
   # Overlays are by far the most obscure and annoying feature of Nix, and if you have
   # interacted with me on a personal level before, you will find that I actively discourage
@@ -20,7 +22,17 @@ in {
 
     (const (prev: {
       lix = prev.lix.overrideAttrs (old: {
-        patches = [./patches/0001-nix-default-flake.patch];
+        patches = [
+          ./patches/0001-nix-default-flake.patch
+          ./patches/0001-nix-reject-flake-config.patch
+        ];
+
+        postPatch =
+          (old.postPatch or "")
+          + ''
+            substituteInPlace src/libmain/shared.cc \
+              --replace-fail "(Lix, like Nix)" "(Lix, Nix for lesbians)"
+          '';
 
         postInstall =
           (old.postInstall or "")
@@ -41,6 +53,9 @@ in {
       zsh = prev.zsh.overrideAttrs (old: {
         patches = [
           ./patches/0001-zsh-globquote.patch
+
+          # From:
+          #  <https://github.com/fugidev/nix-config>
           ./patches/0001-zsh-completion-remote-files.patch
         ];
 
@@ -51,6 +66,53 @@ in {
             sed -i -e '/^name=zsh\/newuser/d' config.modules
           '';
       });
+
+      # Create an overlay for nix-output-monitor to match the inconsistent
+      # and frankly ugly icons with Nerdfonts ones. they look a little larger
+      # than before, but overall consistency is better in general.
+      nix-output-monitor = let
+        oldIcons = [
+          "↑"
+          "↓"
+          "⏱"
+          "⏵"
+          "✔"
+          "⏸"
+          "⚠"
+          "∅"
+          "∑"
+        ];
+        newIcons = [
+          "f062" # 
+          "f063" # 
+          "f520" # 
+          "f04b" # 
+          "f00c" # 
+          "f04c" # 
+          "f071" # 
+          "f1da" # 
+          "f04a0" # 󰒠
+        ];
+      in
+        assert length oldIcons == length newIcons;
+          prev.nix-output-monitor.overrideAttrs (old: {
+            version = "0-unstable-2024-06-22";
+            patches =
+              (old.patches or [])
+              ++ [
+                ./patches/0002-nom-print-traces.patch
+              ];
+
+            postPatch =
+              (old.postPatch or "")
+              + ''
+                sed -i ${escapeShellArg (
+                  concatStringsSep "\n" (zipListsWith (a: b: "s/${a}/\\\\x${b}/") oldIcons newIcons)
+                )} lib/NOM/Print.hs
+
+                substituteInPlace lib/NOM/Print/Tree.hs --replace-fail '┌' '╭'
+              '';
+          });
     }))
   ];
 }
--- modules/core/roles/workstation/system/programs/nh.nix
+++ modules/core/roles/workstation/system/programs/nh.nix
@@ -1,11 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}: let
-  inherit (lib.lists) length zipListsWith;
-  inherit (lib.strings) concatStringsSep escapeShellArg;
-in {
+{pkgs, ...}: {
   # This is set by `programs.nh.flake` by itself. We're just setting it here
   # so that we have a FLAKE variable set even when nh is disabled.
   environment.variables.FLAKE = "/home/notashelf/.config/nyx";
@@ -23,61 +16,4 @@ in {
       dates = "weekly";
     };
   };
-
-  # Normally we wouldn't use overlays, because we wouldn't need the tree-wide
-  # butterfly effect of changing a derivation permanently. However, we *want*
-  # nom to be chaanged permanently across the tree so that the UI is consistent
-  # with the rest of the system.
-  nixpkgs.overlays = [
-    (_: prev: let
-      oldIcons = [
-        "↑"
-        "↓"
-        "⏱"
-        "⏵"
-        "✔"
-        "⏸"
-        "⚠"
-        "∅"
-        "∑"
-      ];
-      newIcons = [
-        "f062" # 
-        "f063" # 
-        "f520" # 
-        "f04b" # 
-        "f00c" # 
-        "f04c" # 
-        "f071" # 
-        "f1da" # 
-        "f04a0" # 󰒠
-      ];
-    in {
-      # Create an overlay for nix-output-monitor to match the inconsistent
-      # and frankly ugly icons with Nerdfonts ones. they look a little larger
-      # than before, but overall consistency is better in general.
-      nix-output-monitor = assert length oldIcons == length newIcons;
-        prev.nix-output-monitor.overrideAttrs (old: {
-          version = "0-unstable-2024-06-22";
-          patches =
-            (old.patches or [])
-            ++ [
-              (pkgs.fetchpatch {
-                url = "https://github.com/maralorn/nix-output-monitor/commit/738f445082d6d5c8f96701ccd1fe1136a7a47715.patch";
-                hash = "sha256-6uq55PmenrrqVx+TWCP2AFlxlZsYu4NXRTTKYIwRdY4=";
-              })
-            ];
-
-          postPatch =
-            (old.postPatch or "")
-            + ''
-              sed -i ${escapeShellArg (
-                concatStringsSep "\n" (zipListsWith (a: b: "s/${a}/\\\\x${b}/") oldIcons newIcons)
-              )} lib/NOM/Print.hs
-
-              substituteInPlace lib/NOM/Print/Tree.hs --replace-fail '┌' '╭'
-            '';
-        });
-    })
-  ];
 }
