bd2ac04a494b5c6677a2ac2b172c437a1e3777ff
NotAShelf <raf@notashelf.dev>
2024-07-08 22:02:18 +0300

    hosts/atlas: provide partitions for noexec rules

--- hosts/atlas/fs/default.nix
+++ hosts/atlas/fs/default.nix
@@ -1,8 +1,22 @@
 {
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_SD";
-    fsType = "ext4";
-    options = ["noatime"];
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+
+    "/var/log" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
   };
 
   swapDevices = [];
