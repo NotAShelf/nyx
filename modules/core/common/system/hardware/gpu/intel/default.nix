8c0d95b29a09df933b5a669d25c8ccff62b63fdc
NotAShelf <raf@notashelf.dev>
2024-07-08 22:03:47 +0300

    gpu/intel: cleanup; replace deprecated `vaapiIntel` with `intel-vaapi-driver`

--- modules/core/common/system/hardware/gpu/intel/default.nix
+++ modules/core/common/system/hardware/gpu/intel/default.nix
@@ -1,38 +1,40 @@
 {
   config,
-  lib,
   pkgs,
+  lib,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (builtins) elem;
+  inherit (lib.modules) mkIf;
 
   dev = config.modules.device;
 
   # let me play youtube videos without h.264, please and thank you
-  vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  vaapiIntel = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
 in {
-  config = mkIf (builtins.elem dev.gpu.type ["intel" "hybrid-intel"]) {
-    # enable the i915 kernel module
+  config = mkIf (elem dev.gpu.type ["intel" "hybrid-intel"]) {
+    # Enable the i915 kernel module
     boot.initrd.kernelModules = ["i915"];
-    # better performance than the actual Intel driver
+
+    # Provides better performance than the actual Intel driver
     services.xserver.videoDrivers = ["modesetting"];
 
     # OpenCL support and VAAPI
     hardware.graphics = {
       extraPackages = with pkgs; [
-        intel-compute-runtime
-        intel-media-driver
         vaapiIntel
         vaapiVdpau
+        intel-compute-runtime
+        intel-media-driver
         libvdpau-va-gl
       ];
 
       extraPackages32 = with pkgs.pkgsi686Linux; [
-        # intel-compute-runtime # FIXME does not build due to unsupported system
-        intel-media-driver
         vaapiIntel
         vaapiVdpau
+        intel-media-driver
         libvdpau-va-gl
+        # intel-compute-runtime # FIXME does not build due to unsupported system
       ];
     };
 
