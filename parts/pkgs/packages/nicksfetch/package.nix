1c19837ea5fd1c326cc403017801a90fa8dae2c6
NotAShelf <raf@notashelf.dev>
2024-07-10 23:54:59 +0300

    pkgs/nicksfetch: modernize derivation

--- parts/pkgs/packages/nicksfetch/package.nix
+++ parts/pkgs/packages/nicksfetch/package.nix
@@ -7,45 +7,52 @@
   pciutils,
   x11Support ? true,
   ueberzug,
-}:
-stdenvNoCC.mkDerivation {
-  pname = "nicksfetch";
-  version = "unstable-2021-12-10";
+}: let
+  inherit (builtins) placeholder;
+  inherit (lib.strings) makeBinPath;
+  inherit (lib.lists) optional;
+in
+  stdenvNoCC.mkDerivation {
+    pname = "nicksfetch";
+    version = "unstable-2021-12-10";
 
-  src = fetchFromGitHub {
-    owner = "dylanaraps";
-    repo = "neofetch";
-    rev = "ccd5d9f52609bbdcd5d8fa78c4fdb0f12954125f";
-    sha256 = "sha256-9MoX6ykqvd2iB0VrZCfhSyhtztMpBTukeKejfAWYW1w=";
-  };
+    src = fetchFromGitHub {
+      owner = "dylanaraps";
+      repo = "neofetch";
+      rev = "ccd5d9f52609bbdcd5d8fa78c4fdb0f12954125f";
+      sha256 = "sha256-9MoX6ykqvd2iB0VrZCfhSyhtztMpBTukeKejfAWYW1w=";
+    };
 
-  patches = [./patches/0001-nicksfetch.patch];
+    patches = [./patches/0001-nicksfetch.patch];
 
-  outputs = ["out" "man"];
+    outputs = ["out" "man"];
 
-  strictDeps = true;
-  buildInputs = [bash];
-  nativeBuildInputs = [makeWrapper];
-  postPatch = ''
-    patchShebangs --host neofetch
-  '';
+    strictDeps = true;
+    buildInputs = [bash];
+    nativeBuildInputs = [makeWrapper];
 
-  postInstall = ''
-    wrapProgram $out/bin/neofetch \
-      --prefix PATH : ${lib.makeBinPath ([pciutils] ++ lib.optional x11Support ueberzug)}
-  '';
+    postPatch = ''
+      patchShebangs --host neofetch
+      substituteInPlace \
+        --replace "neofetch" "nicksfetch"
+    '';
 
-  makeFlags = [
-    "PREFIX=${placeholder "out"}"
-    "SYSCONFDIR=${placeholder "out"}/etc"
-  ];
+    postInstall = ''
+      wrapProgram $out/bin/neofetch \
+        --prefix PATH : ${makeBinPath ([pciutils] ++ optional x11Support ueberzug)}
+    '';
 
-  meta = {
-    description = "A fast, highly customizable system info script";
-    homepage = "https://github.com/dylanaraps/neofetch";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [alibabzo konimex notashelf];
-    mainProgram = "neofetch";
-  };
-}
+    makeFlags = [
+      "PREFIX=${placeholder "out"}"
+      "SYSCONFDIR=${placeholder "out"}/etc"
+    ];
+
+    meta = {
+      description = "A";
+      homepage = "https://github.com/dylanaraps/neofetch";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.all;
+      maintainers = with lib.maintainers; [NotAShelf];
+      mainProgram = "neofetch";
+    };
+  }
