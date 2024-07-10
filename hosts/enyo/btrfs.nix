3c47cc3465ddbc7acacf4556dbccc847f410c4dc
NotAShelf <raf@notashelf.dev>
2024-07-10 21:37:16 +0300

    hosts/enyo: use strings in btrfs backup service name(s)

--- hosts/enyo/btrfs.nix
+++ hosts/enyo/btrfs.nix
@@ -12,18 +12,19 @@
 in {
   config = mkIf (btrfsMounts != {}) {
     systemd = {
-      # create the snapshots directory
-      # it will linger for 30 days before it's dropped
-      # this serves as an easy way to persist the snapshots
-      # for a set amount of time
+      # Create a snapshots directory. It will be used to store periodic snapshots
+      # created via btfs subvolume snapshot. Those snapshots will linger for 30 days
+      # before they are dropped via systemd-tmpfiles settings.
       tmpfiles.settings."10-snapshots"."/var/lib/snapshots".d = {
         user = "root";
         group = "root";
         age = "30d";
       };
 
-      # run the snapshots on a weekly timer
-      timers.snapshot-home = {
+      # Run snapshotting job on a weekly timer. Persistent = true implies
+      # that the job will attempt to cover for missed jobs that were supposed
+      # to run during downtime.
+      timers."snapshot-home" = {
         enable = hasHomeSubvolume;
         description = "snapshot home subvolume";
         wantedBy = ["multi-user.target"];
@@ -33,10 +34,12 @@ in {
         };
       };
 
-      # create a snapshot of the /home subvolume
-      # it will be stored in /var/lib/snapshots with a timestamp
+      # Create a service job that will run if the host has a /home subvolume.
+      # It will create a snapshot of the /home subvolume and store it in
+      # /var/lib/snapshots with a timestamp in its filename.
+      # Timestamp format is as follows:
       #  %s - seconds since the Epoch (1970-01-01 00:00 UTC)
-      services.snapshot-home = {
+      services."snapshot-home" = {
         enable = hasHomeSubvolume;
         path = [pkgs.btrfs-progs];
         script = "btrfs subvolume snapshot /home /var/lib/snapshots/$(date +%s)";
