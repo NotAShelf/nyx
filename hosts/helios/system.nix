7e521febc7dd5efa04c304ad87fe46eff0f67214
NotAShelf <raf@notashelf.dev>
2024-07-10 22:36:47 +0300

    hosts/helios: add static hetzner gateway addresses

--- hosts/helios/system.nix
+++ hosts/helios/system.nix
@@ -2,24 +2,32 @@
   config,
   lib,
   ...
-}: {
+}: let
+  inherit (lib.modules) mkForce;
+in {
   config = {
     networking.domain = "notashelf.dev";
-    services.smartd.enable = lib.mkForce false;
+    services.smartd.enable = mkForce false;
 
     boot = {
       growPartition = !config.boot.initrd.systemd.enable;
       loader.grub = {
         enable = true;
-        useOSProber = lib.mkForce false;
-        efiSupport = lib.mkForce false;
+        useOSProber = mkForce false;
+        efiSupport = mkForce false;
         enableCryptodisk = false;
         theme = null;
         backgroundColor = null;
         splashImage = null;
-        device = lib.mkForce "/dev/disk/by-label/nixos";
+        device = mkForce "/dev/disk/by-label/nixos";
         forceInstall = true;
       };
+    };
+
+    # https://docs.hetzner.com/cloud/networks/faq/#are-any-ip-addresses-reserved
+    networking = {
+      defaultGateway.address = "172.31.1.1";
+      defaultGateway6.address = "fe80::1";
     };
   };
 }
