5909d2e7fd8c122e6bd9ee0113d5acbf70071d00
NotAShelf <raf@notashelf.dev>
2024-07-13 14:25:16 +0300

    services/harmonia: set cache priority

--- modules/core/roles/server/system/services/bincache/harmonia.nix
+++ modules/core/roles/server/system/services/bincache/harmonia.nix
@@ -30,6 +30,7 @@ in {
         settings = {
           # default ip:hostname to bind to
           bind = "${host}:${toString port}";
+          priority = 50; # prefer cache.nixos.org
         };
       };
     };
@@ -47,7 +48,6 @@ in {
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Connection $connection_upgrade;
-
             zstd on;
             zstd_types application/x-nix-archive;
           '';
