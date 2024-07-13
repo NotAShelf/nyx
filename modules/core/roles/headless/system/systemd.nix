57744a51ebeb010c3b1c2b52875889b523ffab0a
NotAShelf <raf@notashelf.dev>
2024-07-13 13:39:43 +0300

    roles/headless: hardcode watchdog device; fix links

--- modules/core/roles/headless/system/systemd.nix
+++ modules/core/roles/headless/system/systemd.nix
@@ -1,15 +1,21 @@
 {
-  # https://github.com/numtide/srvos/blob/main/nixos/server/default.nix
+  # Some values here are borrowed from nix-comunity/srvos project.
+  # See:
+  #  <https://github.com/nix-community/srvos/blob/e3e8ff545ef14f13c69a0f743078637fde952018/nixos/server/default.nix>
   systemd = {
-    # given that our systems are headless, emergency mode is useless.
-    # we prefer the system to attempt to continue booting so
-    # that we can hopefully still access it remotely.
+    # On a headless system, emergency mode is practically useless unless there is
+    # a way to physically access a console, e.g., Hetzner's cloud console. Disable
+    # emergency mode so that the system goes back into a boot loop that we may be
+    # able to use to debug our system.
     enableEmergencyMode = false;
 
     # For more detail, see:
-    #   https://0pointer.de/blog/projects/watchdog.html
+    #  <https://0pointer.de/blog/projects/watchdog.html>
     watchdog = {
-      # systemd will send a signal to the hardware watchdog at half
+      # Hardcode the watchdog device to /dev/watchdog. This is the default
+      # but we'd like to avoid surprises.
+      device = "/dev/watchdog";
+      # Systemd will send a signal to the hardware watchdog at half
       # the interval defined here, so every 10s.
       # If the hardware watchdog does not get a signal for 20s,
       # it will forcefully reboot the system.
