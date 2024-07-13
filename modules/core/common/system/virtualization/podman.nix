5007bc023fd53f7bdffb5a0d16ed60a77819f614
NotAShelf <raf@notashelf.dev>
2024-07-13 14:24:17 +0300

    virtualization/podman: set container registries; cleanup

--- modules/core/common/system/virtualization/podman.nix
+++ modules/core/common/system/virtualization/podman.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib.modules) mkIf;
 
   sys = config.modules.system.virtualization;
 in {
@@ -14,25 +14,37 @@ in {
       podman-desktop
     ];
 
-    virtualisation.podman = {
-      enable = true;
+    virtualisation = {
+      # Registries to search for images on `podman pull`
+      containers.registries.search = [
+        "docker.io"
+        "quay.io"
+        "ghcr.io"
+        "gcr.io"
+      ];
 
-      # make docker backwards compatible with docker interface
-      # certain interface elements will be different, but unless hardcoded
-      # does not cause problems for us
-      dockerCompat = true;
-      dockerSocket.enable = true;
-
-      defaultNetwork.settings.dns_enabled = true;
-
-      # enable nvidia support if any of the video drivers are nvidia
-      enableNvidia = builtins.any (driver: driver == "nvidia") config.services.xserver.videoDrivers;
-
-      # prune images and containers periodically
-      autoPrune = {
+      podman = {
         enable = true;
-        flags = ["--all"];
-        dates = "weekly";
+
+        # Make Podman backwards compatible with Docker socket interface.
+        # Certain interface elements will be different, but unless any
+        # of said values are hardcoded, it should not pose a problem
+        # for us.
+        dockerCompat = true;
+        dockerSocket.enable = true;
+
+        defaultNetwork.settings.dns_enabled = true;
+
+        # Enable Nvidia support for Podman if the Nvidia drivers are found
+        # in the list of xserver.videoDrivers.
+        enableNvidia = builtins.any (driver: driver == "nvidia") config.services.xserver.videoDrivers;
+
+        # Prune images and containers periodically
+        autoPrune = {
+          enable = true;
+          flags = ["--all"];
+          dates = "weekly";
+        };
       };
     };
   };
