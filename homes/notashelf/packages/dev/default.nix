09c41cdc9d0b09b255368bc300de98cdcad56abd
NotAShelf <raf@notashelf.dev>
2024-07-14 17:44:11 +0300

    packages/dev: remove full texlive output
    I'll likely need this in the future but when I do, it's going into a devShell

--- homes/notashelf/packages/dev/default.nix
+++ homes/notashelf/packages/dev/default.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   config = {
     home.packages = [
-      # LaTeX
-      pkgs.texlive.combined.scheme-full
       (pkgs.writeShellApplication {
         name = "pdflatexmk";
         runtimeInputs = [pkgs.texlivePackages.latexmk];
