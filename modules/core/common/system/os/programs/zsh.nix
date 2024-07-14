a3b4a9d835d772b96b081af9893235317c3e6271
NotAShelf <raf@notashelf.dev>
2024-07-14 22:45:08 +0300

    programs/zsh: always export ZDOTDIR

--- modules/core/common/system/os/programs/zsh.nix
+++ modules/core/common/system/os/programs/zsh.nix
@@ -1,8 +1,13 @@
 {
   # While we do have zsh enabled in userspace (i.e. home-manager) we also
-  # need it set system-wide since zsh will be our login shell. Do
+  # need it set system-wide since zsh will be our login shell.
   programs.zsh = {
     enable = true;
+
+    shellInit = ''
+      # Make sure we always use the correct zsh dotdir.
+      export ZDOTDIR=$HOME/.config/zsh
+    '';
 
     # We actually like zsh completion, but its setup belongs in home-manager
     # configuration to better support standalone home-manager installations. We
