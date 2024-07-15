3f1f3b7a927eab1a0ad8d218f67bfeab3f06e8b9
NotAShelf <raf@notashelf.dev>
2024-07-15 21:11:39 +0300

    services/getty: fix alignment of config rev in greetingLine

--- modules/core/common/system/os/services/getty.nix
+++ modules/core/common/system/os/services/getty.nix
@@ -1,8 +1,16 @@
-{config, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkForce;
   revision =
     if config.system.configurationRevision != null
-    then " @ " + config.system.configurationRevision
+    then " @ " + config.system.configurationRevision # this is already a string, no need to toString it
     else "";
 in {
-  services.getty.greetingLine = "<<< Welcome to ${config.meta.hostname} @ ${revision} >>>";
+  services.getty = {
+    greetingLine = "<<< Welcome to ${config.meta.hostname}${revision} >>>";
+    helpLine = mkForce "";
+  };
 }
