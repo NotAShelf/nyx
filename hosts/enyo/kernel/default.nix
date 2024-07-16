08a36b92d292049582ef361f7ec555f3239d5f71
NotAShelf <raf@notashelf.dev>
2024-07-16 22:19:42 +0300

    hosts/enyo: generalize kernel pkg call

--- hosts/enyo/kernel/default.nix
+++ hosts/enyo/kernel/default.nix
@@ -7,14 +7,17 @@
   inherit (lib.lists) filter;
   inherit (lib.strings) hasSuffix;
   inherit (lib.filesystem) listFilesRecursive;
+
+  # Get hostname from meta module.
   inherit (config.meta) hostname;
-  inherit (pkgs.callPackage ./package.nix {inherit hostname;}) xanmod_custom;
+
+  # Call the custom Xanmod package. Any future kernel builds from different sources
+  # should be added in the packages directory.
+  inherit (pkgs.callPackage ./packages/xanmod.nix {inherit hostname;}) xanmod_custom;
 in {
   # Autodiscover Nix files containing patch configurations
   # with patchfiles or extraStructuredConfig. This is not the
   # most optimized way to do it, but it works.
   imports = filter (hasSuffix ".nix") (listFilesRecursive ./config);
-  config = {
-    modules.system.boot.kernel = pkgs.linuxPackagesFor xanmod_custom;
-  };
+  config.modules.system.boot.kernel = pkgs.linuxPackagesFor xanmod_custom;
 }
--- hosts/enyo/kernel/packages/xanmod.nix
+++ hosts/enyo/kernel/packages/xanmod.nix
@@ -4,16 +4,17 @@
   gccStdenv,
   buildLinux,
   fetchFromGitHub,
+  kernelPatches,
   # Args to be passed to the kernel builder
   hostname ? "",
+  suffix ? "shelf",
   ...
 }: let
-  inherit (lib.modules) mkForce;
+  inherit (lib.modules) mkForce mkOverride;
   inherit (lib.kernel) yes no freeform;
   inherit (lib.versions) pad;
 
   version = "6.9.9";
-  suffix = "shelf";
   modDirVersion = pad 3 "${version}-${suffix}";
 
   xanmod_custom =
@@ -30,22 +31,17 @@
         hash = "sha256-ZEU1RIgJ0ckyITFWZndEzXYwnTF39OviLxL9S5dEea4=";
       };
 
-      # Those patches are passed to Xanmod derivations while being called
-      # from top-level. Since we build Xanmod from source now, we need to
-      # source the patches manually. For consistency, I also vendor the
-      # patches in patches/ alongside my custom (future) patches.
+      # Kernel derivations in Nixpkgs apply a set of patches to the kernel
+      # in order to improve the reproducibility, security or compatibility
+      # of the kernel. Patches below from pkgs.kernelPatches are passed to
+      # the nixpkgs Xanmod builds, but since we build Xanmod from source here
+      # they will be missing. Re-add those patches to the manual builder alongside
+      # any other patches that I might need.
       kernelPatches = [
-        {
-          name = "bridge-stp-helper";
-          patch = ./patches/bridge-stp-helper.patch;
-        }
-        {
-          name = "request-key-helper";
-          patch = ./patches/request-key-helper.patch;
-        }
+        kernelPatches.bridge_stp_helper
+        kernelPatches.request_key_helper
       ];
 
-      extraMakeFlags = ["KCFLAGS=-DAMD_PRIVATE_COLOR"];
       ignoreConfigErrors = true;
 
       # after booting to the new kernel
@@ -55,12 +51,12 @@
       # those values in most cases.
       structuredExtraConfig = {
         # CPUFreq governor Performance
-        CPU_FREQ_DEFAULT_GOV_PERFORMANCE = lib.mkOverride 60 yes;
-        CPU_FREQ_DEFAULT_GOV_SCHEDUTIL = lib.mkOverride 60 no;
+        CPU_FREQ_DEFAULT_GOV_PERFORMANCE = mkOverride 60 yes;
+        CPU_FREQ_DEFAULT_GOV_SCHEDUTIL = mkOverride 60 no;
 
         # Full preemption
-        PREEMPT = lib.mkOverride 60 yes;
-        PREEMPT_VOLUNTARY = lib.mkOverride 60 no;
+        PREEMPT = mkOverride 60 yes;
+        PREEMPT_VOLUNTARY = mkOverride 60 no;
 
         # Preemptive Full Tickless Kernel at 250Hz
         HZ = freeform "250";
