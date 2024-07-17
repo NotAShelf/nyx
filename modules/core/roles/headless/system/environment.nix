75ae4b7ae6b5350e3f354213679dbe98cfcffecb
NotAShelf <raf@notashelf.dev>
2024-07-17 18:37:52 +0300

    roles/headless: we are not ready for noXlibs
    FUCK.

--- modules/core/roles/headless/system/environment.nix
+++ modules/core/roles/headless/system/environment.nix
@@ -1,9 +1,10 @@
 {
   environment = {
     # Disable X11 libraries on headless systems to save as much space as we possibly can.
-    # Setting this to true might need some packages to be overlayed, as they may depend
-    # on X libraries but provide an override to avoid the dependency.
-    noXlibs = true;
+    # Settings this to true generally breaks a lot of GUI and non-GUI packages that, for
+    # some reason, depend on xlibs. If this is true, said packages may also need to be
+    # put into overlays. See `./nix.nix` for an example for Nginx.
+    noXlibs = false;
 
     # On servers, print the URL instead of trying to open them with a browser.
     variables.BROWSER = "echo";
