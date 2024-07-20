632e9514c765d9cee825f52c23fc1693a653116e
NotAShelf <raf@notashelf.dev>
2024-07-20 11:36:19 +0300

    parts: add top-level hydraJobs

--- parts/default.nix
+++ parts/default.nix
@@ -1,23 +1,24 @@
 {
-  # Imports for constructing a final flake to be built.
+  # Explicitly import "parts" of a flake to compose it modularly. This
+  # allows me to import each part to construct the "final flake" instead
+  # of declaring everything from a single, convoluted file.
+  # By convention, things that would usually go to flake.nix should
+  # have its own file in the `flake/` directory.
   imports = [
-    # Explicitly import parts of the flake, which allows me to build the
-    # "final flake" from various parts, arranged in a way that makes
-    # sense to me the most. By convention, things that would usually
-    # go to flake.nix should have its own file in the `flake/` directory.
-    ./apps # apps provided by the flake
-    ./checks # checks that are performed on `nix flake check`
-    ./lib # extended library on top of `nixpkgs.lib`
-    ./modules # nixos and home-manager modules provided by this flake
-    ./pkgs # packages exposed by the flake
-    ./pre-commit # pre-commit hooks, performed before each commit inside the devShell
-    ./templates # flake templates
+    ./apps # "Runnables" exposed by the flake, used with `nix run .#<appName`
+    ./checks # Checks that will be built when `nix flake check is run`
+    ./lib # Extensible extended library built on top of `nixpkgs.lib`
+    ./modules # NixOS and Home-Manager modules provided by the flake
+    ./pkgs # Per-system packages exposed by the flake
+    ./pre-commit # Pre-commit hooks, to be ran before changes are committed.
+    ./templates # Templates for initiating flakes with `nix flake init -t ...`
 
-    ./args.nix # args that are passed to the flake, moved away from the main file
+    ./args.nix # Args for the flake, consumed or propagated to parts by flake-parts
     ./ci.nix # GitHub actions matrices generated via Nix
     ./deployments.nix # deploy-rs configurations for active hosts
-    ./fmt.nix # various formatter configurations for this flake
-    ./iso-images.nix # local installation media
+    ./hydra.nix # Hydra build jobs to be evaluated on each commit
+    ./fmt.nix # Formatter configurations via Treefmt
+    ./iso-images.nix # Build recipes for local installation media
     ./shell.nix # devShells exposed by the flake
   ];
 }
---
+++ parts/hydra.nix
@@ -0,0 +1,28 @@
+{
+  self,
+  lib,
+  ...
+}: let
+  inherit (builtins) mapAttrs;
+  inherit (lib.trivial) const;
+  inherit (lib.attrsets) mapAttrs' foldlAttrs filterAttrs recursiveUpdate nameValuePair;
+
+  toHydraJob =
+    foldlAttrs
+    (jobset: system: attrs:
+      recursiveUpdate jobset
+      (mapAttrs (const (drv: {${system} = drv;}))
+        (filterAttrs (name: const (name != "default")) attrs)))
+    {};
+
+  # This is a simple-ish way to generate a flake that has a jobset for each
+  # nixosConfiguration available in the schema. Very costy though.
+  configJobs = mapAttrs' (name: config: nameValuePair "nixos-${name}" config.config.system.build.toplevel) self.nixosConfigurations;
+in {
+  flake.hydraJobs = {
+    packages = toHydraJob self.packages;
+    checks = toHydraJob self.checks;
+
+    inherit configJobs;
+  };
+}
