189fe235d5180ba9b32c4ad0d4fa85ecdfbec0ed
NotAShelf <raf@notashelf.dev>
2024-07-21 21:32:52 +0300

    roles/headless: allow overriding noXlibs

--- modules/core/roles/headless/system/environment.nix
+++ modules/core/roles/headless/system/environment.nix
@@ -1,10 +1,12 @@
-{
+{lib, ...}: let
+  inherit (lib.modules) mkDefault;
+in {
   environment = {
     # Disable X11 libraries on headless systems to save as much space as we possibly can.
     # Settings this to true generally breaks a lot of GUI and non-GUI packages that, for
     # some reason, depend on xlibs. If this is true, said packages may also need to be
     # put into overlays. See `./nix.nix` for an example for Nginx.
-    noXlibs = false;
+    noXlibs = mkDefault false;
 
     # On servers, print the URL instead of trying to open them with a browser.
     variables.BROWSER = "echo";
