6e958f19d9165dd76b407567990ad3f48a9f7f52
NotAShelf <raf@notashelf.dev>
2024-07-24 14:36:38 +0300

    parts/args: cleanup; add doc comments

--- parts/args.nix
+++ parts/args.nix
@@ -1,6 +1,6 @@
 {inputs, ...}: let
   # Add `pins` to self so that the flake may refer it freely by accessing
-  # `pings` in an argset (e.g. `{pkgs, lib, pins, ...}`).
+  # `pins` in an argset (e.g. `{pkgs, lib, pins, ...}`).
   # Pinned sources can be updated via `npins update` in `flake/`
   # which will automatically bump all sources in the `npins`
   # directory relatvie to this file.
@@ -17,22 +17,10 @@ in {
     system,
     ...
   }: {
-    imports = [
-      {
-        _module.args = {
-          pins = pinnedSources;
-          keys = publicKeys;
-
-          # Unify all instances of nixpkgs into a single `pkgs` set
-          # Wthat includes our own overlays within `perSystem`. This
-          # is not done by flake-parts, so we do it ourselves.
-          # See:
-          #  <https://github.com/hercules-ci/flake-parts/issues/106#issuecomment-1399041045>
-          pkgs = config.legacyPackages;
-        };
-      }
-    ];
-
+    # Configure nixpkgs locally and expose it as <flakeRef>.legacyPackages.
+    # This will then be consumed to override flake-parts' pkgs argument
+    # to make sure pkgs instances in flake-parts modules are all referring
+    # to the same configuration instance - this one.
     legacyPackages = import inputs.nixpkgs {
       inherit system;
       config = {
@@ -41,6 +29,21 @@ in {
       };
 
       overlays = [inputs.self.overlays.default];
+    };
+
+    _module.args = {
+      # Pass `pins` and `keys` to flake-parts' module args. This allows
+      # parts of the module system to refer to them in a more concise
+      # way than importing them directly.
+      pins = pinnedSources;
+      keys = publicKeys;
+
+      # Unify all instances of nixpkgs into a single `pkgs` set
+      # Wthat includes our own overlays within `perSystem`. This
+      # is not done by flake-parts, so we do it ourselves.
+      # See:
+      #  <https://github.com/hercules-ci/flake-parts/issues/106#issuecomment-1399041045>
+      pkgs = config.legacyPackages;
     };
   };
 
